142e3c8a0a0ba0a420f809c70123c1ba6b4c5541
David Négrier <d.negrier@thecodingmachine.com>
2020-04-05 23:30:22 +0200

    Adding sprite on scene, first test

--- front/src/GameScene.ts
+++ front/src/GameScene.ts
@@ -1,5 +1,8 @@
+import Sprite = Phaser.GameObjects.Sprite;
 
 export class GameScene extends Phaser.Scene {
+
+    private player: Sprite;
 
     constructor() {
         super({
@@ -29,34 +32,35 @@ export class GameScene extends Phaser.Scene {
         // Let's manage animations of the player
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 8 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { start: 9, end: 12 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'up',
-            frames: this.anims.generateFrameNumbers('player', { start: 13, end: 16 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 9, end: 12 }),
             frameRate: 10,
             repeat: -1
         });
 
-        //let player = this.add.sprite(450, 450, 'player');
-        //player.anims.play('down');
+        // TODO: create a Player class extending a Character class that itself extends from physics.Sprite.
+        this.player = this.add.sprite(150, 130, 'player');
+        this.player.anims.play('right');
         //player.setBounce(0.2);
         //player.setCollideWorldBounds(true);
 
@@ -66,8 +70,10 @@ export class GameScene extends Phaser.Scene {
     private angle: number = 0;
 
     update(dt: number): void {
-        this.cameras.main.scrollX = Math.floor(300 + 300 * Math.cos(this.angle));
-        this.cameras.main.scrollY = Math.floor(300 + 300 * Math.sin(this.angle));
+        this.cameras.main.scrollX = Math.floor(dt * 0.05);
+        this.player.x = Math.floor(dt * 0.05) + 150;
+        //this.cameras.main.scrollX = Math.floor(300 + 300 * Math.cos(this.angle));
+        //this.cameras.main.scrollY = Math.floor(300 + 300 * Math.sin(this.angle));
 
         this.angle = dt * 0.0001;
     }
