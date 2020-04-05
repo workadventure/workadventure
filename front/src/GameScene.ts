b10f8d90695cc14f3db2aff139d7b8f4c22bff91
David Négrier <d.negrier@thecodingmachine.com>
2020-04-05 16:20:53 +0200

    Loading first sprite

--- front/src/GameScene.ts
+++ front/src/GameScene.ts
@@ -10,6 +10,10 @@ export class GameScene extends Phaser.Scene {
     preload(): void {
         this.load.image('tiles', 'maps/tiles.png');
         this.load.tilemapTiledJSON('map', 'maps/map2.json');
+        this.load.spritesheet('player',
+            'resources/characters/pipoya/Male 01-1.png',
+            { frameWidth: 32, frameHeight: 32 }
+        );
     }
 
     init(): void {
@@ -21,6 +25,41 @@ export class GameScene extends Phaser.Scene {
 
         let bottomLayer = mappy.createStaticLayer("Calque 1", [terrain], 0, 0);
         let topLayer = mappy.createStaticLayer("Calque 2", [terrain], 0, 0);
+
+        // Let's manage animations of the player
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { start: 9, end: 12 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('player', { start: 13, end: 16 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //let player = this.add.sprite(450, 450, 'player');
+        //player.anims.play('down');
+        //player.setBounce(0.2);
+        //player.setCollideWorldBounds(true);
+
 
     }
 
