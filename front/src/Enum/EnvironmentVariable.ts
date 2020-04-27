0d6c4032d51c05e7e4d2f328c19c97779e9148c3
David Négrier <d.negrier@thecodingmachine.com>
2020-04-27 18:12:36 +0200

    Playing with zoom levels

--- front/src/Enum/EnvironmentVariable.ts
+++ front/src/Enum/EnvironmentVariable.ts
@@ -1,8 +1,8 @@
 const DEBUG_MODE: boolean = !!process.env.DEBUG_MODE || false;
 const API_URL = process.env.API_URL || "http://api.workadventure.localhost";
 const ROOM = [process.env.ROOM || "THECODINGMACHINE"];
-const RESOLUTION = 2;
-const ZOOM_LEVEL = 3/4;
+const RESOLUTION = 4;
+const ZOOM_LEVEL = 1/*3/4*/;
 
 export {
     DEBUG_MODE,
