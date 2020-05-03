bf723b2e3012b8c7a7cc8804e98fe7cca828bb8b
David Négrier <d.negrier@thecodingmachine.com>
2020-05-03 17:46:26 +0200

    Reducing connection distance and group radius

--- back/src/Enum/EnvironmentVariable.ts
+++ back/src/Enum/EnvironmentVariable.ts
@@ -1,7 +1,7 @@
 const SECRET_KEY = process.env.SECRET_KEY || "THECODINGMACHINE_SECRET_KEY";
 const ROOM = process.env.ROOM || "THECODINGMACHINE";
-const MINIMUM_DISTANCE = process.env.MINIMUM_DISTANCE ? Number(process.env.MINIMUM_DISTANCE) : 160;
-const GROUP_RADIUS = process.env.GROUP_RADIUS ? Number(process.env.GROUP_RADIUS) : 128;
+const MINIMUM_DISTANCE = process.env.MINIMUM_DISTANCE ? Number(process.env.MINIMUM_DISTANCE) : 64;
+const GROUP_RADIUS = process.env.GROUP_RADIUS ? Number(process.env.GROUP_RADIUS) : 48;
 
 export {
     SECRET_KEY,
