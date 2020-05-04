b51ce518477950db685c761fea30753138f9567f
gparant <g.parant@thecodingmachine.com>
2020-05-04 08:44:07 +0200

    Fix unit test

--- back/tests/MessageTest.ts
+++ back/tests/MessageTest.ts
@@ -3,14 +3,15 @@ import {Message} from "../src/Model/Websocket/Message";
 
 describe("Message Model", () => {
     it("should find userId and roomId", () => {
-        let message = {userId: "test1", roomId: "test2"};
+        let message = {userId: "test1", roomId: "test2", name: "foo"};
         let messageObject = new Message(message);
         expect(messageObject.userId).toBe("test1");
         expect(messageObject.roomId).toBe("test2");
+        expect(messageObject.name).toBe("foo");
     })
 
     it("should expose a toJson method", () => {
-        let message = {userId: "test1", roomId: "test2"};
+        let message = {userId: "test1", roomId: "test2", name: "foo"};
         let messageObject = new Message(message);
         expect(messageObject.toJson()).toEqual({userId: "test1", roomId: "test2", name: "foo"});
     })
@@ -24,6 +25,13 @@ describe("Message Model", () => {
 
     it("should find throw error when no roomId", () => {
         let message = {userId: "test1"};
+        expect(() => {
+            let messageObject = new Message(message);
+        }).toThrow(new Error("userId or roomId cannot be null"));
+    })
+
+    it("should find throw error when no roomId", () => {
+        let message = {name: "foo"};
         expect(() => {
             let messageObject = new Message(message);
         }).toThrow(new Error("userId or roomId cannot be null"));
