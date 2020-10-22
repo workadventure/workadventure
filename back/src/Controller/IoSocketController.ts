a68c6612215f0d5094c63eb4dc7c42d755abfd70
Gregoire Parant <g.parant@thecodingmachine.com>
2020-10-22 11:28:34 +0200

    Fix CI

--- back/src/Controller/IoSocketController.ts
+++ back/src/Controller/IoSocketController.ts
@@ -159,11 +159,10 @@ export class IoSocketController {
                         }
 
                         const userUuid = await jwtTokenManager.getUserUuidFromToken(token);
-                        let client = socketManager.searchClientByUuid(userUuid);
+                        const client = socketManager.searchClientByUuid(userUuid);
                         //if client socket exist, close connexion
                         if(client){
                             console.info(`Close connexion, client with uuid : ${userUuid} already exist !`);
-                            socketManager.emitCloseMessage(client, 500);
                             socketManager.leaveRoom(client);
                         }
 
