c633de5eedc03fdf6744f099131a88e12ee3f127
David Négrier <d.negrier@thecodingmachine.com>
2020-12-14 22:43:05 +0100

    Making HTTP and GRPC ports configurable in API server

--- back/server.ts
+++ back/server.ts
@@ -3,11 +3,13 @@ import App from "./src/App";
 import grpc from "grpc";
 import {roomManager} from "./src/RoomManager";
 import {IRoomManagerServer, RoomManagerService} from "./src/Messages/generated/messages_grpc_pb";
+import {HTTP_PORT, GRPC_PORT} from "./src/Enum/EnvironmentVariable";
 
-App.listen(8080, () => console.log(`WorkAdventure starting on port 8080!`))
+App.listen(HTTP_PORT, () => console.log(`WorkAdventure HTTP API starting on port %d!`, HTTP_PORT))
 
 const server = new grpc.Server();
 server.addService<IRoomManagerServer>(RoomManagerService, roomManager);
 
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+server.bind('0.0.0.0:'+GRPC_PORT, grpc.ServerCredentials.createInsecure());
 server.start();
+console.log('WorkAdventure HTTP/2 API starting on port %d!', GRPC_PORT);
