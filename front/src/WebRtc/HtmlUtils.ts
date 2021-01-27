cdbae5fa4325a75aff58a3e42fa1ca6cdf401e2e
Tony Amirault <tonyamirault@macbook-pro.home>
2021-01-27 18:33:40 +0100

    use type guard on html element

--- front/src/WebRtc/HtmlUtils.ts
+++ front/src/WebRtc/HtmlUtils.ts
@@ -1,21 +1,19 @@
 export class HtmlUtils {
     public static getElementByIdOrFail<T extends HTMLElement>(id: string): T {
         const elem = document.getElementById(id);
-        if (elem === null) {
-            throw new Error("Cannot find HTML element with id '"+id+"'");
+        if (HtmlUtils.isHtmlElement<T>(elem)) {
+           return elem
         }
-        // FIXME: does not check the type of the returned type
-        return elem as T;
+        throw new Error("Cannot find HTML element with id '"+id+"'");
     }
 
     public static removeElementByIdOrFail<T extends HTMLElement>(id: string): T {
         const elem = document.getElementById(id);
-        if (elem === null) {
-            throw new Error("Cannot find HTML element with id '"+id+"'");
+        if (HtmlUtils.isHtmlElement<T>(elem)) {
+            elem.remove();
+            return elem
         }
-        // FIXME: does not check the type of the returned type
-        elem.remove();
-        return elem as T;
+        throw new Error("Cannot find HTML element with id '"+id+"'");
     }
 
     public static urlify(text: string): string {
@@ -23,5 +21,9 @@ export class HtmlUtils {
         return text.replace(urlRegex, (url: string) => {
             return '<a href="' + url + '" target="_blank" style=":visited {color: white}">' + url + '</a>';
         })
+    }
+
+    private static isHtmlElement<T extends HTMLElement>(elem: HTMLElement | null): elem is T {
+        return elem !== null
     }
 }
