f99db4856fbc4754f0413111fd752b9f71816776
Gregoire Parant <g.parant@thecodingmachine.com>
2021-01-30 20:33:31 +0100

    Add WorkAdventure logo

--- front/src/Phaser/Components/Loader.ts
+++ front/src/Phaser/Components/Loader.ts
@@ -1,6 +1,18 @@
+const LogoNameIndex: string = 'logo';
+const LogoResource: string = 'resources/logos/logo.png';
 
-export const addLoader = (scene:Phaser.Scene): void => {
-    const loadingText = scene.add.text(scene.game.renderer.width / 2, 200, 'Loading');
+export const addLoader = (scene: Phaser.Scene): void => {
+    const loaderPlugin = scene.load.image(LogoNameIndex, LogoResource);
+    loaderPlugin.spritesheet(LogoNameIndex, LogoResource);
+    const promiseLoadLogoTexture = new Promise<Phaser.GameObjects.Image>((res) => {
+        if (loaderPlugin.textureManager.exists(LogoNameIndex)) {
+            return res(scene.add.image(scene.game.renderer.width / 2, 100, LogoNameIndex));
+        }
+        loaderPlugin.once(`filecomplete-spritesheet-${LogoNameIndex}`, () => {
+            res(scene.add.image(scene.game.renderer.width / 2, 100, LogoNameIndex))
+        });
+    });
+
     const progress = scene.add.graphics();
     scene.load.on('progress', (value: number) => {
         progress.clear();
@@ -8,7 +20,9 @@ export const addLoader = (scene:Phaser.Scene): void => {
         progress.fillRect(0, 270, 800 * value, 60);
     });
     scene.load.on('complete', () => {
-        loadingText.destroy();
+        promiseLoadLogoTexture.then((resLoadingImage: Phaser.GameObjects.Image) => {
+            resLoadingImage.destroy();
+        });
         progress.destroy();
     });
 }
