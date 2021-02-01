183769198477c09dda63e6a7f9130bfff7cbce5b
Christian Loelkes <christian.loelkes@gmail.com>
2021-02-01 16:08:49 +0100

    set the public url for the front server in webpack
    Without this the client tries to guess the server port and url. This
    fails since the server runs behind a reverse proxy: the server runs on
    :8080 but the reverse proxy listens to :443.
    
    See https://webpack.js.org/configuration/dev-server/#devserverpublic

--- front/webpack.prod.js
+++ front/webpack.prod.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
 
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map'
-});
+module.exports = env => {
+  console.log('FRONT_URL is: ' + env.FRONT_URL);
+  return merge(common, {
+      mode: 'production',
+      devtool: 'source-map',
+      devServer: {'public': env.FRONT_URL}
+    });
+};
