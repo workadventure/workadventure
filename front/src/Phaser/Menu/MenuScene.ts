03f1f5bf75272c6494eb72df2d1fcbb1fb027374
Gregoire Parant <g.parant@thecodingmachine.com>
2021-02-04 16:05:05 +0100

    Update feedback @moufmouf

--- front/src/Phaser/Menu/MenuScene.ts
+++ front/src/Phaser/Menu/MenuScene.ts
@@ -117,10 +117,11 @@ export class MenuScene extends Phaser.Scene {
             const adminSection = this.menuElement.getChildByID('adminConsoleSection') as HTMLElement;
             adminSection.hidden = false;
         }
-        if (connectionManager.getConnexionType === GameConnexionTypes.anonymous){
+        //TODO bind with future metadata of card
+        //if (connectionManager.getConnexionType === GameConnexionTypes.anonymous){
             const adminSection = this.menuElement.getChildByID('socialLinks') as HTMLElement;
             adminSection.hidden = false;
-        }
+        //}
         this.tweens.add({
             targets: this.menuElement,
             x: openedSideMenuX,
