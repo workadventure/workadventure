05b829da7092009387cda7773973303a3922d8c6
Tim Schlüter <mail@tim-s.de>
2021-02-13 21:19:45 +0100

    fix multi user videochat mode

--- front/src/WebRtc/LayoutManager.ts
+++ front/src/WebRtc/LayoutManager.ts
@@ -188,7 +188,7 @@ class LayoutManager {
         } else {
             HtmlUtils.getElementByIdOrFail<HTMLDivElement>('sidebar').style.display = 'none';
             HtmlUtils.getElementByIdOrFail<HTMLDivElement>('main-section').style.display = 'none';
-            HtmlUtils.getElementByIdOrFail<HTMLDivElement>('chat-mode').style.display = 'flex';
+            HtmlUtils.getElementByIdOrFail<HTMLDivElement>('chat-mode').style.display = 'grid';
         }
 
         for (const div of this.importantDivs.values()) {
