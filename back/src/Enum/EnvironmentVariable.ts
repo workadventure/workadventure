ce2c5e0cb55af3d452b30b92120b84c37b70b3bf
kharhamel <oognic@gmail.com>
2021-04-14 15:36:25 +0200

    FEATURE: added the env variable MAX_PER_GROUP

--- back/src/Enum/EnvironmentVariable.ts
+++ back/src/Enum/EnvironmentVariable.ts
@@ -11,6 +11,7 @@ const HTTP_PORT = parseInt(process.env.HTTP_PORT || '8080') || 8080;
 const GRPC_PORT = parseInt(process.env.GRPC_PORT || '50051') || 50051;
 export const SOCKET_IDLE_TIMER = parseInt(process.env.SOCKET_IDLE_TIMER as string) || 30; // maximum time (in second) without activity before a socket is closed
 export const TURN_STATIC_AUTH_SECRET = process.env.TURN_STATIC_AUTH_SECRET || '';
+export const MAX_PER_GROUP = parseInt(process.env.MAX_PER_GROUP || '4');
 
 export {
     MINIMUM_DISTANCE,
