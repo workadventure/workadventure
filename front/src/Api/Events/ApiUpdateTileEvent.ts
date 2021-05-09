1c918b01a80efb91d5abf396a9ef4260f156969e
jonny <ga86lad@mytum.de>
2021-05-10 01:21:37 +0200

    refactored to Array of tile

--- front/src/Api/Events/ApiUpdateTileEvent.ts
+++ front/src/Api/Events/ApiUpdateTileEvent.ts
@@ -3,13 +3,14 @@ import * as tg from "generic-type-guard";
 export const updateTile = "updateTile"
 
 
-export const isUpdateTileEvent =
+export const isUpdateTileEvent = tg.isArray(
     new tg.IsInterface().withProperties({
         x: tg.isNumber,
         y: tg.isNumber,
         tile: tg.isUnion(tg.isNumber, tg.isString),
         layer: tg.isUnion(tg.isNumber, tg.isString)
-    }).get();
+    }).get()
+);
 /**
  * A message sent from the game to the iFrame when a user enters or leaves a zone marked with the "zone" property.
  */
