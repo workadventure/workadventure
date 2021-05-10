9c1601c2b4fff022f49bd7c2e843a2b7d54f122b
Gregoire Parant <g.parant@thecodingmachine.com>
2021-05-10 13:36:56 +0200

    Remove old changes

--- front/src/Connexion/LocalUser.ts
+++ front/src/Connexion/LocalUser.ts
@@ -28,23 +28,3 @@ export class LocalUser {
     constructor(public readonly uuid:string, public readonly jwtToken: string, public readonly textures: CharacterTexture[]) {
     }
 }
-
-export class ConnectedUser {
-    constructor(
-        public readonly name:string,
-        public readonly email: string,
-        public readonly uuid:string,
-        public readonly jwtToken: string,
-        public notification?: [],
-        public announcements?: [],
-    ) {
-    }
-
-    setNotification(notification: []){
-        this.notification = notification;
-    }
-
-    setAnnouncements(announcements: []){
-        this.announcements = announcements;
-    }
-}
