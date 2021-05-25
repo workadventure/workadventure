36f0cd1a23206c14b718165875ba4d970def49ed
GRL <g.lesniewski@thecodingmachine.com>
2021-05-25 10:11:27 +0200

    Revert "pause for loading tileset on the fly"
    This reverts commit a3165a0540f8aef8477c62e6b4d4dad6adac1150.

--- front/src/Phaser/Game/GameScene.ts
+++ front/src/Phaser/Game/GameScene.ts
@@ -910,12 +910,12 @@ ${escapedMessage}
             iframeListener.sendUserTagList({list: this.connection.getAllTag()});
         }))
 
-/*        this.iframeSubscriptionList.push(iframeListener.tilesetLoaderStream.subscribe((tileset) => {
+        this.iframeSubscriptionList.push(iframeListener.tilesetLoaderStream.subscribe((tileset) => {
             //this.load.tilemapTiledJSON('logo', tileset.imgUrl);
             this.load.image('logo', tileset.imgUrl);
             this.Terrains.push(this.Map.addTilesetImage(tileset.name, tileset.imgUrl, tileset.tilewidth, tileset.tileheight, tileset.margin, tileset.spacing));
             this.gameMap.addTerrain(this.Terrains[this.Terrains.length - 1]);
-        }))*/
+        }))
 
     }
 
