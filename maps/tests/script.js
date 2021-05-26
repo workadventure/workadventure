29dc5329ce73d56bfd8fe080cd4707346df9efcf
jonny <ga86lad@mytum.de>
2021-05-26 18:09:28 +0200

    added script example in test

--- maps/tests/script.js
+++ maps/tests/script.js
@@ -1,40 +1,42 @@
+///<reference path="../../front/src/iframe_api.ts" />
+
 console.log('SCRIPT LAUNCHED');
 //WA.sendChatMessage('Hi, my name is Poly and I repeat what you say!', 'Poly Parrot');
 var isFirstTimeTuto = false;
 var textFirstPopup = 'Hey ! This is how to open start a discussion with someone ! You can be 4 max in a booble';
 var textSecondPopup = 'You can also use the chat to communicate ! ';
-var targetObjectTutoBubble ='myPopup1';
-var targetObjectTutoChat ='myPopup2';
+var targetObjectTutoBubble = 'myPopup1';
+var targetObjectTutoChat = 'myPopup2';
 var popUpExplanation = undefined;
-function launchTuto (){
-        WA.openPopup(targetObjectTutoBubble, textFirstPopup, [
-            {
-                label: "Next",
-                className: "popUpElement",
-                callback: (popup) => {
-                    popup.close();
+function launchTuto() {
+    WA.openPopup(targetObjectTutoBubble, textFirstPopup, [
+        {
+            label: "Next",
+            className: "popUpElement",
+            callback: (popup) => {
+                popup.close();
 
-                    WA.openPopup(targetObjectTutoChat, textSecondPopup, [
-                        {
-                            label: "Open Chat",
-                            className: "popUpElement",
-                            callback: (popup1) => {
-                                WA.sendChatMessage("Hey you can talk here too ! ", 'WA Guide');
-                                popup1.close();
-                                WA.restorePlayerControls();
-                            }
+                WA.openPopup(targetObjectTutoChat, textSecondPopup, [
+                    {
+                        label: "Open Chat",
+                        className: "popUpElement",
+                        callback: (popup1) => {
+                            WA.sendChatMessage("Hey you can talk here too ! ", 'WA Guide');
+                            popup1.close();
+                            WA.restorePlayerControls();
                         }
+                    }
 
-                    ])
-                }
+                ])
             }
-        ]);
-        WA.disablePlayerControls();
+        }
+    ]);
+    WA.disablePlayerControls();
 
 }
 WA.onChatMessage((message => {
     console.log('CHAT MESSAGE RECEIVED BY SCRIPT');
-    WA.sendChatMessage('Poly Parrot says: "'+message+'"', 'Poly Parrot');
+    WA.sendChatMessage('Poly Parrot says: "' + message + '"', 'Poly Parrot');
 }));
 
 WA.onEnterZone('myTrigger', () => {
@@ -50,11 +52,11 @@ WA.onEnterZone('notExist', () => {
 
 WA.onEnterZone('popupZone', () => {
     WA.displayBubble();
-    if (!isFirstTimeTuto) {
+    if(!isFirstTimeTuto) {
         isFirstTimeTuto = true;
         launchTuto();
     }
-     else popUpExplanation =  WA.openPopup(targetObjectTutoChat,'Do you want to review the explanation ? ', [
+    else popUpExplanation = WA.openPopup(targetObjectTutoChat, 'Do you want to review the explanation ? ', [
         {
             label: "No",
             className: "popUpElementReviewexplanation",
@@ -74,6 +76,12 @@ WA.onEnterZone('popupZone', () => {
 });
 
 WA.onLeaveZone('popupZone', () => {
-    if (popUpExplanation !== undefined) popUpExplanation.close();
+    if(popUpExplanation !== undefined) popUpExplanation.close();
     WA.removeBubble();
 })
+
+
+const message = WA.triggerMessage("testMessage")
+setTimeout(() => {
+    message.remove();
+}, 5000)
