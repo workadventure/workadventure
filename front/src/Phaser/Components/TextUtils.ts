9bcdc9ba338df6fcfe6140e217d036bac18ea950
jonny <ga86lad@mytum.de>
2021-07-02 19:05:03 +0200

    linter fixes

--- front/src/Phaser/Components/TextUtils.ts
+++ front/src/Phaser/Components/TextUtils.ts
@@ -1,10 +1,10 @@
-import type { ITiledMapObject } from "../Map/ITiledMap";
-import type { GameScene } from "../Game/GameScene";
+import type { ITiledMapObject } from '../Map/ITiledMap';
+import type { GameScene } from '../Game/GameScene';
 
 export class TextUtils {
     public static createTextFromITiledMapObject(scene: GameScene, object: ITiledMapObject): void {
         if (object.text === undefined) {
-            throw new Error("This object has not textual representation.");
+            throw new Error('This object has not textual representation.');
         }
         const options: {
             fontStyle?: string;
@@ -18,18 +18,18 @@ export class TextUtils {
             };
         } = {};
         if (object.text.italic) {
-            options.fontStyle = "italic";
+            options.fontStyle = 'italic';
         }
         // Note: there is no support for "strikeout" and "underline"
         let fontSize: number = 16;
         if (object.text.pixelsize) {
             fontSize = object.text.pixelsize;
         }
-        options.fontSize = fontSize + "px";
+        options.fontSize = fontSize + 'px';
         if (object.text.fontfamily) {
             options.fontFamily = '"' + object.text.fontfamily + '"';
         }
-        let color = "#000000";
+        let color = '#000000';
         if (object.text.color !== undefined) {
             color = object.text.color;
         }
--- front/src/iframe_api.ts
+++ front/src/iframe_api.ts
@@ -1,4 +1,4 @@
-import { registeredCallbacks } from "./Api/iframe/registeredCallbacks";
+import { registeredCallbacks } from './Api/iframe/registeredCallbacks';
 import {
     IframeResponseEvent,
     IframeResponseEventMap,
@@ -6,19 +6,19 @@ import {
     isIframeErrorAnswerEvent,
     isIframeResponseEventWrapper,
     TypedMessageEvent,
-} from "./Api/Events/IframeEvent";
-import chat from "./Api/iframe/chat";
-import type { IframeCallback } from "./Api/iframe/IframeApiContribution";
-import nav from "./Api/iframe/nav";
-import controls from "./Api/iframe/controls";
-import ui from "./Api/iframe/ui";
-import sound from "./Api/iframe/sound";
-import room from "./Api/iframe/room";
-import player from "./Api/iframe/player";
-import type { ButtonDescriptor } from "./Api/iframe/Ui/ButtonDescriptor";
-import type { Popup } from "./Api/iframe/Ui/Popup";
-import type { Sound } from "./Api/iframe/Sound/Sound";
-import { answerPromises, sendToWorkadventure } from "./Api/iframe/IframeApiContribution";
+} from './Api/Events/IframeEvent';
+import chat from './Api/iframe/chat';
+import type { IframeCallback } from './Api/iframe/IframeApiContribution';
+import nav from './Api/iframe/nav';
+import controls from './Api/iframe/controls';
+import ui from './Api/iframe/ui';
+import sound from './Api/iframe/sound';
+import room from './Api/iframe/room';
+import player from './Api/iframe/player';
+import type { ButtonDescriptor } from './Api/iframe/Ui/ButtonDescriptor';
+import type { Popup } from './Api/iframe/Ui/Popup';
+import type { Sound } from './Api/iframe/Sound/Sound';
+import { answerPromises, sendToWorkadventure } from './Api/iframe/IframeApiContribution';
 
 const wa = {
     ui,
@@ -36,7 +36,7 @@ const wa = {
      * @deprecated Use WA.chat.sendChatMessage instead
      */
     sendChatMessage(message: string, author: string): void {
-        console.warn("Method WA.sendChatMessage is deprecated. Please use WA.chat.sendChatMessage instead");
+        console.warn('Method WA.sendChatMessage is deprecated. Please use WA.chat.sendChatMessage instead');
         chat.sendChatMessage(message, author);
     },
 
@@ -45,7 +45,7 @@ const wa = {
      */
     disablePlayerControls(): void {
         console.warn(
-            "Method WA.disablePlayerControls is deprecated. Please use WA.controls.disablePlayerControls instead"
+            'Method WA.disablePlayerControls is deprecated. Please use WA.controls.disablePlayerControls instead'
         );
         controls.disablePlayerControls();
     },
@@ -55,7 +55,7 @@ const wa = {
      */
     restorePlayerControls(): void {
         console.warn(
-            "Method WA.restorePlayerControls is deprecated. Please use WA.controls.restorePlayerControls instead"
+            'Method WA.restorePlayerControls is deprecated. Please use WA.controls.restorePlayerControls instead'
         );
         controls.restorePlayerControls();
     },
@@ -64,7 +64,7 @@ const wa = {
      * @deprecated Use WA.ui.displayBubble instead
      */
     displayBubble(): void {
-        console.warn("Method WA.displayBubble is deprecated. Please use WA.ui.displayBubble instead");
+        console.warn('Method WA.displayBubble is deprecated. Please use WA.ui.displayBubble instead');
         ui.displayBubble();
     },
 
@@ -72,7 +72,7 @@ const wa = {
      * @deprecated Use WA.ui.removeBubble instead
      */
     removeBubble(): void {
-        console.warn("Method WA.removeBubble is deprecated. Please use WA.ui.removeBubble instead");
+        console.warn('Method WA.removeBubble is deprecated. Please use WA.ui.removeBubble instead');
         ui.removeBubble();
     },
 
@@ -80,7 +80,7 @@ const wa = {
      * @deprecated Use WA.nav.openTab instead
      */
     openTab(url: string): void {
-        console.warn("Method WA.openTab is deprecated. Please use WA.nav.openTab instead");
+        console.warn('Method WA.openTab is deprecated. Please use WA.nav.openTab instead');
         nav.openTab(url);
     },
 
@@ -88,7 +88,7 @@ const wa = {
      * @deprecated Use WA.sound.loadSound instead
      */
     loadSound(url: string): Sound {
-        console.warn("Method WA.loadSound is deprecated. Please use WA.sound.loadSound instead");
+        console.warn('Method WA.loadSound is deprecated. Please use WA.sound.loadSound instead');
         return sound.loadSound(url);
     },
 
@@ -96,7 +96,7 @@ const wa = {
      * @deprecated Use WA.nav.goToPage instead
      */
     goToPage(url: string): void {
-        console.warn("Method WA.goToPage is deprecated. Please use WA.nav.goToPage instead");
+        console.warn('Method WA.goToPage is deprecated. Please use WA.nav.goToPage instead');
         nav.goToPage(url);
     },
 
@@ -104,15 +104,15 @@ const wa = {
      * @deprecated Use WA.nav.goToRoom instead
      */
     goToRoom(url: string): void {
-        console.warn("Method WA.goToRoom is deprecated. Please use WA.nav.goToRoom instead");
+        console.warn('Method WA.goToRoom is deprecated. Please use WA.nav.goToRoom instead');
         nav.goToRoom(url);
     },
 
     /**
      * @deprecated Use WA.nav.openCoWebSite instead
      */
-    openCoWebSite(url: string, allowApi: boolean = false, allowPolicy: string = ""): void {
-        console.warn("Method WA.openCoWebSite is deprecated. Please use WA.nav.openCoWebSite instead");
+    openCoWebSite(url: string, allowApi: boolean = false, allowPolicy: string = ''): void {
+        console.warn('Method WA.openCoWebSite is deprecated. Please use WA.nav.openCoWebSite instead');
         nav.openCoWebSite(url, allowApi, allowPolicy);
     },
 
@@ -120,7 +120,7 @@ const wa = {
      * @deprecated Use WA.nav.closeCoWebSite instead
      */
     closeCoWebSite(): void {
-        console.warn("Method WA.closeCoWebSite is deprecated. Please use WA.nav.closeCoWebSite instead");
+        console.warn('Method WA.closeCoWebSite is deprecated. Please use WA.nav.closeCoWebSite instead');
         nav.closeCoWebSite();
     },
 
@@ -128,28 +128,28 @@ const wa = {
      * @deprecated Use WA.controls.restorePlayerControls instead
      */
     openPopup(targetObject: string, message: string, buttons: ButtonDescriptor[]): Popup {
-        console.warn("Method WA.openPopup is deprecated. Please use WA.ui.openPopup instead");
+        console.warn('Method WA.openPopup is deprecated. Please use WA.ui.openPopup instead');
         return ui.openPopup(targetObject, message, buttons);
     },
     /**
      * @deprecated Use WA.chat.onChatMessage instead
      */
     onChatMessage(callback: (message: string) => void): void {
-        console.warn("Method WA.onChatMessage is deprecated. Please use WA.chat.onChatMessage instead");
+        console.warn('Method WA.onChatMessage is deprecated. Please use WA.chat.onChatMessage instead');
         chat.onChatMessage(callback);
     },
     /**
      * @deprecated Use WA.room.onEnterZone instead
      */
     onEnterZone(name: string, callback: () => void): void {
-        console.warn("Method WA.onEnterZone is deprecated. Please use WA.room.onEnterZone instead");
+        console.warn('Method WA.onEnterZone is deprecated. Please use WA.room.onEnterZone instead');
         room.onEnterZone(name, callback);
     },
     /**
      * @deprecated Use WA.room.onLeaveZone instead
      */
     onLeaveZone(name: string, callback: () => void): void {
-        console.warn("Method WA.onLeaveZone is deprecated. Please use WA.room.onLeaveZone instead");
+        console.warn('Method WA.onLeaveZone is deprecated. Please use WA.room.onLeaveZone instead');
         room.onLeaveZone(name, callback);
     },
 };
@@ -166,7 +166,7 @@ declare global {
 window.WA = wa;
 
 window.addEventListener(
-    "message",
+    'message',
     <T extends keyof IframeResponseEventMap>(message: TypedMessageEvent<IframeResponseEvent<T>>) => {
         if (message.source !== window.parent) {
             return; // Skip message in this event listener
@@ -181,7 +181,7 @@ window.addEventListener(
 
             const resolver = answerPromises.get(queryId);
             if (resolver === undefined) {
-                throw new Error("In Iframe API, got an answer for a question that we have no track of.");
+                throw new Error('In Iframe API, got an answer for a question that we have no track of.');
             }
             resolver.resolve(payloadData);
 
@@ -192,7 +192,7 @@ window.addEventListener(
 
             const resolver = answerPromises.get(queryId);
             if (resolver === undefined) {
-                throw new Error("In Iframe API, got an error answer for a question that we have no track of.");
+                throw new Error('In Iframe API, got an error answer for a question that we have no track of.');
             }
             resolver.reject(payloadError);
 
