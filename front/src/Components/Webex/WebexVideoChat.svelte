ab76af77953639f63529873a00765774b64cb1d7
Mweya Ruider <mweya.ruider@iits-consulting.de>
2021-09-09 10:37:23 +0200

    Person to call != Person who is calling

--- front/src/Components/Webex/WebexVideoChat.svelte
+++ front/src/Components/Webex/WebexVideoChat.svelte
@@ -7,8 +7,9 @@
 
     let webexCDNLink = "https://unpkg.com/webex/umd/webex.min.js";
 
-    let initials = "TP";
-    let fullName = "Testing Person";
+    let initials = "L";
+    let fullName = "Loading...";
+    let personToCall = "Whiteboard";
     let ready = false
     let currentMeeting = null;
     let webex = null;
@@ -138,12 +139,45 @@
     <h1>WebEx Meeting PoC</h1>
 
     <br/><br/>
-    {#if (!ready)}
-        <h2>Webex loading...</h2>
-    {:else}
-        {#if (currentMeeting === null)}
-            <div class="widget-demo--widgetSpaceComponentContainer--3L80J" style="width: calc( 100% - 30px); height: 100%; right: 0;">
-                <div id="my-ciscospark-space-widget">
+    <div class="widget-demo--widgetSpaceComponentContainer--3L80J"
+         style="width: calc( 100% - 30px); height: 100%; right: 0;">
+        <div id="my-ciscospark-space-widget">
+            {#if (!ready)}
+                <div class="widget-demo--wrapper--2FMs0">
+                    <div class="ciscospark-space-widget md widget-demo--spaceWidget--3h8bX">
+                        <div class="ciscospark-title-bar-wrapper">
+                            <div class="ciscospark-title-bar widget-demo--titleBar--32_8a">
+                                <div class="ciscospark-avatar-container widget-demo--avatarContainer--shGVy">
+                                    <div class="md-avatar md-avatar--24" title="Loading..."><span
+                                            class="md-avatar__letter">L</span></div>
+                                </div>
+                                <div class="ciscospark-title-text widget-demo--titleText--3jwIv"><p><strong
+                                        class="ciscospark-title widget-demo--title--6SJXl">Loading...</strong></p></div>
+                                <div class="ciscospark-title-meta widget-demo--titleMeta--2Sz7g">
+                                    <div class="ciscospark-activity-menu-button-wrapper widget-demo--activityMenuButtonWrapper--3eABx">
+                                        <div class="ciscospark-activity-menu-button widget-demo--activityMenuButton--1FfBJ"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ciscospark-widget-body widget-demo--widgetBody--XshYm">
+                            <div class="ciscospark-meet-wrapper widget-demo--activityComponentWrapper--2OQlx">
+                                <div class="widget-demo--wrapper--2FMs0">
+                                    <div class="webex-teams-loading widget-demo--loading--2kR9l"><img
+                                            src="https://code.s4d.io/widget-demo/archives/0.2.50/logo.png"
+                                            alt="Webex Teams Logo"
+                                            class="webex-teams-spark-logo widget-demo--logo--BY-5z">
+                                        <div class="webex-teams-loading-message widget-demo--loadingMessage--2ZJ8e"></div>
+                                        <div class="webex-teams-spinner-container widget-demo--spinner--2zE8b"><i
+                                                class="md-spinner md-spinner--36 md-spinner--black"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {:else}
+                {#if (currentMeeting === null)}
                     <div class="widget-demo--wrapper--2FMs0">
                         <div class="ciscospark-space-widget md widget-demo--spaceWidget--3h8bX">
                             <div class="ciscospark-title-bar-wrapper">
@@ -169,12 +203,13 @@
                                             <div class="widget-demo--callInactiveContainer--pCOsm call-inactive-container">
                                                 <div class="md-avatar md-avatar--84" title="{fullName}"><span
                                                         class="md-avatar__letter">{initials}</span></div>
-                                                <div class="widget-demo--personName--3fVsV call-person-name">{fullName}
+                                                <div class="widget-demo--personName--3fVsV call-person-name">{personToCall}
                                                 </div>
                                                 <div class="widget-demo--callControls--35xR2 call-controls-container">
                                                     <div class="ciscospark-controls-container widget-demo--controlContainer--1F4XU">
                                                         <div class="md-button__container--small">
-                                                            <button on:click={prevent_default(startCall)} class="md-button md-button--circle md-button--68 md-activity md-activity__camera"
+                                                            <button on:click={prevent_default(startCall)}
+                                                                    class="md-button md-button--circle md-button--68 md-activity md-activity__camera"
                                                                     type="button"
                                                                     aria-label="Start Call" tabindex="0"><span
                                                                     class="md-button__children" style="opacity: 1;"><i
@@ -191,25 +226,83 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        {:else}
 
+                {:else}
+                    {#if true}<!-- TODO Waiting in line?-->
+                        <div style="display: flex">
+                            <video style="width:50%" id="self-view" muted autoplay></video>
+                            <div style="width:50%">
+                                <audio id="remote-view-audio" autoplay></audio>
+                                <video id="remote-view-video" muted autoplay></video>
+                            </div>
+                        </div>
 
+                        <button id="hangup" title="hangup" type="button" on:click={hangup}>cancel/hangup</button>
 
-            <div style="display: flex">
-                <video style="width:50%" id="self-view" muted autoplay></video>
-                <div style="width:50%">
-                    <audio id="remote-view-audio" autoplay></audio>
-                    <video id="remote-view-video" muted autoplay></video>
-                </div>
-            </div>
-
-            <button id="hangup" title="hangup" type="button" on:click={hangup}>cancel/hangup</button>
-        {/if}
-        <br/><br/>
-    {/if}
-
+                    {:else}
+                        <div class="widget-demo--wrapper--2FMs0">
+                            <div class="ciscospark-space-widget md widget-demo--spaceWidget--3h8bX">
+                                <div class="ciscospark-title-bar-wrapper">
+                                    <div class="ciscospark-title-bar widget-demo--titleBar--32_8a">
+                                        <div class="ciscospark-avatar-container widget-demo--avatarContainer--shGVy">
+                                            <div class="md-avatar md-avatar--24" title="{fullName}"><span
+                                                    class="md-avatar__letter">{initials}</span></div>
+                                        </div>
+                                        <div class="ciscospark-title-text widget-demo--titleText--3jwIv"><p><strong
+                                                class="ciscospark-title widget-demo--title--6SJXl">{fullName}</strong>
+                                        </p></div>
+                                        <div class="ciscospark-title-meta widget-demo--titleMeta--2Sz7g">
+                                            <div class="ciscospark-activity-menu-button-wrapper widget-demo--activityMenuButtonWrapper--3eABx">
+                                                <div class="ciscospark-activity-menu-button widget-demo--activityMenuButton--1FfBJ"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ciscospark-widget-body widget-demo--widgetBody--XshYm">
+                                    <div class="ciscospark-meet-wrapper widget-demo--activityComponentWrapper--2OQlx">
+                                        <div class="widget-demo--wrapper--2FMs0">
+                                            <div class="widget-demo--meetWidgetContainer--1fog_ meet-widget-container">
+                                                <div class="widget-demo--callContainer--3K3TZ call-container">
+                                                    <div class="widget-demo--waiting--tZklC participants-waiting">
+                                                        Waiting for others to join...
+                                                    </div>
+                                                    <div class="widget-demo--callControls--2-7gU call-controls">
+                                                        <div class="ciscospark-controls-container widget-demo--controlContainer--1F4XU">
+                                                            <button class="md-button md-button--circle md-button--56 md-button--red md-call-control"
+                                                                    type="button"
+                                                                    aria-label="Toggle Mute Audio" tabindex="0"><span
+                                                                    class="md-button__children" style="opacity: 1;"><i
+                                                                    class="md-icon icon icon-microphone-muted_24"
+                                                                    style="font-size: 24px; color: rgb(255, 255, 255);"></i></span>
+                                                            </button>
+                                                            <button class="md-button md-button--circle md-button--56 md-button--red md-call-control"
+                                                                    type="button"
+                                                                    aria-label="Mute Video" tabindex="0"><span
+                                                                    class="md-button__children" style="opacity: 1;"><i
+                                                                    class="md-icon icon icon-camera-muted_24"
+                                                                    style="font-size: 24px; color: rgb(255, 255, 255);"></i></span>
+                                                            </button>
+                                                            <button class="md-button md-button--circle md-button--56 md-button--red md-call-control"
+                                                                    type="button" aria-label="Hangup"
+                                                                    tabindex="0"><span class="md-button__children"
+                                                                                       style="opacity: 1;"><i
+                                                                    class="md-icon icon icon-cancel_24"
+                                                                    style="font-size: 24px; color: rgb(255, 255, 255);"></i></span>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    {/if}
+                {/if}
+            {/if}
+        </div>
+    </div>
 
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
