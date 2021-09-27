ad5489c102c2849e3001bbd06a0869a5f6c7bf9f
David Négrier <d.negrier@thecodingmachine.com>
2021-09-27 11:32:57 +0200

    Adding edit URLs to allow people to directly propose changes

--- docs/maps/menu.php
+++ docs/maps/menu.php
@@ -6,52 +6,62 @@ return [
     [
         'title' => 'Getting started',
         'url' => '/map-building/',
-        'markdown' => 'maps.index'
+        'markdown' => 'maps.index',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/index.md',
     ],
     [
         'title' => 'WorkAdventure maps',
         'url' => '/map-building/wa-maps.md',
-        'markdown' => 'maps.wa-maps'
+        'markdown' => 'maps.wa-maps',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/wa-maps.md',
     ],
     [
         'title' => 'Entries and exits',
         'url' => '/map-building/entry-exit.md',
-        'markdown' => 'maps.entry-exit'
+        'markdown' => 'maps.entry-exit',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/entry-exit.md',
     ],
     [
         'title' => 'Opening a website',
         'url' => '/map-building/opening-a-website.md',
-        'markdown' => 'maps.opening-a-website'
+        'markdown' => 'maps.opening-a-website',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/opening-a-website.md',
     ],
     [
         'title' => 'Meeting rooms',
         'url' => '/map-building/meeting-rooms.md',
-        'markdown' => 'maps.meeting-rooms'
+        'markdown' => 'maps.meeting-rooms',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/meeting-rooms.md',
     ],
     [
         'title' => 'Special zones',
         'url' => '/map-building/special-zones.md',
-        'markdown' => 'maps.special-zones'
+        'markdown' => 'maps.special-zones',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/special-zones.md',
     ],
     [
         'title' => 'Animations',
         'url' => '/map-building/animations.md',
-        'markdown' => 'maps.animations'
+        'markdown' => 'maps.animations',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/animations.md',
     ],
     [
         'title' => 'Integrated websites',
         'url' => '/map-building/website-in-map.md',
-        'markdown' => 'maps.website-in-map'
+        'markdown' => 'maps.website-in-map',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/website-in-map.md',
     ],
     [
         'title' => 'Variables',
         'url' => '/map-building/variables.md',
-        'markdown' => 'maps.variables'
+        'markdown' => 'maps.variables',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/variables.md',
     ],
     [
         'title' => 'Self-hosting your map',
         'url' => '/map-building/hosting.md',
-        'markdown' => 'maps.hosting'
+        'markdown' => 'maps.hosting',
+        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/hosting.md',
     ],
     $extraMenu,
     [
@@ -62,7 +72,8 @@ return [
             [
                 'title' => 'Using Typescript',
                 'url' => '/map-building/using-typescript.md',
-                'markdown' => 'maps.using-typescript'
+                'markdown' => 'maps.using-typescript',
+                'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/using-typescript.md',
             ],
             [
                 'title' => 'API Reference',
@@ -74,51 +85,61 @@ return [
                         'title' => 'Initialization',
                         'url' => '/map-building/api-start.md',
                         'markdown' => 'maps.api-start',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-start.md',
                     ],
                     [
                         'title' => 'Navigation',
                         'url' => '/map-building/api-nav.md',
                         'markdown' => 'maps.api-nav',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-nav.md',
                     ],
                     [
                         'title' => 'Chat',
                         'url' => '/map-building/api-chat.md',
                         'markdown' => 'maps.api-chat',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-chat.md',
                     ],
                     [
                         'title' => 'Room',
                         'url' => '/map-building/api-room.md',
                         'markdown' => 'maps.api-room',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-room.md',
                     ],
                     [
                         'title' => 'State',
                         'url' => '/map-building/api-state.md',
                         'markdown' => 'maps.api-state',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-state.md',
                     ],
                     [
                         'title' => 'Player',
                         'url' => '/map-building/api-player.md',
                         'markdown' => 'maps.api-player',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-player.md',
                     ],
                     [
                         'title' => 'UI',
                         'url' => '/map-building/api-ui.md',
                         'markdown' => 'maps.api-ui',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-ui.md',
                     ],
                     [
                         'title' => 'Sound',
                         'url' => '/map-building/api-sound.md',
                         'markdown' => 'maps.api-sound',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-sound.md',
                     ],
                     [
                         'title' => 'Controls',
                         'url' => '/map-building/api-controls.md',
                         'markdown' => 'maps.api-controls',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-controls.md',
                     ],
                     [
                         'title' => 'Deprecated',
                         'url' => '/map-building/api-deprecated.md',
                         'markdown' => 'maps.api-deprecated',
+                        'editUrl' => 'https://github.com/thecodingmachine/workadventure/edit/develop/docs/maps/api-deprecated.md',
                     ],
                 ]
             ],
