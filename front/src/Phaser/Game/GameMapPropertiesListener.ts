1ee0b28f66ac251a339fa7617e8848bdf69d4913
Alexis Faizeau <a.faizeau@workadventu.re>
2021-10-07 14:40:02 +0200

    Fix co website closing on exit zone

--- front/src/Phaser/Game/GameMapPropertiesListener.ts
+++ front/src/Phaser/Game/GameMapPropertiesListener.ts
@@ -37,37 +37,44 @@ export class GameMapPropertiesListener {
             }
         });
         this.gameMap.onPropertyChange("openWebsite", (newValue, oldValue, allProps) => {
-            if (newValue === undefined) {
-                layoutManagerActionStore.removeAction("openWebsite");
-                coWebsiteManager.closeCoWebsite();
-            } else {
-                const openWebsiteFunction = () => {
-                    coWebsiteManager.loadCoWebsite(
-                        newValue as string,
-                        this.scene.MapUrlFile,
-                        allProps.get("openWebsiteAllowApi") as boolean | undefined,
-                        allProps.get("openWebsitePolicy") as string | undefined,
-                        allProps.get("openWebsiteWidth") as number | undefined
-                    );
+            const handler = async () => {
+                if (newValue === undefined || newValue !== oldValue) {
                     layoutManagerActionStore.removeAction("openWebsite");
-                };
-                const openWebsiteTriggerValue = allProps.get(TRIGGER_WEBSITE_PROPERTIES);
-                if (openWebsiteTriggerValue && openWebsiteTriggerValue === ON_ACTION_TRIGGER_BUTTON) {
-                    let message = allProps.get(WEBSITE_MESSAGE_PROPERTIES);
-                    if (message === undefined) {
-                        message = "Press SPACE or touch here to open web site";
-                    }
-                    layoutManagerActionStore.addAction({
-                        uuid: "openWebsite",
-                        type: "message",
-                        message: message,
-                        callback: () => openWebsiteFunction(),
-                        userInputManager: this.scene.userInputManager,
-                    });
-                } else {
-                    openWebsiteFunction();
+                    await coWebsiteManager.closeCoWebsites();
                 }
-            }
+
+                if (newValue !== undefined) {
+                    const openWebsiteFunction = () => {
+                        coWebsiteManager.loadCoWebsite(
+                            newValue as string,
+                            this.scene.MapUrlFile,
+                            allProps.get("openWebsiteAllowApi") as boolean | undefined,
+                            allProps.get("openWebsitePolicy") as string | undefined,
+                            allProps.get("openWebsiteWidth") as number | undefined
+                        );
+
+                        layoutManagerActionStore.removeAction("openWebsite");
+                    };
+                    const openWebsiteTriggerValue = allProps.get(TRIGGER_WEBSITE_PROPERTIES);
+                    if (openWebsiteTriggerValue && openWebsiteTriggerValue === ON_ACTION_TRIGGER_BUTTON) {
+                        let message = allProps.get(WEBSITE_MESSAGE_PROPERTIES);
+                        if (message === undefined) {
+                            message = "Press SPACE or touch here to open web site";
+                        }
+                        layoutManagerActionStore.addAction({
+                            uuid: "openWebsite",
+                            type: "message",
+                            message: message,
+                            callback: () => openWebsiteFunction(),
+                            userInputManager: this.scene.userInputManager,
+                        });
+                    } else {
+                        openWebsiteFunction();
+                    }
+                }
+            };
+
+            handler();
         });
     }
 }
