d1122d75ecf6998c71b5560c57c5c9ff9961191b
David Négrier <d.negrier@thecodingmachine.com>
2021-11-24 16:59:22 +0100

    Fixing CI

--- back/src/Services/Logger.ts
+++ back/src/Services/Logger.ts
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
-                winston.format.colorize(),
+                //winston.format.colorize(),
                 winston.format.timestamp(),
                 winston.format.align(),
                 winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
