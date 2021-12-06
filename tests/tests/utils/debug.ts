f6803b6d5d74b7af0f93a52b136215260cc0e0fb
David Négrier <d.negrier@thecodingmachine.com>
2021-12-06 15:38:30 +0100

    Fixing pusher and back url

--- tests/tests/utils/debug.ts
+++ tests/tests/utils/debug.ts
@@ -5,7 +5,7 @@ export async function getPusherDump(): Promise<any> {
     let url = 'http://pusher.workadventure.localhost/dump?token=123';
     if (fs.existsSync('/project')) {
         // We are inside a container. Let's use a direct route
-        url = 'http://pusher/dump?token=123';
+        url = 'http://pusher:8080/dump?token=123';
     }
 
     return (await axios({
@@ -19,7 +19,7 @@ export async function getBackDump(): Promise<any> {
     let url = 'http://api.workadventure.localhost/dump?token=123';
     if (fs.existsSync('/project')) {
         // We are inside a container. Let's use a direct route
-        url = 'http://back/dump?token=123';
+        url = 'http://back:8080/dump?token=123';
     }
 
     return (await axios({
