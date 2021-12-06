15565c9a29862f69fb2d3b51f67057b976ed4203
David Négrier <d.negrier@thecodingmachine.com>
2021-12-06 15:29:24 +0100

    Changing pusher / back URL depending on environment

--- tests/tests/utils/debug.ts
+++ tests/tests/utils/debug.ts
@@ -1,16 +1,29 @@
 import axios from "axios";
+const fs = require('fs');
 
 export async function getPusherDump(): Promise<any> {
+    let url = 'http://pusher.workadventure.localhost/dump?token=123';
+    if (fs.existsSync('/project')) {
+        // We are inside a container. Let's use a direct route
+        url = 'http://pusher/dump?token=123';
+    }
+
     return (await axios({
-        url: 'http://pusher.workadventure.localhost/dump?token=123',
+        url,
         method: 'get',
     })).data;
 }
 
 
 export async function getBackDump(): Promise<any> {
+    let url = 'http://api.workadventure.localhost/dump?token=123';
+    if (fs.existsSync('/project')) {
+        // We are inside a container. Let's use a direct route
+        url = 'http://back/dump?token=123';
+    }
+
     return (await axios({
-        url: 'http://api.workadventure.localhost/dump?token=123',
+        url,
         method: 'get',
     })).data;
 }
