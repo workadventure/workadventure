331daca854389fcd55e7ee4e3dfa35b8ec5b4b41
David Négrier <d.negrier@thecodingmachine.com>
2021-12-06 16:37:27 +0100

    Fixing admin token for requests

--- tests/tests/utils/debug.ts
+++ tests/tests/utils/debug.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 const fs = require('fs');
 
+const ADMIN_API_TOKEN = process.env.ADMIN_API_TOKEN;
+
 export async function getPusherDump(): Promise<any> {
-    let url = 'http://pusher.workadventure.localhost/dump?token=123';
+    let url = 'http://pusher.workadventure.localhost/dump?token='+ADMIN_API_TOKEN;
     if (fs.existsSync('/project')) {
         // We are inside a container. Let's use a direct route
-        url = 'http://pusher:8080/dump?token=123';
+        url = 'http://pusher:8080/dump?token='+ADMIN_API_TOKEN;
     }
 
     return (await axios({
@@ -14,12 +16,11 @@ export async function getPusherDump(): Promise<any> {
     })).data;
 }
 
-
 export async function getBackDump(): Promise<any> {
-    let url = 'http://api.workadventure.localhost/dump?token=123';
+    let url = 'http://api.workadventure.localhost/dump?token='+ADMIN_API_TOKEN;
     if (fs.existsSync('/project')) {
         // We are inside a container. Let's use a direct route
-        url = 'http://back:8080/dump?token=123';
+        url = 'http://back:8080/dump?token='+ADMIN_API_TOKEN;
     }
 
     return (await axios({
