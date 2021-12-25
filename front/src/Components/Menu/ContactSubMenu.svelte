f08999db3436e2852f39c9aada141f0f7aa712d2
tabascoeye <tabascoeye@gmail.com>
2021-12-25 23:23:21 +0000

    Remove workadventure texts from contact sub page

--- front/src/Components/Menu/ContactSubMenu.svelte
+++ front/src/Components/Menu/ContactSubMenu.svelte
@@ -1,36 +1,9 @@
 <script lang="ts">
-    import LL from "../../i18n/i18n-svelte";
     import { contactPageStore } from "../../Stores/MenuStore";
-
-    function goToGettingStarted() {
-        const sparkHost = "https://workadventu.re/getting-started";
-        window.open(sparkHost, "_blank");
-    }
-
-    function goToBuildingMap() {
-        const sparkHost = "https://workadventu.re/map-building/";
-        window.open(sparkHost, "_blank");
-    }
 </script>
 
 <div class="create-map-main">
     <section class="container-overflow">
-        <section>
-            <h3>{$LL.menu.contact.gettingStarted.title()}</h3>
-            <p>{$LL.menu.contact.gettingStarted.description()}</p>
-            <button type="button" class="nes-btn is-primary" on:click={goToGettingStarted}
-                >{$LL.menu.contact.gettingStarted.title()}</button
-            >
-        </section>
-
-        <section>
-            <h3>{$LL.menu.contact.createMap.title()}</h3>
-            <p>{$LL.menu.contact.createMap.description()}</p>
-            <button type="button" class="nes-btn" on:click={goToBuildingMap}
-                >{$LL.menu.contact.createMap.title()}</button
-            >
-        </section>
-
         <iframe
             title="contact"
             src={$contactPageStore}
