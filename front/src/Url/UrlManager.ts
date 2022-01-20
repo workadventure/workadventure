cb0c5beecfde3c50c96acc7256f0d5385dfd7de1
Hanusiak Piotr <piotr@ltmp.co>
2022-01-20 14:14:24 +0100

    get additional hash parameters from url

--- front/src/Url/UrlManager.ts
+++ front/src/Url/UrlManager.ts
@@ -49,6 +49,14 @@ class UrlManager {
         return hash.length > 1 ? hash.substring(1) : null;
     }
 
+    public getHashParameters(): Record<string, string> {
+        return window.location.hash.split("&").reduce((res: Record<string, string>, item: string) => {
+            const parts = item.split("=");
+            res[parts[0]] = parts[1];
+            return res;
+        }, {});
+    }
+
     pushStartLayerNameToUrl(startLayerName: string): void {
         window.location.hash = startLayerName;
     }
