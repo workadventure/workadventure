cdc0646bb01e4a0b59dab048a9fe79850b89422e
Alexis Faizeau <faizeau.alexis@gmail.com>
2022-01-28 15:56:38 +0100

    Fix actions fr-FR translations

--- front/src/i18n/fr-FR/message.ts
+++ front/src/i18n/fr-FR/message.ts
@@ -1,9 +1,9 @@
 import type { Translation } from "../i18n-types";
 
 const message: NonNullable<Translation["message"]> = {
-    openWebsiteTrigger: "Appuyez sur ESPACE ou touchez ici pour ouvrir le site Web",
-    openWebsiteTabTrigger: "Appuyez sur ESPACE ou touchez ici pour ouvrir le site Web dans un nouvel onglet",
-    openJitsiTrigger: "Appuyez sur ESPACE ou touchez ici pour entrer dans la salle Jitsi Meet",
+    openWebsiteTrigger: "Appuyez sur la touche ESPACE ou touchez moi pour ouvrir le site web",
+    openWebsiteTabTrigger: "Appuyez sur la touche ESPACE ou touchez moi pour ouvrir le site web dans un nouvel onglet",
+    openJitsiTrigger: "Appuyez sur la touche ESPACE ou touchez moi pour entrer dans la salle de conférence Jitsi",
 };
 
 export default message;
