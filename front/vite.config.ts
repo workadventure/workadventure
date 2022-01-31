b83fc7718396b90fb42bde989e2825a51d805217
Lukas Hass <lukas@slucky.de>
2022-01-31 16:56:50 +0100

    use vite plugin for env-config generation

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,38 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { envConfig } from "@geprog/vite-plugin-env-config";
 
 export default defineConfig({
-    plugins: [svelte({})],
+    plugins: [
+        svelte({}),
+        envConfig({
+            variables: [
+                "SKIP_RENDER_OPTIMIZATIONS",
+                "DISABLE_NOTIFICATIONS",
+                "PUSHER_URL",
+                "UPLOADER_URL",
+                "ADMIN_URL",
+                "CONTACT_URL",
+                "PROFILE_URL",
+                "ICON_URL",
+                "DEBUG_MODE",
+                "STUN_SERVER",
+                "TURN_SERVER",
+                "TURN_USER",
+                "TURN_PASSWORD",
+                "JITSI_URL",
+                "JITSI_PRIVATE_MODE",
+                "START_ROOM_URL",
+                "MAX_USERNAME_LENGTH",
+                "MAX_PER_GROUP",
+                "DISPLAY_TERMS_OF_USE",
+                "POSTHOG_API_KEY",
+                "POSTHOG_URL",
+                "NODE_ENV",
+                "DISABLE_ANONYMOUS",
+                "OPID_LOGIN_SCREEN_PROVIDER",
+                "FALLBACK_LOCALE",
+            ],
+        }),
+    ],
 });
