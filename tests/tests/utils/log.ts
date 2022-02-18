cf6204f0dbd2cd70eddccfdbbfda5c1864eddca9
Lukas Hass <lukas@slucky.de>
2022-02-18 15:41:02 +0100

    increase log retrieval timeout

--- tests/tests/utils/log.ts
+++ tests/tests/utils/log.ts
@@ -1,20 +1,26 @@
 import { expect } from '@playwright/test';
 
+const POLLING_INTERVAL = 50;
+
 /**
  * Tries to find a given log message in the logs (for 10 seconds)
  */
-export async function assertLogMessage(page, substring): Promise<void> {
+export async function assertLogMessage(
+  page,
+  substring: string,
+  timeout: number = 10000
+): Promise<void> {
   let logs = [];
   await page.on('console', async (msg) => {
     logs.push(await msg.text());
   });
 
   // wait for log to appear
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < timeout / POLLING_INTERVAL; i++) {
     if (logs.includes(substring)) {
       break;
     }
-    await page.waitForTimeout(50);
+    await page.waitForTimeout(POLLING_INTERVAL);
   }
 
   expect(logs).toContain(substring);
