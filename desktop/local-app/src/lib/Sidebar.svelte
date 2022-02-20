6844e0d39d623d1186382c40616319cce4e5ca89
Anton Bracke <anton@ju60.de>
2022-02-20 21:51:55 +0100

    improve style

--- desktop/local-app/src/lib/Sidebar.svelte
+++ desktop/local-app/src/lib/Sidebar.svelte
@@ -5,6 +5,12 @@
 
     let isDevelopment = false;
 
+    function getServerColor(i: number) {
+        const serverColors = ['bg-red-400', 'bg-yellow-500', 'bg-green-500', 'bg-blue-500', 'bg-indigo-500', 'bg-purple-500'];
+
+        return serverColors[i % serverColors.length];
+    }
+
     onMount(async () => {
         await loadServers();
         isDevelopment = await window?.WorkAdventureDesktopApi?.isDevelopment();
@@ -12,16 +18,16 @@
 </script>
 
 <aside class="flex flex-col bg-gray-700 items-center">
-    <div class="flex flex-col mt-4 space-y-4">
-        {#each $servers as server}
+    <div class="flex flex-col mt-4 space-y-4 overflow-y-auto pb-4">
+        {#each $servers as server, i}
             <div class="flex flex-col items-center justify-center text-gray-400">
                 <div
-                    class={`w-12 h-12 rounded-lg flex cursor-pointer text-center items-center justify-center bg-yellow-500 text-light-50 ${
+                    class={`w-12 h-12 rounded-lg flex cursor-pointer text-center items-center justify-center text-light-50 ${getServerColor(i)} ${
                         $selectedServer === server._id ? "" : ""
                     }`}
                     on:click={() => selectServer(server)}
                 >
-                    {server.name.slice(0, 2)}
+                    {server.name.slice(0, 2).toLocaleUpperCase()}
                 </div>
             </div>
         {/each}
