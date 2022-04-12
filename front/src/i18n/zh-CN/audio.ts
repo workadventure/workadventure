9973c68ee724077c0d38b5cd794afe84891b7fb3
Zachary Jia <jia199474@gmail.com>
2022-04-12 03:59:12 +0000

    add chinese translation

---
+++ front/src/i18n/zh-CN/audio.ts
@@ -0,0 +1,11 @@
+import type { BaseTranslation } from "../i18n-types";
+
+const audio: BaseTranslation = {
+    manager: {
+        reduce: "说话时降低播放器音量",
+        allow: "允许语音",
+    },
+    message: "音频消息",
+};
+
+export default audio;
---
+++ front/src/i18n/zh-CN/chat.ts
@@ -0,0 +1,12 @@
+import type { BaseTranslation } from "../i18n-types";
+
+const chat: BaseTranslation = {
+    intro: "聊天历史:",
+    enter: "输入消息...",
+    menu: {
+        visitCard: "Visit card",
+        addFriend: "添加朋友",
+    },
+};
+
+export default chat;
