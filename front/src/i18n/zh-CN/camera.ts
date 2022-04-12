08e843549c549f27215fb7abdf3bac5b8b4ddc84
Zachary Jia <jia199474@gmail.com>
2022-04-12 21:19:14 +0800

    Add chinese translation (#2074)
    * add chinese translation
    * fix prettier problem

---
+++ front/src/i18n/zh-CN/camera.ts
@@ -0,0 +1,25 @@
+import type { BaseTranslation } from "../i18n-types";
+
+const camera: BaseTranslation = {
+    enable: {
+        title: "开启你的摄像头和麦克风",
+        start: "出发!",
+    },
+    help: {
+        title: "需要摄像头/麦克风权限",
+        permissionDenied: "拒绝访问",
+        content: "你必须在浏览器设置里允许摄像头和麦克风访问权限。",
+        firefoxContent: '如果你不希望Firefox反复要求授权，请选中"记住此决定"。',
+        refresh: "刷新",
+        continue: "不使用摄像头继续游戏",
+        screen: {
+            firefox: "/resources/help-setting-camera-permission/en-US-firefox.png",
+            chrome: "/resources/help-setting-camera-permission/en-US-firefox.png",
+        },
+    },
+    my: {
+        silentZone: "安静区",
+    },
+};
+
+export default camera;
