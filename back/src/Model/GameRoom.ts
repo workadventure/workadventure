a8d56263ea0f1538767725ee2b67038f72eb22ef
Piotr 'pwh' Hanusiak <p.hanusiak@workadventu.re>
2022-04-20 14:32:23 +0200

    leave group immediately if status changed to silent | jitsi

--- back/src/Model/GameRoom.ts
+++ back/src/Model/GameRoom.ts
@@ -206,7 +206,11 @@ export class GameRoom {
     }
 
     updatePlayerDetails(user: User, playerDetailsMessage: SetPlayerDetailsMessage) {
+        const isSilent = user.silent;
         user.updateDetails(playerDetailsMessage);
+        if (!isSilent && user.group !== undefined && user.silent) {
+            this.leaveGroup(user);
+        }
     }
 
     private updateUserGroup(user: User): void {
