e11d3eb034d48425123c5918d953fec3bb7d1b9f
Benedicte Quimbert <b.quimbert@workadventu.re>
2022-04-25 16:20:02 +0200

    Initial commit

--- front/src/Components/Menu/TestSubMenu.svelte
+++ front/src/Components/Menu/TestSubMenu.svelte
@@ -1,50 +1,41 @@
-<div class="tw-container tw-bg-darker">
-  <h1>
-    Basic color & classes tests
-  </h1>
-  <div class="tw-flex tw-flex-col tw-space-y-1">
-    <a href="#" class="btn pop-blue">
-      Test btn
-    </a>
-    <a href="#" class="btn pop-yellow">
-      Test btn
-    </a>
-    <a href="#" class="btn pop-red">
-      Test btn
-    </a>
-    <a href="#" class="btn pop-green">
-      Test btn
-    </a>
-    <p class="tw-text-pop-blue-500">
-      Text test
-    </p>
-    <p class="tw-text-pop-yellow">
-      Text test
-    </p>
-    <p class="tw-text-pop-red">
-      Text test
-    </p>
-    <p class="tw-text-pop-green">
-      Text test
-    </p>
-  </div>
-  <h1>
-    Form tests
-  </h1>
-  <form>
-    <div>
-      <label for="text_input">Text input</label>
-      <input type="text" name="text_input" id="text_input" placeholder="a placeholder">
+<div class="tw-container tw-bg-dark-purple">
+
+  <h2 class="tw-justify-start">
+    Audio, video & graphics
+  </h2>
+  <form class="tw-flex tw-flex-col">
+    <div class="tw-flex form-select tw-w-full">
+      <label for="select_input" class="subtitle">Select input</label>
+      <select name="select_input" id="select_input" class="tw-rounded-full tw-w-5/6">
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+        <option value="4">Option 4</option>
+      </select>
     </div>
-    <div>
-      <label for="text_input_bis">Text input bis</label>
-      <input type="text" name="text_input" id="text_input_bis" value="ladida !">
-    </div>
-    <div class="tw-toggle-switch">
-      <label for="cb_input"><input type="checkbox" id="cb_input" name="cb_input" value="cb_input">A checkbox<span>test</span></label>
+    <div class="tw-flex form-checkbox tw-w-full">
+      <h3 class="subtitle">Checkboxes</h3>
+      <div class="tw-flew-row checkbox-group">
+        <label for="checkbox_input">A checkbox<input type="checkbox" id="checkbox_input" name="checkbox_input">
+        </label>
+      </div>
     </div>
   </form>
+  <h3 class="subtitle">Buttons</h3>
+  <div class="tw-bg-light-purple tw-py-2 tw-px-2">
+    <button type="button" class="btn">A button</button>
+    <button type="button" class="btn light">A button</button>
+    <button type="button" class="btn dark">A button</button>
+    <button type="button" class="btn outline">A button</button>
+  </div>
+  <h2 class="tw-justify-start">
+    Away-mode
+  </h2>
+  <h2 class="tw-justify-start">
+    Miscellaneous
+  </h2>
 </div>
 <script>
+  import "../../../style/breakpoints.scss";
   import "../../../style/tailwind.scss";
 </script>
