90121e997561bd3f18ce270538554c96cdd885a5
Benedicte Quimbert <b.quimbert@workadventu.re>
2022-04-27 17:35:35 +0200

    Adds tailwind/forms and refactors theming

--- front/src/Components/Menu/TestSubMenu.svelte
+++ front/src/Components/Menu/TestSubMenu.svelte
@@ -1,39 +1,38 @@
-<div class="tw-container tw-bg-dark-purple">
+<div class="tw-container">
 
-  <h2 class="tw-justify-start">
-    Audio, video & graphics
+  <h2 class="tw-justify-start small-blue-title">
+    Buttons
   </h2>
-  <form class="tw-flex tw-flex-col">
-    <div class="tw-flex form-select tw-w-full">
-      <label for="select_input" class="subtitle">Select input</label>
-      <select name="select_input" id="select_input" class="tw-rounded-full tw-w-5/6">
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-        <option value="4">Option 4</option>
-      </select>
-    </div>
-    <div class="tw-flex form-checkbox tw-w-full">
-      <h3 class="subtitle">Checkboxes</h3>
-      <div class="tw-flew-row checkbox-group">
-        <label for="checkbox_input">A checkbox<input type="checkbox" id="checkbox_input" name="checkbox_input">
-        </label>
-      </div>
-    </div>
-  </form>
-  <h3 class="subtitle">Buttons</h3>
   <div class="tw-bg-light-purple tw-py-2 tw-px-2">
-    <button type="button" class="btn">A button</button>
-    <button type="button" class="btn light">A button</button>
-    <button type="button" class="btn dark">A button</button>
-    <button type="button" class="btn outline">A button</button>
+    <button type="button">Simple btn</button>
+    <button type="button" class="light">Light btn</button>
+    <button type="button" class="dark">Dark btn</button>
+    <button type="button" class="outline">Outlined btn</button>
   </div>
-  <h2 class="tw-justify-start">
-    Away-mode
-  </h2>
-  <h2 class="tw-justify-start">
-    Miscellaneous
-  </h2>
+
+  <!--- Here starts real theming stuff -->
+  <h2 class="small-blue-title">Form stuff</h2>
+  <p>Selects</p>
+  <select name="test-select" id="test-select" class="tw-w-full">
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+  </select>
+
+  <p>Inputs</p>
+  <input type="email" class="tw-w-full" placeholder="My placeholder lol"/>
+
+  <p>Checkboxes</p>
+  <label for="real_checkbox" class="">A checkbox
+    <input type="checkbox" id="real_checkbox">
+  </label>
+  <label for="real_checkbox2" class="">A checkbox
+    <input type="checkbox" id="real_checkbox2">
+  </label>
+  <p>Radios</p>
+  <label for="real_checkbox3" class="">A radio button
+    <input type="radio" id="real_checkbox3">
+  </label>
 </div>
 <script>
   import "../../../style/breakpoints.scss";
