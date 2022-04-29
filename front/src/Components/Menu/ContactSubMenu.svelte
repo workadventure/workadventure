f1f953913c13b07f7f5c5d0f8b86db6ac0eb591c
Benedicte Quimbert <b.quimbert@workadventu.re>
2022-04-29 10:55:42 +0200

    Reworks Guest sub menu

--- front/src/Components/Menu/ContactSubMenu.svelte
+++ front/src/Components/Menu/ContactSubMenu.svelte
@@ -1,67 +1,47 @@
 <script lang="ts">
-    import LL from "../../i18n/i18n-svelte";
-    import { contactPageStore } from "../../Stores/MenuStore";
+  import LL from "../../i18n/i18n-svelte";
+  import { contactPageStore } from "../../Stores/MenuStore";
 
-    function goToGettingStarted() {
-        const sparkHost = "https://workadventu.re/getting-started";
-        window.open(sparkHost, "_blank");
-    }
+  function goToGettingStarted() {
+    const sparkHost = "https://workadventu.re/getting-started";
+    window.open(sparkHost, "_blank");
+  }
 
-    function goToBuildingMap() {
-        const sparkHost = "https://workadventu.re/map-building/";
-        window.open(sparkHost, "_blank");
-    }
+  function goToBuildingMap() {
+    const sparkHost = "https://workadventu.re/map-building/";
+    window.open(sparkHost, "_blank");
+  }
 </script>
 
-<div class="create-map-main">
-    <section class="container-overflow">
-        <section>
-            <h3>{$LL.menu.contact.gettingStarted.title()}</h3>
-            <p>{$LL.menu.contact.gettingStarted.description()}</p>
-            <button type="button" class="nes-btn is-primary" on:click={goToGettingStarted}
-                >{$LL.menu.contact.gettingStarted.title()}</button
-            >
-        </section>
-
-        <section>
-            <h3>{$LL.menu.contact.createMap.title()}</h3>
-            <p>{$LL.menu.contact.createMap.description()}</p>
-            <button type="button" class="nes-btn" on:click={goToBuildingMap}
-                >{$LL.menu.contact.createMap.title()}</button
-            >
-        </section>
-
-        <iframe
-            title="contact"
-            src={$contactPageStore}
-            allow="clipboard-read; clipboard-write self {$contactPageStore}"
-            allowfullscreen
-        />
+<div>
+  <section>
+    <section>
+      <h3 class="blue-title">{$LL.menu.contact.gettingStarted.title()}</h3>
+      <p>{$LL.menu.contact.gettingStarted.description()}</p>
+      <div class="centered-column">
+        <button type="button" class="light" on:click={goToGettingStarted}
+        >{$LL.menu.contact.gettingStarted.title()}</button
+        >
+      </div>
     </section>
+
+    <section>
+      <h3 class="blue-title">{$LL.menu.contact.createMap.title()}</h3>
+      <p>{$LL.menu.contact.createMap.description()}</p>
+      <div class="centered-column">
+        <button type="button" class="light" on:click={goToBuildingMap}
+        >{$LL.menu.contact.createMap.title()}</button
+        >
+      </div>
+    </section>
+
+    <iframe
+      title="contact"
+      src={$contactPageStore}
+      allow="clipboard-read; clipboard-write self {$contactPageStore}"
+      allowfullscreen
+      class="tw-border-0 tw-w-full tw-h-screen"
+    />
+  </section>
 </div>
 
-<style lang="scss">
-    div.create-map-main {
-        height: calc(100% - 56px);
-
-        text-align: center;
-
-        section {
-            margin-bottom: 50px;
-        }
-
-        section.container-overflow {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            overflow: auto;
-        }
-    }
-
-    iframe {
-        border: none;
-        height: calc(100% - 56px);
-        width: 100%;
-        margin: 0;
-    }
-</style>
