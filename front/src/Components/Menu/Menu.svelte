aa5e4c8d6ceb25162a442ff19f0b627821cea4d6
Benedicte Quimbert <b.quimbert@workadventu.re>
2022-05-03 18:04:46 +0200

    Prettier

--- front/src/Components/Menu/Menu.svelte
+++ front/src/Components/Menu/Menu.svelte
@@ -1,136 +1,135 @@
 <script lang="ts">
-  import { fly } from "svelte/transition";
-  import SettingsSubMenu from "./SettingsSubMenu.svelte";
-  import ProfileSubMenu from "./ProfileSubMenu.svelte";
-  import AboutRoomSubMenu from "./AboutRoomSubMenu.svelte";
-  import GlobalMessageSubMenu from "./GlobalMessagesSubMenu.svelte";
-  import ContactSubMenu from "./ContactSubMenu.svelte";
-  import CustomSubMenu from "./CustomSubMenu.svelte";
-  import GuestSubMenu from "./GuestSubMenu.svelte";
-  import chevronImg from "../images/chevron.svg";
-  import "../../../style/tailwind.scss";
+    import { fly } from "svelte/transition";
+    import SettingsSubMenu from "./SettingsSubMenu.svelte";
+    import ProfileSubMenu from "./ProfileSubMenu.svelte";
+    import AboutRoomSubMenu from "./AboutRoomSubMenu.svelte";
+    import GlobalMessageSubMenu from "./GlobalMessagesSubMenu.svelte";
+    import ContactSubMenu from "./ContactSubMenu.svelte";
+    import CustomSubMenu from "./CustomSubMenu.svelte";
+    import GuestSubMenu from "./GuestSubMenu.svelte";
+    import chevronImg from "../images/chevron.svg";
+    import "../../../style/tailwind.scss";
 
-  import {
-    checkSubMenuToShow,
-    customMenuIframe,
-    menuVisiblilityStore,
-    SubMenusInterface,
-    subMenusStore
-  } from "../../Stores/MenuStore";
-  import type { MenuItem } from "../../Stores/MenuStore";
-  import { onDestroy, onMount } from "svelte";
-  import type { Unsubscriber } from "svelte/store";
-  import { sendMenuClickedEvent } from "../../Api/iframe/Ui/MenuItem";
-  import LL from "../../i18n/i18n-svelte";
+    import {
+        checkSubMenuToShow,
+        customMenuIframe,
+        menuVisiblilityStore,
+        SubMenusInterface,
+        subMenusStore,
+    } from "../../Stores/MenuStore";
+    import type { MenuItem } from "../../Stores/MenuStore";
+    import { onDestroy, onMount } from "svelte";
+    import type { Unsubscriber } from "svelte/store";
+    import { sendMenuClickedEvent } from "../../Api/iframe/Ui/MenuItem";
+    import LL from "../../i18n/i18n-svelte";
 
-  let activeSubMenu: MenuItem = $subMenusStore[0];
-  let activeComponent: typeof ProfileSubMenu | typeof CustomSubMenu = ProfileSubMenu;
-  let props: { url: string; allowApi: boolean };
-  let unsubscriberSubMenuStore: Unsubscriber;
+    let activeSubMenu: MenuItem = $subMenusStore[0];
+    let activeComponent: typeof ProfileSubMenu | typeof CustomSubMenu = ProfileSubMenu;
+    let props: { url: string; allowApi: boolean };
+    let unsubscriberSubMenuStore: Unsubscriber;
 
-  onMount(() => {
-    unsubscriberSubMenuStore = subMenusStore.subscribe(() => {
-      if (!$subMenusStore.includes(activeSubMenu)) {
+    onMount(() => {
+        unsubscriberSubMenuStore = subMenusStore.subscribe(() => {
+            if (!$subMenusStore.includes(activeSubMenu)) {
+                switchMenu($subMenusStore[0]);
+            }
+        });
+
+        checkSubMenuToShow();
+
         switchMenu($subMenusStore[0]);
-      }
     });
 
-    checkSubMenuToShow();
+    onDestroy(() => {
+        if (unsubscriberSubMenuStore) {
+            unsubscriberSubMenuStore();
+        }
+    });
 
-    switchMenu($subMenusStore[0]);
-  });
-
-  onDestroy(() => {
-    if (unsubscriberSubMenuStore) {
-      unsubscriberSubMenuStore();
+    function switchMenu(menu: MenuItem) {
+        if (menu.type === "translated") {
+            activeSubMenu = menu;
+            switch (menu.key) {
+                case SubMenusInterface.settings:
+                    activeComponent = SettingsSubMenu;
+                    break;
+                case SubMenusInterface.profile:
+                    activeComponent = ProfileSubMenu;
+                    break;
+                case SubMenusInterface.invite:
+                    activeComponent = GuestSubMenu;
+                    break;
+                case SubMenusInterface.aboutRoom:
+                    activeComponent = AboutRoomSubMenu;
+                    break;
+                case SubMenusInterface.globalMessages:
+                    activeComponent = GlobalMessageSubMenu;
+                    break;
+                case SubMenusInterface.contact:
+                    activeComponent = ContactSubMenu;
+                    break;
+            }
+        } else {
+            const customMenu = customMenuIframe.get(menu.label);
+            if (customMenu !== undefined) {
+                activeSubMenu = menu;
+                props = { url: customMenu.url, allowApi: customMenu.allowApi };
+                activeComponent = CustomSubMenu;
+            } else {
+                sendMenuClickedEvent(menu.label);
+                menuVisiblilityStore.set(false);
+            }
+        }
     }
-  });
 
-  function switchMenu(menu: MenuItem) {
-    if (menu.type === "translated") {
-      activeSubMenu = menu;
-      switch (menu.key) {
-        case SubMenusInterface.settings:
-          activeComponent = SettingsSubMenu;
-          break;
-        case SubMenusInterface.profile:
-          activeComponent = ProfileSubMenu;
-          break;
-        case SubMenusInterface.invite:
-          activeComponent = GuestSubMenu;
-          break;
-        case SubMenusInterface.aboutRoom:
-          activeComponent = AboutRoomSubMenu;
-          break;
-        case SubMenusInterface.globalMessages:
-          activeComponent = GlobalMessageSubMenu;
-          break;
-        case SubMenusInterface.contact:
-          activeComponent = ContactSubMenu;
-          break;
-      }
-    } else {
-      const customMenu = customMenuIframe.get(menu.label);
-      if (customMenu !== undefined) {
-        activeSubMenu = menu;
-        props = { url: customMenu.url, allowApi: customMenu.allowApi };
-        activeComponent = CustomSubMenu;
-      } else {
-        sendMenuClickedEvent(menu.label);
+    function closeMenu() {
         menuVisiblilityStore.set(false);
-      }
-    }
-  }
-
-  function closeMenu() {
-    menuVisiblilityStore.set(false);
-  }
-
-  function onKeyDown(e: KeyboardEvent) {
-    if (e.key === "Escape") {
-      closeMenu();
-    }
-  }
-
-  function translateMenuName(menu: MenuItem) {
-    if (menu.type === "scripting") {
-      return menu.label;
     }
 
-    // Bypass the proxy of typesafe for getting the menu name : https://github.com/ivanhofer/typesafe-i18n/issues/156
-    const getMenuName = $LL.menu.sub[menu.key];
+    function onKeyDown(e: KeyboardEvent) {
+        if (e.key === "Escape") {
+            closeMenu();
+        }
+    }
 
-    return getMenuName();
-  }
+    function translateMenuName(menu: MenuItem) {
+        if (menu.type === "scripting") {
+            return menu.label;
+        }
+
+        // Bypass the proxy of typesafe for getting the menu name : https://github.com/ivanhofer/typesafe-i18n/issues/156
+        const getMenuName = $LL.menu.sub[menu.key];
+
+        return getMenuName();
+    }
 </script>
 
 <svelte:window on:keydown={onKeyDown} />
 
 <div class="menu-container">
-  <div class="menu-nav-sidebar" transition:fly={{ x: -1000, duration: 500 }}>
-    <h2 class="tw-p-5 blue-title">{$LL.menu.title()}</h2>
-    <nav>
-      {#each $subMenusStore as submenu}
-        <div class="menu-item-container {activeSubMenu === submenu ? 'active' : ''}" on:click|preventDefault={() => switchMenu(submenu)}>
-          <a
-            type="button"
-            class="tw-flex menu-item tw-align middle"
-          >
-            {translateMenuName(submenu)}
-          </a>
-          <img src="{chevronImg}" class="menu-icon" alt="open submenu"/>
-        </div>
-      {/each}
-    </nav>
-  </div>
-  <div class="menu-submenu-container tw-bg-dark-purple/95 tw-rounded" transition:fly={{ y: -1000, duration: 500 }}>
-    <button type="button" class="close" on:click={closeMenu}>&times</button>
-    <h2>{translateMenuName(activeSubMenu)}</h2>
-    <svelte:component this={activeComponent} {...props} />
-  </div>
+    <div class="menu-nav-sidebar" transition:fly={{ x: -1000, duration: 500 }}>
+        <h2 class="tw-p-5 blue-title">{$LL.menu.title()}</h2>
+        <nav>
+            {#each $subMenusStore as submenu}
+                <div
+                    class="menu-item-container {activeSubMenu === submenu ? 'active' : ''}"
+                    on:click|preventDefault={() => switchMenu(submenu)}
+                >
+                    <a type="button" class="tw-flex menu-item tw-align middle">
+                        {translateMenuName(submenu)}
+                    </a>
+                    <img src={chevronImg} class="menu-icon" alt="open submenu" />
+                </div>
+            {/each}
+        </nav>
+    </div>
+    <div class="menu-submenu-container tw-bg-dark-purple/95 tw-rounded" transition:fly={{ y: -1000, duration: 500 }}>
+        <button type="button" class="close" on:click={closeMenu}>&times</button>
+        <h2>{translateMenuName(activeSubMenu)}</h2>
+        <svelte:component this={activeComponent} {...props} />
+    </div>
 </div>
 
 <style lang="scss">
-  @import "../../../style/breakpoints.scss";
+    @import "../../../style/breakpoints.scss";
 </style>
-
--- front/src/Components/Menu/SettingsSubMenu.svelte
+++ front/src/Components/Menu/SettingsSubMenu.svelte
@@ -1,241 +1,226 @@
 <script lang="ts">
-  import { localUserStore } from "../../Connexion/LocalUserStore";
-  import { videoConstraintStore } from "../../Stores/MediaStore";
-  import { HtmlUtils } from "../../WebRtc/HtmlUtils";
-  import { menuVisiblilityStore } from "../../Stores/MenuStore";
-  import LL, { locale } from "../../i18n/i18n-svelte";
-  import type { Locales } from "../../i18n/i18n-types";
-  import { displayableLocales, setCurrentLocale } from "../../i18n/locales";
-  import { isMediaBreakpointUp } from "../../Utils/BreakpointsUtils";
-  import { audioManagerVolumeStore } from "../../Stores/AudioManagerStore";
+    import { localUserStore } from "../../Connexion/LocalUserStore";
+    import { videoConstraintStore } from "../../Stores/MediaStore";
+    import { HtmlUtils } from "../../WebRtc/HtmlUtils";
+    import { menuVisiblilityStore } from "../../Stores/MenuStore";
+    import LL, { locale } from "../../i18n/i18n-svelte";
+    import type { Locales } from "../../i18n/i18n-types";
+    import { displayableLocales, setCurrentLocale } from "../../i18n/locales";
+    import { isMediaBreakpointUp } from "../../Utils/BreakpointsUtils";
+    import { audioManagerVolumeStore } from "../../Stores/AudioManagerStore";
 
-  import infoImg from "../images/info.svg";
+    import infoImg from "../images/info.svg";
 
-  let fullscreen: boolean = localUserStore.getFullscreen();
-  let notification: boolean = localUserStore.getNotification() === "granted";
-  let forceCowebsiteTrigger: boolean = localUserStore.getForceCowebsiteTrigger();
-  let ignoreFollowRequests: boolean = localUserStore.getIgnoreFollowRequests();
-  let decreaseAudioPlayerVolumeWhileTalking: boolean = localUserStore.getDecreaseAudioPlayerVolumeWhileTalking();
-  let valueGame: number = localUserStore.getGameQualityValue();
-  let valueVideo: number = localUserStore.getVideoQualityValue();
-  let valueLocale: string = $locale;
-  let valueCameraPrivacySettings = localUserStore.getCameraPrivacySettings();
-  let valueMicrophonePrivacySettings = localUserStore.getMicrophonePrivacySettings();
+    let fullscreen: boolean = localUserStore.getFullscreen();
+    let notification: boolean = localUserStore.getNotification() === "granted";
+    let forceCowebsiteTrigger: boolean = localUserStore.getForceCowebsiteTrigger();
+    let ignoreFollowRequests: boolean = localUserStore.getIgnoreFollowRequests();
+    let decreaseAudioPlayerVolumeWhileTalking: boolean = localUserStore.getDecreaseAudioPlayerVolumeWhileTalking();
+    let valueGame: number = localUserStore.getGameQualityValue();
+    let valueVideo: number = localUserStore.getVideoQualityValue();
+    let valueLocale: string = $locale;
+    let valueCameraPrivacySettings = localUserStore.getCameraPrivacySettings();
+    let valueMicrophonePrivacySettings = localUserStore.getMicrophonePrivacySettings();
 
-  let previewValueGame = valueGame;
-  let previewValueVideo = valueVideo;
-  let previewValueLocale = valueLocale;
-  let previewCameraPrivacySettings = valueCameraPrivacySettings;
-  let previewMicrophonePrivacySettings = valueMicrophonePrivacySettings;
+    let previewValueGame = valueGame;
+    let previewValueVideo = valueVideo;
+    let previewValueLocale = valueLocale;
+    let previewCameraPrivacySettings = valueCameraPrivacySettings;
+    let previewMicrophonePrivacySettings = valueMicrophonePrivacySettings;
 
-  function saveSetting() {
-    let change = false;
+    function saveSetting() {
+        let change = false;
 
-    if (valueLocale !== previewValueLocale) {
-      previewValueLocale = valueLocale;
-      setCurrentLocale(valueLocale as Locales);
+        if (valueLocale !== previewValueLocale) {
+            previewValueLocale = valueLocale;
+            setCurrentLocale(valueLocale as Locales);
+        }
+
+        if (valueVideo !== previewValueVideo) {
+            previewValueVideo = valueVideo;
+            videoConstraintStore.setFrameRate(valueVideo);
+        }
+
+        if (valueGame !== previewValueGame) {
+            previewValueGame = valueGame;
+            localUserStore.setGameQualityValue(valueGame);
+            change = true;
+        }
+
+        if (valueCameraPrivacySettings !== previewCameraPrivacySettings) {
+            previewCameraPrivacySettings = valueCameraPrivacySettings;
+            localUserStore.setCameraPrivacySettings(valueCameraPrivacySettings);
+        }
+
+        if (valueMicrophonePrivacySettings !== previewMicrophonePrivacySettings) {
+            previewMicrophonePrivacySettings = valueMicrophonePrivacySettings;
+            localUserStore.setMicrophonePrivacySettings(valueMicrophonePrivacySettings);
+        }
+
+        audioManagerVolumeStore.setDecreaseWhileTalking(decreaseAudioPlayerVolumeWhileTalking);
+
+        if (change) {
+            window.location.reload();
+        }
+
+        closeMenu();
     }
 
-    if (valueVideo !== previewValueVideo) {
-      previewValueVideo = valueVideo;
-      videoConstraintStore.setFrameRate(valueVideo);
+    function changeFullscreen() {
+        const body = HtmlUtils.querySelectorOrFail("body");
+        if (body) {
+            if (document.fullscreenElement !== null && !fullscreen) {
+                document.exitFullscreen().catch((e) => console.error(e));
+            } else {
+                body.requestFullscreen().catch((e) => console.error(e));
+            }
+            localUserStore.setFullscreen(fullscreen);
+        }
     }
 
-    if (valueGame !== previewValueGame) {
-      previewValueGame = valueGame;
-      localUserStore.setGameQualityValue(valueGame);
-      change = true;
-    }
-
-    if (valueCameraPrivacySettings !== previewCameraPrivacySettings) {
-      previewCameraPrivacySettings = valueCameraPrivacySettings;
-      localUserStore.setCameraPrivacySettings(valueCameraPrivacySettings);
-    }
-
-    if (valueMicrophonePrivacySettings !== previewMicrophonePrivacySettings) {
-      previewMicrophonePrivacySettings = valueMicrophonePrivacySettings;
-      localUserStore.setMicrophonePrivacySettings(valueMicrophonePrivacySettings);
-    }
-
-    audioManagerVolumeStore.setDecreaseWhileTalking(decreaseAudioPlayerVolumeWhileTalking);
-
-    if (change) {
-      window.location.reload();
-    }
-
-    closeMenu();
-  }
-
-  function changeFullscreen() {
-    const body = HtmlUtils.querySelectorOrFail("body");
-    if (body) {
-      if (document.fullscreenElement !== null && !fullscreen) {
-        document.exitFullscreen().catch((e) => console.error(e));
-      } else {
-        body.requestFullscreen().catch((e) => console.error(e));
-      }
-      localUserStore.setFullscreen(fullscreen);
-    }
-  }
-
-  function changeNotification() {
-    if (Notification.permission === "granted") {
-      localUserStore.setNotification(notification ? "granted" : "denied");
-    } else {
-      Notification.requestPermission()
-        .then((response) => {
-          if (response === "granted") {
+    function changeNotification() {
+        if (Notification.permission === "granted") {
             localUserStore.setNotification(notification ? "granted" : "denied");
-          } else {
-            localUserStore.setNotification("denied");
-            notification = false;
-          }
-        })
-        .catch((e) => console.error(e));
+        } else {
+            Notification.requestPermission()
+                .then((response) => {
+                    if (response === "granted") {
+                        localUserStore.setNotification(notification ? "granted" : "denied");
+                    } else {
+                        localUserStore.setNotification("denied");
+                        notification = false;
+                    }
+                })
+                .catch((e) => console.error(e));
+        }
     }
-  }
 
-  function changeForceCowebsiteTrigger() {
-    localUserStore.setForceCowebsiteTrigger(forceCowebsiteTrigger);
-  }
+    function changeForceCowebsiteTrigger() {
+        localUserStore.setForceCowebsiteTrigger(forceCowebsiteTrigger);
+    }
 
-  function changeIgnoreFollowRequests() {
-    localUserStore.setIgnoreFollowRequests(ignoreFollowRequests);
-  }
+    function changeIgnoreFollowRequests() {
+        localUserStore.setIgnoreFollowRequests(ignoreFollowRequests);
+    }
 
-  function changeDecreaseAudioPlayerVolumeWhileTalking() {
-    localUserStore.setDecreaseAudioPlayerVolumeWhileTalking(decreaseAudioPlayerVolumeWhileTalking);
-  }
+    function changeDecreaseAudioPlayerVolumeWhileTalking() {
+        localUserStore.setDecreaseAudioPlayerVolumeWhileTalking(decreaseAudioPlayerVolumeWhileTalking);
+    }
 
-  function closeMenu() {
-    menuVisiblilityStore.set(false);
-  }
+    function closeMenu() {
+        menuVisiblilityStore.set(false);
+    }
 
-  const isMobile = isMediaBreakpointUp("md");
+    const isMobile = isMediaBreakpointUp("md");
 </script>
 
 <div on:submit|preventDefault={saveSetting}>
-  <section class="bottom-separator">
-    <h3 class="blue-title">{$LL.menu.settings.gameQuality.title()}</h3>
-    <select bind:value={valueGame} class="tw-w-full">
-      <option value={120}
-      >{isMobile
-        ? $LL.menu.settings.gameQuality.short.high()
-        : $LL.menu.settings.gameQuality.long.high()}</option
-      >
-      <option value={60}
-      >{isMobile
-        ? $LL.menu.settings.gameQuality.short.medium()
-        : $LL.menu.settings.gameQuality.long.medium()}</option
-      >
-      <option value={40}
-      >{isMobile
-        ? $LL.menu.settings.gameQuality.short.small()
-        : $LL.menu.settings.gameQuality.long.small()}</option
-      >
-      <option value={20}
-      >{isMobile
-        ? $LL.menu.settings.gameQuality.short.minimum()
-        : $LL.menu.settings.gameQuality.long.minimum()}</option
-      >
-    </select>
-    <h3 class="blue-title">{$LL.menu.settings.videoQuality.title()}</h3>
-    <select bind:value={valueVideo} class="tw-w-full">
-      <option value={30}
-      >{isMobile
-        ? $LL.menu.settings.videoQuality.short.high()
-        : $LL.menu.settings.videoQuality.long.high()}</option
-      >
-      <option value={20}
-      >{isMobile
-        ? $LL.menu.settings.videoQuality.short.medium()
-        : $LL.menu.settings.videoQuality.long.medium()}</option
-      >
-      <option value={10}
-      >{isMobile
-        ? $LL.menu.settings.videoQuality.short.small()
-        : $LL.menu.settings.videoQuality.long.small()}</option
-      >
-      <option value={5}
-      >{isMobile
-        ? $LL.menu.settings.videoQuality.short.minimum()
-        : $LL.menu.settings.videoQuality.long.minimum()}</option
-      >
-    </select>
-    <h3 class="blue-title">{$LL.menu.settings.language.title()}</h3>
-    <select class="tw-w-full" bind:value={valueLocale}>
-      {#each displayableLocales as locale (locale.id)}
-        <option value={locale.id}>{`${locale.language} (${locale.country})`}</option>
-      {/each}
-    </select>
+    <section class="bottom-separator">
+        <h3 class="blue-title">{$LL.menu.settings.gameQuality.title()}</h3>
+        <select bind:value={valueGame} class="tw-w-full">
+            <option value={120}
+                >{isMobile
+                    ? $LL.menu.settings.gameQuality.short.high()
+                    : $LL.menu.settings.gameQuality.long.high()}</option
+            >
+            <option value={60}
+                >{isMobile
+                    ? $LL.menu.settings.gameQuality.short.medium()
+                    : $LL.menu.settings.gameQuality.long.medium()}</option
+            >
+            <option value={40}
+                >{isMobile
+                    ? $LL.menu.settings.gameQuality.short.small()
+                    : $LL.menu.settings.gameQuality.long.small()}</option
+            >
+            <option value={20}
+                >{isMobile
+                    ? $LL.menu.settings.gameQuality.short.minimum()
+                    : $LL.menu.settings.gameQuality.long.minimum()}</option
+            >
+        </select>
+        <h3 class="blue-title">{$LL.menu.settings.videoQuality.title()}</h3>
+        <select bind:value={valueVideo} class="tw-w-full">
+            <option value={30}
+                >{isMobile
+                    ? $LL.menu.settings.videoQuality.short.high()
+                    : $LL.menu.settings.videoQuality.long.high()}</option
+            >
+            <option value={20}
+                >{isMobile
+                    ? $LL.menu.settings.videoQuality.short.medium()
+                    : $LL.menu.settings.videoQuality.long.medium()}</option
+            >
+            <option value={10}
+                >{isMobile
+                    ? $LL.menu.settings.videoQuality.short.small()
+                    : $LL.menu.settings.videoQuality.long.small()}</option
+            >
+            <option value={5}
+                >{isMobile
+                    ? $LL.menu.settings.videoQuality.short.minimum()
+                    : $LL.menu.settings.videoQuality.long.minimum()}</option
+            >
+        </select>
+        <h3 class="blue-title">{$LL.menu.settings.language.title()}</h3>
+        <select class="tw-w-full" bind:value={valueLocale}>
+            {#each displayableLocales as locale (locale.id)}
+                <option value={locale.id}>{`${locale.language} (${locale.country})`}</option>
+            {/each}
+        </select>
 
-    <div class="centered-column">
-      <p>{$LL.menu.settings.save.warning()}</p>
-      <button type="button" class="light" on:click|preventDefault={saveSetting}
-      >{$LL.menu.settings.save.button()}</button>
-    </div>
-  </section>
-  <section class="bottom-separator tw-flex tw-flex-col">
-    <div class="tooltip">
-      <h3
-        class="blue-title tw-underline tw-decoration-light-blue tw-decoration-dotted">{$LL.menu.settings.privacySettings.title()}
-        <img src={infoImg} alt="info icon" width="18px" height="18px"/></h3>
-      <span class="tooltiptext">{$LL.menu.settings.privacySettings.explanation()}</span>
-    </div>
-    <label>
-      <input type="checkbox" bind:checked={valueCameraPrivacySettings} />
-      {$LL.menu.settings.privacySettings.cameraToggle()}
-    </label>
-    <label>
-      <input type="checkbox" bind:checked={valueMicrophonePrivacySettings} />
-      {$LL.menu.settings.privacySettings.microphoneToggle()}
-    </label>
-  </section>
-  <section class="tw-flex tw-flex-col">
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={fullscreen}
-        on:change={changeFullscreen}
-      />
-      <span>{$LL.menu.settings.fullscreen()}</span>
-    </label>
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={notification}
-        on:change={changeNotification}
-      />
-      <span>{$LL.menu.settings.notifications()}</span>
-    </label>
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={forceCowebsiteTrigger}
-        on:change={changeForceCowebsiteTrigger}
-      />
-      <span>{$LL.menu.settings.cowebsiteTrigger()}</span>
-    </label>
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={ignoreFollowRequests}
-        on:change={changeIgnoreFollowRequests}
-      />
-      <span>{$LL.menu.settings.ignoreFollowRequest()}</span>
-    </label>
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={decreaseAudioPlayerVolumeWhileTalking}
-        on:change={changeDecreaseAudioPlayerVolumeWhileTalking}
-      />
-      <span>{$LL.audio.manager.reduce()}</span>
-    </label>
-
-  </section>
+        <div class="centered-column">
+            <p>{$LL.menu.settings.save.warning()}</p>
+            <button type="button" class="light" on:click|preventDefault={saveSetting}
+                >{$LL.menu.settings.save.button()}</button
+            >
+        </div>
+    </section>
+    <section class="bottom-separator tw-flex tw-flex-col">
+        <div class="tooltip">
+            <h3 class="blue-title tw-underline tw-decoration-light-blue tw-decoration-dotted">
+                {$LL.menu.settings.privacySettings.title()}
+                <img src={infoImg} alt="info icon" width="18px" height="18px" />
+            </h3>
+            <span class="tooltiptext">{$LL.menu.settings.privacySettings.explanation()}</span>
+        </div>
+        <label>
+            <input type="checkbox" bind:checked={valueCameraPrivacySettings} />
+            {$LL.menu.settings.privacySettings.cameraToggle()}
+        </label>
+        <label>
+            <input type="checkbox" bind:checked={valueMicrophonePrivacySettings} />
+            {$LL.menu.settings.privacySettings.microphoneToggle()}
+        </label>
+    </section>
+    <section class="tw-flex tw-flex-col">
+        <label>
+            <input type="checkbox" bind:checked={fullscreen} on:change={changeFullscreen} />
+            <span>{$LL.menu.settings.fullscreen()}</span>
+        </label>
+        <label>
+            <input type="checkbox" bind:checked={notification} on:change={changeNotification} />
+            <span>{$LL.menu.settings.notifications()}</span>
+        </label>
+        <label>
+            <input type="checkbox" bind:checked={forceCowebsiteTrigger} on:change={changeForceCowebsiteTrigger} />
+            <span>{$LL.menu.settings.cowebsiteTrigger()}</span>
+        </label>
+        <label>
+            <input type="checkbox" bind:checked={ignoreFollowRequests} on:change={changeIgnoreFollowRequests} />
+            <span>{$LL.menu.settings.ignoreFollowRequest()}</span>
+        </label>
+        <label>
+            <input
+                type="checkbox"
+                bind:checked={decreaseAudioPlayerVolumeWhileTalking}
+                on:change={changeDecreaseAudioPlayerVolumeWhileTalking}
+            />
+            <span>{$LL.audio.manager.reduce()}</span>
+        </label>
+    </section>
 </div>
 
 <style lang="scss">
-  @import "../../../style/breakpoints.scss";
+    @import "../../../style/breakpoints.scss";
 </style>
