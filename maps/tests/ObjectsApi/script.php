5dedb1d49567aa579ed55d6de70399eeb1367437
Piotr 'pwh' Hanusiak <p.hanusiak@workadventu.re>
2022-05-03 15:40:52 +0200

    working test map for setting up areas properties

--- maps/tests/ObjectsApi/script.php
+++ maps/tests/ObjectsApi/script.php
@@ -9,24 +9,21 @@
             WA.onInit().then(() => {
                 console.log('After WA init');
                 const updateButton = document.getElementById('updateButton');
-                const xField = document.getElementById('x');
-                const yField = document.getElementById('y');
-                const widthField = document.getElementById('width');
-                const heightField = document.getElementById('height');
 
-                // setCameraButton.addEventListener('click', () => {
-                //     WA.camera.set(
-                //         parseInt(xField.value),
-                //         parseInt(yField.value),
-                //         widthField.value ? parseInt(widthField.value) : undefined,
-                //         heightField.value ? parseInt(heightField.value) : undefined,
-                //         lockField.checked,
-                //         smoothField.checked,
-                //     );
-                // });
+                const focusableField = document.getElementById('focusable');
+                const zoomMarginField = document.getElementById('zoom_margin');
+                const silentField = document.getElementById('silent');
+                const openWebisteField = document.getElementById('openWebiste');
+                const jitsiRoomField = document.getElementById('jitsiRoom');
+                const jitsiTriggerField = document.getElementById('jitsiTrigger');
 
                 updateButton.addEventListener('click', () => {
-                    WA.room.setAreaProperty('Center', 'focusable', false);
+                    WA.room.setAreaProperty('Center', 'focusable', focusableField.checked);
+                    WA.room.setAreaProperty('Center', 'silent', silentField.checked);
+                    WA.room.setAreaProperty('Center', 'zoom_margin', zoomMarginField.value || undefined);
+                    WA.room.setAreaProperty('Center', 'openWebsite', openWebisteField.value || undefined);
+                    WA.room.setAreaProperty('Center', 'jitsiRoom', jitsiRoomField.value || undefined);
+                    WA.room.setAreaProperty('Center', 'jitsiTrigger', jitsiTriggerField.value || undefined);
                 });
             });
         })
@@ -37,7 +34,6 @@ focusable: <input type="checkbox" id="focusable" value=1 checked /><br/>
 zoom_margin: <input type="text" id="zoom_margin" value=0 /><br/>
 silent: <input type="checkbox" id="silent" value=0 /><br/>
 openWebsite: <input type="text" id="openWebiste" value="https://workadventu.re/" /><br/>
-silent: <input type="checkbox" id="silent" value=0 /><br/>
 jitsiRoom: <input type="text" id="jitsiRoom" value="Meeting Room" /><br/>
 jitsiTrigger: <input type="text" id="jitsiTrigger" value="onaction" /><br/>
 
