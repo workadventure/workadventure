99a4c66520c769751735075eae60b89e51fee7c0
Yassine Bousaidi <yassine.bousaidi@hotmail.com>
2022-06-08 22:08:05 +0200

    Ajout du debut de la map

---
+++ maps/starter/perso2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="perso2" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="../assets/free_tileset_16x16_by_chaoticcherrycake_ddi8611-300w.png" width="300" height="250"/>
+</tileset>
---
+++ maps/starter/perso3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="perso" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="../assets/First Asset pack.png" width="384" height="384"/>
+</tileset>
