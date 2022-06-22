2c3330fc5e08d75a55e2b6a8e44961347348cadb
CEC <c.cardinale@workadventu.re>
2022-06-22 11:15:57 +0200

    Translation + Messages

---
+++ front/chat/src/Messages/JsonMessages/ChatData.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const isUserData = z.object({
+    uuid: z.string(),
+    email: z.optional(z.nullable(z.string())),
+    name: z.string(),
+    playUri: z.string(),
+    authToken: z.optional(z.string())
+});
+
+export type UserData = z.infer<typeof isUserData>;
---
+++ front/chat/src/Messages/JsonMessages/RoomRedirect.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+/*
+ * WARNING! The original file is in /messages/JsonMessages.
+ * All other files are automatically copied from this file on container startup / build
+ */
+
+export const isRoomRedirect = z.object({
+    redirectUrl: z.string(),
+});
+
+export type RoomRedirect = z.infer<typeof isRoomRedirect>;
