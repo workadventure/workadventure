7d1a87dd2723114bbc95987d04a3fc7aef715e20
CEC <c.cardinale@workadventu.re>
2022-06-23 16:49:40 +0200

    Fix lint errors (pusher)

--- pusher/src/Services/XmppClient.ts
+++ pusher/src/Services/XmppClient.ts
@@ -3,7 +3,6 @@ import { v4 } from "uuid";
 import {
     MucRoomDefinitionMessage,
     PusherToIframeMessage,
-    ServerToClientMessage,
     SubMessage,
     XmppConnectionStatusChangeMessage,
     XmppMessage,
