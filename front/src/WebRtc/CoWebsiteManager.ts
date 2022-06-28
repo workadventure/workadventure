0be21328e329b33d226651b2b8bb801c722356d0
Alexis Faizeau <faizeau.alexis@gmail.com>
2022-06-28 13:54:22 +0200

    CoWebsites fixes (#2350)
    * Fix cowebsite action bar
    
    * Fix cowebsite full screen
    
    * Add a min size on cowebsite aside icons
    
    Co-authored-by: Alexis Faizeau <a.faizeau@workadventu.re>

--- front/src/WebRtc/CoWebsiteManager.ts
+++ front/src/WebRtc/CoWebsiteManager.ts
@@ -58,6 +58,12 @@ class CoWebsiteManager {
 
     private resizeObserver = new ResizeObserver(() => {
         this.resizeAllIframes();
+
+        if (!this.isFullScreen && this.cowebsiteAsideHolderDom.style.visibility === "hidden") {
+            this.toggleFullScreenIcon(true);
+            this.resetStyleMain();
+            this.fire();
+        }
     });
 
     public getMainState() {
@@ -155,7 +161,7 @@ class CoWebsiteManager {
         buttonFullScreenFrame.addEventListener("click", () => {
             analyticsClient.fullScreenMultiIframe();
             buttonFullScreenFrame.blur();
-            this.fullscreen();
+            this.toggleFullscreen();
         });
 
         const buttonSwipe = HtmlUtils.getElementByIdOrFail(cowebsiteSwipeButtonId);
@@ -648,7 +654,7 @@ class CoWebsiteManager {
             this.displayMain();
         }
 
-        const coWebsiteLloading = coWebsite
+        const coWebsiteLoading = coWebsite
             .load()
             .then(() => {
                 const mainCoWebsite = this.getMainCoWebsite();
@@ -679,7 +685,7 @@ class CoWebsiteManager {
                 this.removeCoWebsiteFromStack(coWebsite);
             });
 
-        return coWebsiteLloading;
+        return coWebsiteLoading;
     }
 
     public unloadCoWebsite(coWebsite: CoWebsite): Promise<void> {
@@ -756,7 +762,7 @@ class CoWebsiteManager {
         waScaleManager.refreshFocusOnTarget();
     }
 
-    private fullscreen(): void {
+    private toggleFullscreen(): void {
         if (this.isFullScreen) {
             this.toggleFullScreenIcon(true);
             this.resetStyleMain();
