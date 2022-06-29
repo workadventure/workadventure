cb1170eaeef62805db91190cfc9486f97a5c2214
Gregoire Parant <g.parant@thecodingmachine.com>
2022-06-29 15:22:43 +0200

    Add FALLBACK_LOCALE
    Signed-off-by: Gregoire Parant <g.parant@thecodingmachine.com>

--- front/chat/src/Enum/EnvironmentVariable.ts
+++ front/chat/src/Enum/EnvironmentVariable.ts
@@ -1,5 +1,6 @@
 import { getEnvConfig } from "@geprog/vite-plugin-env-config/getEnvConfig";
 
 const PUSHER_URL = getEnvConfig("PUSHER_URL") || "//pusher.workadventure.localhost";
+const FALLBACK_LOCALE = getEnvConfig("FALLBACK_LOCALE") || undefined;
 
 export { PUSHER_URL};
