e6a555bb5f4ccce605a8770161a37fcb585a8371
Waclaw-I <hanusiakp@gmail.com>
2022-07-07 15:27:42 +0200

    prettier

--- back/src/Services/MapStorageClient.ts
+++ back/src/Services/MapStorageClient.ts
@@ -1,8 +1,9 @@
 import { MapStorageClient } from "../Messages/generated/messages_grpc_pb";
 import grpc from "grpc";
-import { MAP_STORAGE_URL } from "../Enum/EnvironmentVariable";
+import { ENABLE_FEATURE_MAP_EDITOR, MAP_STORAGE_URL } from "../Enum/EnvironmentVariable";
 
-console.log(`%%%%%%%%% MAP STORAGE URL: ${MAP_STORAGE_URL}`);
+if (ENABLE_FEATURE_MAP_EDITOR) {
+    console.log(`%%%%%%%%% MAP STORAGE URL: ${MAP_STORAGE_URL}`);
+}
 
 export const mapStorageClient = new MapStorageClient(MAP_STORAGE_URL, grpc.credentials.createInsecure());
-
