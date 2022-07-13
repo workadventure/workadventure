727993e6b06a8db51d56e84947e2703cd076b55d
Gregoire Parant <g.parant@thecodingmachine.com>
2022-07-13 19:01:25 +0200

    Fix pretty pusher
    Signed-off-by: Gregoire Parant <g.parant@thecodingmachine.com>

--- pusher/src/Services/LocalAdmin.ts
+++ pusher/src/Services/LocalAdmin.ts
@@ -26,7 +26,7 @@ class LocalAdmin implements AdminInterface {
             textures: (await localWokaService.fetchWokaDetails(characterLayers)) ?? [],
             userRoomToken: undefined,
             //@ts-ignore
-            mucRooms: [{ name: "Default", url: playUri, type: "default"}],
+            mucRooms: [{ name: "Default", url: playUri, type: "default" }],
             activatedInviteUser: true,
         };
     }
