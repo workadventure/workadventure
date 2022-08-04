493a3f0bb3b14c71708c4f84a0c552ae129a0ff9
grégoire parant <gregoire@workadventu.re>
2022-08-04 13:14:11 +0200

    Fix continous integration

--- front/chat/src/Components/Content/HtmlMessage.svelte
+++ front/chat/src/Components/Content/HtmlMessage.svelte
@@ -2,7 +2,6 @@
     import { iframeListener } from "../../IframeListener";
     import { onDestroy, onMount } from "svelte";
     import { linkFunction } from "../../Services/WebLinkManager";
-import { CircleIcon } from "svelte-feather-icons";
 
     const checkIcone = `<svg xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check tw-text-pop-green"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
     const alertCircleIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle "><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`
