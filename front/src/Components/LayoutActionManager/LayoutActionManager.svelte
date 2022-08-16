38c7215ba9bfb8b05376e91158280ec753d12c6a
Bénédicte Q <37311765+HimeShaman@users.noreply.github.com>
2022-08-16 09:41:39 +0200

    fix:Makes popins about access to camera denied clickable (#2480)
    Co-authored-by: Benedicte Quimbert <b.quimbert@workadventu.re>

--- front/src/Components/LayoutActionManager/LayoutActionManager.svelte
+++ front/src/Components/LayoutActionManager/LayoutActionManager.svelte
@@ -10,14 +10,14 @@
     {#each $layoutManagerActionStore as action}
         {#if action.type === "warning"}
             <div
-                class="blue-dialog-box tw-w-56 tw-min-h-10 tw-border-pop-yellow tw-text-pop-yellow tw-border tw-border-solid"
+                class="blue-dialog-box tw-w-56 tw-min-h-10 tw-border-pop-yellow tw-text-pop-yellow tw-border tw-border-solid tw-pointer-events-auto"
                 on:click={() => onClick(action.callback)}
             >
                 <p class="tw-text-center tw-m-0 tw-p-2">{action.message}</p>
             </div>
         {:else}
             <div
-                class="blue-dialog-box tw-w-56 tw-min-h-10 tw-border-light-blue tw-text-light-blue tw-border tw-border-solid"
+                class="blue-dialog-box tw-w-56 tw-min-h-10 tw-border-light-blue tw-text-light-blue tw-border tw-border-solid tw-pointer-events-auto"
                 on:click={() => onClick(action.callback)}
             >
                 <p class="tw-text-center tw-m-0 tw-p-2">{action.message}</p>
