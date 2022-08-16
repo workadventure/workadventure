c4b736bbe0cc51bb75ce35f73946a96b0846569a
Waclaw-I <hanusiakp@gmail.com>
2022-08-16 14:23:18 +0200

    revert changes to tsconfig

--- map-storage/src/MapsManager.ts
+++ map-storage/src/MapsManager.ts
@@ -9,16 +9,16 @@ export class MapsManager {
     }
 
     public async getMap(path: string): Promise<any> {
-        const inMemoryGameMap = this.loadedMaps.get(path);
-        if (inMemoryGameMap) {
-            console.log("SERVE MAP FROM MEMORY");
-            console.log(inMemoryGameMap);
-            // return inMemoryGameMap.getMap();
-        }
+        // const inMemoryGameMap = this.loadedMaps.get(path);
+        // if (inMemoryGameMap) {
+        //     console.log("SERVE MAP FROM MEMORY");
+        //     console.log(inMemoryGameMap);
+        //     // return inMemoryGameMap.getMap();
+        // }
         console.log("SERVE MAP FROM FILE");
         const file = await readFile(`./public${path}`, "utf-8");
         const map = JSON.parse(file);
-        const gameMap = new GameMap(map);
+        // const gameMap = new GameMap(map);
         this.loadedMaps.set(path, map);
         return map;
     }
