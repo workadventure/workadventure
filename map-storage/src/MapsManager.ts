eebf9d329f61b47224527825769553815bcb1c4c
Waclaw-I <hanusiakp@gmail.com>
2022-08-18 13:24:43 +0200

    server map from memory if possible

--- map-storage/src/MapsManager.ts
+++ map-storage/src/MapsManager.ts
@@ -9,17 +9,15 @@ export class MapsManager {
     }
 
     public async getMap(path: string): Promise<any> {
-        // const inMemoryGameMap = this.loadedMaps.get(path);
-        // if (inMemoryGameMap) {
-        //     console.log("SERVE MAP FROM MEMORY");
-        //     console.log(inMemoryGameMap);
-        //     // return inMemoryGameMap.getMap();
-        // }
+        const inMemoryGameMap = this.loadedMaps.get(path);
+        if (inMemoryGameMap) {
+            console.log("SERVE MAP FROM MEMORY");
+            return inMemoryGameMap.getMap();
+        }
         console.log("SERVE MAP FROM FILE");
         const file = await readFile(`./public${path}`, "utf-8");
         const map = JSON.parse(file);
-        const gameMap = new GameMap(map);
-        this.loadedMaps.set(path, map);
+        this.loadedMaps.set(path, new GameMap(map));
         return map;
     }
 }
