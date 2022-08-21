043dd28353c42443b54ae463e5ca1bb851280945
David Négrier <d.negrier@thecodingmachine.com>
2022-08-21 19:05:50 +0200

    Fixing tests / linting

--- tests/tests/chat.spec.ts
+++ tests/tests/chat.spec.ts
@@ -1,24 +1,10 @@
 import {expect, Page, test} from '@playwright/test';
-import {abortRecordLogs, assertLogMessage, startRecordLogs} from './utils/log';
 import { login } from './utils/roles';
-import {expectInViewport} from "./utils/viewport";
 import {openChat} from "./utils/menu";
-
-const WAIT_FOR_INIT_OF_USERS_LIST = 2_000;
 
 test.setTimeout(60_000);
 
 test.describe('Chat', () => {
-  test('should be fully loaded', async ({ page }) => {
-    startRecordLogs(page);
-    await page.goto(
-        'http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/E2E/empty.json'
-    );
-    const nickname = getUniqueNickname('A');
-    await login(page, nickname, 2);
-    await assertLogMessage(page, 'Chat fully loaded');
-    abortRecordLogs(page);
-  });
 
   test('should connect to ejabberd and show list of users', async ({ page, browser }) => {
     await page.goto(
@@ -29,9 +15,10 @@ test.describe('Chat', () => {
 
     await openChat(page);
     const chat = await getChat(page);
-    await page.waitForTimeout(WAIT_FOR_INIT_OF_USERS_LIST);
 
-    await expect(chat.locator('#users')).toContainText(nickname);
+    await expect(chat.locator('#users')).toContainText(nickname, {
+      timeout: 10_000
+    });
 
     const newBrowser = await browser.browserType().launch();
     const page2 = await newBrowser.newPage();
@@ -44,7 +31,6 @@ test.describe('Chat', () => {
 
     await openChat(page2);
     const chat2 = await getChat(page2);
-    await page2.waitForTimeout(WAIT_FOR_INIT_OF_USERS_LIST);
 
     await expect(chat2.locator('#users')).toContainText(nickname);
     await expect(chat2.locator('#users')).toContainText(nickname2);
@@ -61,8 +47,9 @@ test.describe('Chat', () => {
 
     await openChat(page);
     const chat = await getChat(page);
-    await page.waitForTimeout(WAIT_FOR_INIT_OF_USERS_LIST);
-    await expect(chat.locator('#users')).toContainText(nickname);
+    await expect(chat.locator('#users')).toContainText(nickname, {
+      timeout: 10_000
+    });
 
     await page.locator('#game').focus();
 
