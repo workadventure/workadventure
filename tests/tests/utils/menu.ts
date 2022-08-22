e92e238005247cdcabd48249f7f4cd34f3f7dc0e
David Négrier <d.negrier@thecodingmachine.com>
2022-08-22 18:35:58 +0200

    Removing condition on menu

--- tests/tests/utils/menu.ts
+++ tests/tests/utils/menu.ts
@@ -2,14 +2,8 @@ import {expect, Page} from "@playwright/test";
 import {expectInViewport} from "./viewport";
 
 export async function openChat(page: Page) {
-    try {
-        // If chat is opened
-        await expect(page.locator('#chatWindow')).toHaveClass(/show/, {timeout: 100});
-    } catch {
-        // If chat is not opened
-        await page.click('button.chat-btn');
-        await expectInViewport('#chatWindow', page);
-        await expect(page.locator('button.chat-btn')).toHaveClass(/border-top-light/);
-        await expect(page.locator('#chatWindow')).toHaveClass(/show/);
-    }
+    await page.click('button.chat-btn');
+    await expectInViewport('#chatWindow', page);
+    await expect(page.locator('button.chat-btn')).toHaveClass(/border-top-light/);
+    await expect(page.locator('#chatWindow')).toHaveClass(/show/);
 }
