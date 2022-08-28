5bc519aef894dae99480338a53897e4c0ea5c006
David Négrier <d.negrier@thecodingmachine.com>
2022-08-28 22:00:05 +0200

    Configuring a max timeout for actions
    By default, Playwright has no timeout for actions.
    So if an action cannot be fulfilled, you have to wait until the test timeout (that can be quite high with WA)
    
    This will allow failing tests to fail faster (so we have a faster feedback in CI)

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -35,7 +35,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 20_000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
