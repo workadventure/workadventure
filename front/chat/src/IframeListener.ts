66e8f925e29f75d016d169b72101e9dba98f9427
grégoire parant <gregoire@workadventu.re>
2022-08-29 15:39:31 +0200

    Fix time line errors
     - User cannot receive message
     - Typing error, never find user

--- front/chat/src/IframeListener.ts
+++ front/chat/src/IframeListener.ts
@@ -74,14 +74,14 @@ class IframeListener {
                             break;
                         }
                         case "addChatMessage": {
-                            if (iframeEvent.data.author?.userUuid == undefined || iframeEvent.data.text == undefined) {
+                            if (iframeEvent.data.author == undefined || iframeEvent.data.text == undefined) {
                                 break;
                             }
 
                             let userData = defaultUserData;
                             const mucRoomDefault = mucRoomsStore.getDefaultRoom();
                             if (mucRoomDefault) {
-                                userData = mucRoomDefault.getUserDataByUuid(iframeEvent.data.author?.userUuid);
+                                userData = mucRoomDefault.getUserDataByUuid(iframeEvent.data.author);
                             }
 
                             for (const chatMessageText of iframeEvent.data.text) {
