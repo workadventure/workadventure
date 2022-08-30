6e95ae7e90798a5c7bc8348a1124357a96b08afb
CEC <c.cardinale@workadventu.re>
2022-08-30 11:38:33 +0200

    Reverse usersList style

--- front/chat/src/Components/UsersList.svelte
+++ front/chat/src/Components/UsersList.svelte
@@ -20,11 +20,8 @@
     export let showUsers: boolean;
     export let searchValue: string;
 
-    //let minimizeUser = true;
-    //const maxUsersMinimized = 7;
-
-    let usersContainerEl: HTMLElement;
-    let usersListEl: HTMLElement;
+    let minimizeUser = true;
+    const maxUsersMinimized = 7;
 
     function openChat(user: User) {
         return user;
@@ -40,17 +37,20 @@
     $: me = usersList.find((user) => user.isMe);
     $: meArray = me ? [me] : [];
 
-    $: usersFiltered = meArray.concat(
-        usersList
-            .filter((user) => user.active && !user.isMe && user.name.toLocaleLowerCase().includes(searchValue))
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .concat(
-                usersList
-                    .filter((user) => !user.active && !user.isMe && user.name.toLocaleLowerCase().includes(searchValue))
-                    .sort((a, b) => a.name.localeCompare(b.name))
-            )
-    );
-    //.splice(0, minimizeUser ? maxUsersMinimized : usersList.length);
+    $: usersFiltered = meArray
+        .concat(
+            usersList
+                .filter((user) => user.active && !user.isMe && user.name.toLocaleLowerCase().includes(searchValue))
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .concat(
+                    usersList
+                        .filter(
+                            (user) => !user.active && !user.isMe && user.name.toLocaleLowerCase().includes(searchValue)
+                        )
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                )
+        )
+        .splice(0, minimizeUser ? maxUsersMinimized : usersList.length);
 </script>
 
 <div id="users" class="users tw-border-b tw-border-solid tw-border-0 tw-border-transparent tw-border-b-light-purple">
@@ -68,56 +68,37 @@
         </button>
     </div>
     {#if showUsers}
-        <div
-            class="users-container"
-            transition:fly={{ y: -30, duration: 100 }}
-            bind:this={usersContainerEl}
-            style={`overflow-y: ${usersContainerEl?.clientHeight < usersListEl?.clientHeight ? "scroll" : "hidden"}`}
-        >
-            <div bind:this={usersListEl}>
-                {#if usersList.length === 0}
-                    <div class="tw-px-5 tw-mb-2">
-                        <p>{$LL.roomEmpty()}</p>
-                        <button
-                            type="button"
-                            class="light tw-m-auto tw-cursor-pointer tw-px-3"
-                            on:click={showInviteMenu}
-                        >
-                            {$LL.invite()}
-                        </button>
-                    </div>
-                {:else}
-                    {#each usersFiltered as user}
-                        <ChatUser
-                            {mucRoom}
-                            {openChat}
-                            {user}
-                            on:goTo={(event) => dispatch("goTo", event.detail)}
-                            on:rankUp={(event) => dispatch("rankUp", event.detail)}
-                            on:rankDown={(event) => dispatch("rankDown", event.detail)}
-                            on:ban={(event) => dispatch("ban", event.detail)}
-                            {searchValue}
-                            {meStore}
-                        />
-                    {/each}
-                {/if}
-            </div>
-            <!--
-            {#if [...$usersListStore].length > maxUsersMinimized}
-                <div class="tw-px-2 tw-mb-1  tw-flex tw-justify-end" on:click={() => (minimizeUser = !minimizeUser)}>
-                    <button class="tw-underline tw-text-sm tw-text-lighter-purple tw-font-condensed hover:tw-underline">
-                        {$LL.see()}
-                        {minimizeUser ? $LL.more() : $LL.less()} …
+        <div transition:fly={{ y: -30, duration: 100 }}>
+            {#if usersList.length === 0}
+                <div class="tw-px-5 tw-mb-2">
+                    <p>{$LL.roomEmpty()}</p>
+                    <button type="button" class="light tw-m-auto tw-cursor-pointer tw-px-3" on:click={showInviteMenu}>
+                        {$LL.invite()}
                     </button>
                 </div>
+            {:else}
+                {#each usersFiltered as user}
+                    <ChatUser
+                        {mucRoom}
+                        {openChat}
+                        {user}
+                        on:goTo={(event) => dispatch("goTo", event.detail)}
+                        on:rankUp={(event) => dispatch("rankUp", event.detail)}
+                        on:rankDown={(event) => dispatch("rankDown", event.detail)}
+                        on:ban={(event) => dispatch("ban", event.detail)}
+                        {searchValue}
+                        {meStore}
+                    />
+                {/each}
             {/if}
-            -->
         </div>
+        {#if [...$usersListStore].length > maxUsersMinimized}
+            <div class="tw-px-2 tw-mb-1  tw-flex tw-justify-end" on:click={() => (minimizeUser = !minimizeUser)}>
+                <button class="tw-underline tw-text-sm tw-text-lighter-purple tw-font-condensed hover:tw-underline">
+                    {$LL.see()}
+                    {minimizeUser ? $LL.more() : $LL.less()} …
+                </button>
+            </div>
+        {/if}
     {/if}
 </div>
-
-<style lang="scss">
-    .users-container {
-        max-height: 40vh;
-    }
-</style>
