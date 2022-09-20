236d95e0f2583cf973754d7a21ddd8a5bf9dead6
grégoire parant <gregoire@workadventu.re>
2022-09-20 09:42:03 +0200

    Update test n2n

--- tests/tests/modal_script.spec.ts
+++ tests/tests/modal_script.spec.ts
@@ -1,25 +1,26 @@
 import { test } from '@playwright/test';
-//import {expectInViewport} from "./utils/viewport";
-
-/*function timeout(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}*/
+import { login } from './utils/roles';
+import {evaluateScript} from "./utils/scripting";
+import {expectInViewport} from "./utils/viewport";
 
 test.describe('Modal', () => {
     test('test', async ({ page }) => {
-        // Go to http://play.workadventure.localhost/_/test/maps.workadventure.localhost/tests/modal/modal.json
-        await page.goto('http://play.workadventure.localhost/_/test/maps.workadventure.localhost/tests/modal/modal.json');
-        // Fill input[name="loginSceneName"]
-        await page.locator('input[name="loginSceneName"]').fill('TEST');
-        // Click text=Continue
-        await page.locator('button.loginSceneFormSubmit').click();
-        // Click text=Continue
-        await page.locator('button.selectCharacterSceneFormSubmit').click();
-        // Click text=Let's go!
-        await page.locator('button.light').click();
+        // Go to 
+        await page.goto(
+            'http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/E2E/empty.json'
+        );
+        await login(page, "Alice");
+        await evaluateScript(page, async () => {
+            return WA.modal.openModal({
+                src: "https://workadventu.re"
+            });
+        });
+
         // Check the component of the Webpage
-        //await expectInViewport("#modalIframe", page);
-        // Check the component of the Webpage
+        await expectInViewport("#modalIframe", page);
+
+        //TODO fix me
+        //await timeout(3000);
         //await expect(page.locator('#modalIframe')).toHaveCount(0);
     });
 });
