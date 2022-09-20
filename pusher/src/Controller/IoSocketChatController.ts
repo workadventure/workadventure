8caaa58b3797453e9ec2dc0e7d1265f550a324a7
César Cardinale <c.cardinale@workadventu.re>
2022-09-20 11:24:07 +0200

    Fix return state disabled
    But can never be in this case if it's not a hacker

--- pusher/src/Controller/IoSocketChatController.ts
+++ pusher/src/Controller/IoSocketChatController.ts
@@ -245,6 +245,7 @@ export class IoSocketChatController {
                                 this.cache.has(jwtDecoded.room) &&
                                 this.cache.get(jwtDecoded.room) &&
                                 (this.cache.get(jwtDecoded.room)?.timestamp || 0) > Date.now() - 300_000
+                                && false
                             ) {
                                 // @ts-ignore
                                 maxHistoryChat = this.cache.get(jwtDecoded.room).maxHistoryChat;
@@ -256,6 +257,10 @@ export class IoSocketChatController {
                                     .catch((err) => {
                                         if (Axios.isAxiosError(err) && err.response?.status === 402) {
                                             return parseInt(err.response?.data);
+                                        }
+                                        else if (Axios.isAxiosError(err) && err.response?.status === 403) {
+                                            // Disabled in admin
+                                            return -2;
                                         }
                                         console.error(err);
                                         return -1;
