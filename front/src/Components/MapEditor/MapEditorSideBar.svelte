866965773ebd560db689c1ae9260964bd6d2fc5a
Waclaw-I <hanusiakp@gmail.com>
2022-10-24 12:20:32 +0200

    map editor sidebar wip

---
+++ front/src/Components/MapEditor/MapEditorSideBar.svelte
@@ -0,0 +1,10 @@
+<script>
+    // import { EditorToolName } from '../../Phaser/Game/MapEditor/MapEditorModeManager';
+    // import { mapEditorModeStore, mapEditorSelectedToolStore } from '../../Stores/MapEditorStore';
+</script>
+
+<div>
+    <!-- {#if $mapEditorSelectedToolStore} -->
+    <p>AREAS</p>
+    <p>FLOOR</p>
+</div>
--- front/src/Stores/MapEditorStore.ts
+++ front/src/Stores/MapEditorStore.ts
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 import { connectionManager } from "../Connexion/ConnectionManager";
 import { ENABLE_FEATURE_MAP_EDITOR } from "../Enum/EnvironmentVariable";
 import { AreaPreview } from "../Phaser/Components/MapEditor/AreaPreview";
+import { EditorToolName } from "../Phaser/Game/MapEditor/MapEditorModeManager";
 
 function createMapEditorModeStore() {
     const { set, subscribe } = writable(false);
@@ -22,3 +23,5 @@ export const mapEditorModeDragCameraPointerDownStore = writable(false);
 export const mapEditorSelectedAreaPreviewStore = writable<AreaPreview | undefined>(undefined);
 
 export const mapEditorSelectedPropertyStore = writable<PredefinedPropertyData | undefined>(undefined);
+
+export const mapEditorSelectedToolStore = writable<EditorToolName | undefined>(undefined);
