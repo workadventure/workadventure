cb54bdbf9fe610b9d8aaa9c25adf8469ba772d19
grégoire parant <gregoire@workadventu.re>
2022-10-26 10:24:50 +0200

    Hot Fix
     - Fix camera out when user have no microphone
     - Update size of sound meter
     - Fix sound meter crash

--- play/src/front/Components/SoundMeterWidget.svelte
+++ play/src/front/Components/SoundMeterWidget.svelte
@@ -18,20 +18,22 @@
 </script>
 
 <div class="tw-flex tw-justify-between tw-w-10 tw-h-10 tw-items-center {classcss ?? ''}" class:active={display}>
-    {#each volume as bar}
-        <div
-            class="tw-h-8 voice-meter-bar {barClass}"
-            class:tw-scale-y-[3%]={bar < 10}
-            class:tw-scale-y-[10%]={bar > 20 && bar < 30}
-            class:tw-scale-y-[20%]={bar > 30 && bar < 40}
-            class:tw-scale-y-[30%]={bar > 40 && bar < 50}
-            class:tw-scale-y-[40%]={bar > 50 && bar < 60}
-            class:tw-scale-y-[50%]={bar > 60 && bar < 70}
-            class:tw-scale-y-[60%]={bar > 70 && bar < 80}
-            class:tw-scale-y-[70%]={bar > 80 && bar < 90}
-            class:tw-scale-y-[80%]={bar > 90 && bar < 120}
-            class:tw-scale-y-[90%]={bar > 120 && bar < 140}
-            class:tw-scale-y-[100%]={bar >= 140}
-        />
-    {/each}
+    {#if volume}
+        {#each volume as bar}
+            <div
+                class="tw-h-4 voice-meter-bar {barClass}"
+                class:tw-scale-y-[3%]={bar < 10}
+                class:tw-scale-y-[10%]={bar > 20 && bar < 30}
+                class:tw-scale-y-[20%]={bar > 30 && bar < 40}
+                class:tw-scale-y-[30%]={bar > 40 && bar < 50}
+                class:tw-scale-y-[40%]={bar > 50 && bar < 60}
+                class:tw-scale-y-[50%]={bar > 60 && bar < 70}
+                class:tw-scale-y-[60%]={bar > 70 && bar < 80}
+                class:tw-scale-y-[70%]={bar > 80 && bar < 90}
+                class:tw-scale-y-[80%]={bar > 90 && bar < 120}
+                class:tw-scale-y-[90%]={bar > 120 && bar < 140}
+                class:tw-scale-y-[100%]={bar >= 140}
+            />
+        {/each}
+    {/if}
 </div>
