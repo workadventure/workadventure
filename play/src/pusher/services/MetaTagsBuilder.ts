3dfc701c302cc3e0458bd9fd8660fcfebe21e340
David Négrier <d.negrier@thecodingmachine.com>
2022-10-26 21:59:46 +0200

    Adding the ability to customize the manifest.json from the admin
    Adding the ability from the admin to have a custom manifest.json page for PWAs.

--- play/src/pusher/services/MetaTagsBuilder.ts
+++ play/src/pusher/services/MetaTagsBuilder.ts
@@ -75,6 +75,112 @@ export const MetaTagsDefaultValue: RequiredMetaTagsData = {
             src: "/static/images/favicons/android-icon-192x192.png",
         },
     ],
+    manifestIcons: [
+        {
+            src: "/static/images/favicons/apple-icon-57x57.png",
+            sizes: "57x57",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-114x114.png",
+            sizes: "114x114",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/apple-icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+        },
+
+        {
+            src: "/static/images/favicons/android-icon-36x36.png",
+            sizes: "36x36",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/android-icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/android-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/android-icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/android-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/android-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+        },
+
+        {
+            src: "/static/images/favicons/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+        },
+        {
+            src: "/static/images/favicons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+        },
+    ],
     appName: "WorkAdventure",
     shortAppName: "WA",
     themeColor: "#000000",
@@ -165,6 +271,7 @@ export class MetaTagsBuilder {
                 ? String(mapDescriptionProperty.value)
                 : MetaTagsDefaultValue.description,
             favIcons: MetaTagsDefaultValue.favIcons,
+            manifestIcons: MetaTagsDefaultValue.manifestIcons,
             appName: MetaTagsDefaultValue.appName,
             shortAppName: MetaTagsDefaultValue.shortAppName,
             themeColor: MetaTagsDefaultValue.themeColor,
