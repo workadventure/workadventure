d5c2bf2f5aa59d1c096a0da3247c02dbdb2266ea
grégoire parant <gregoire@workadventu.re>
2022-11-03 03:56:30 +0100

    Improve meta tag
     - Add author meta tag
     - Add provider meta tag

--- play/src/pusher/services/MetaTagsBuilder.ts
+++ play/src/pusher/services/MetaTagsBuilder.ts
@@ -8,6 +8,8 @@ import type { MetaTagsData, RequiredMetaTagsData, MapDetailsData } from "../../m
 export const MetaTagsDefaultValue: RequiredMetaTagsData = {
     title: "WorkAdventure",
     description: "Create your own digital office, Metaverse and meet online with the world.",
+    author: "WorkAdventure team",
+    provider: "WorkAdventure",
     favIcons: [
         {
             rel: "icon",
@@ -270,6 +272,8 @@ export class MetaTagsBuilder {
             description: mapDescriptionProperty?.value
                 ? String(mapDescriptionProperty.value)
                 : MetaTagsDefaultValue.description,
+            author: MetaTagsDefaultValue.author,
+            provider: MetaTagsDefaultValue.provider,
             favIcons: MetaTagsDefaultValue.favIcons,
             manifestIcons: MetaTagsDefaultValue.manifestIcons,
             appName: MetaTagsDefaultValue.appName,
