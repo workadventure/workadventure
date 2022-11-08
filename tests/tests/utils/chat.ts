fb0592878f0868d3bad829ec61dd402f7b541715
César Cardinale <c.cardinale@workadventu.re>
2022-11-08 14:22:29 +0100

    Update tests/tests/utils/chat.ts
    Co-authored-by: grégoire parant <gregoire@workadventu.re>

--- tests/tests/utils/chat.ts
+++ tests/tests/utils/chat.ts
@@ -133,7 +133,6 @@ class Chat {
 
     async expandForums(page: Page){
         await this.get(page).locator('#forumRooms div:has-text("Forums") button > .feather-chevron-up').click();
-        //await expect(page.locator('#forumRooms div:has-text("Forums") button .feather-chevron-up')).not.toHaveClass(/tw-rotate-180/);
     }
 
     async forumExist(page: Page, name: string) {
