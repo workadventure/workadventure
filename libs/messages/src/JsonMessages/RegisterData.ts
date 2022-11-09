ef03b9fc73485e3f7c3e5c9e1d82c6dfca8896e7
Piotr Hanusiak <hanusiakp@gmail.com>
2022-11-09 15:05:01 +0100

    added precommit prettier rule for JsonMessages

--- libs/messages/src/JsonMessages/RegisterData.ts
+++ libs/messages/src/JsonMessages/RegisterData.ts
@@ -6,13 +6,13 @@ import { z } from "zod";
  */
 
 export const isRegisterData = z.object({
-    roomUrl: z.string(),
-    email: z.string().nullable(),
-    organizationMemberToken: z.string().nullable(),
-    mapUrlStart: z.string(),
-    userUuid: z.string(),
-    authToken: z.string(),
-    messages: z.optional(z.array(z.unknown())),
+  roomUrl: z.string(),
+  email: z.string().nullable(),
+  organizationMemberToken: z.string().nullable(),
+  mapUrlStart: z.string(),
+  userUuid: z.string(),
+  authToken: z.string(),
+  messages: z.optional(z.array(z.unknown())),
 });
 
 export type RegisterData = z.infer<typeof isRegisterData>;
--- libs/messages/src/JsonMessages/RoomRedirect.ts
+++ libs/messages/src/JsonMessages/RoomRedirect.ts
@@ -7,10 +7,10 @@ import { extendApi } from "@anatine/zod-openapi";
  */
 
 export const isRoomRedirect = z.object({
-    redirectUrl: extendApi(z.string(), {
-        description: "The WorkAdventure URL to redirect to.",
-        example: "/_/global/example.com/start.json",
-    }),
+  redirectUrl: extendApi(z.string(), {
+    description: "The WorkAdventure URL to redirect to.",
+    example: "/_/global/example.com/start.json",
+  }),
 });
 
 export type RoomRedirect = z.infer<typeof isRoomRedirect>;
