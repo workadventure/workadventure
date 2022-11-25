9fba46181366fc1138496e8c0ab79d3640e572a1
David Négrier <d.negrier@thecodingmachine.com>
2022-11-25 10:46:33 +0100

    Fixing ts-node path

--- uploader/tests/startTestServer.ts
+++ uploader/tests/startTestServer.ts
@@ -2,7 +2,7 @@ import {fork} from "child_process";
 
 export default function(env: {}) {
     return fork("tests/testServer.ts", {
-        execArgv: ["./node_modules/.bin/ts-node"],
+        execArgv: ["ts-node"],
         env: env
     })
 }
