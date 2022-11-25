8965cc9188acd8cda911055c5b5239c9689420ac
David Négrier <d.negrier@thecodingmachine.com>
2022-11-25 11:18:45 +0100

    Working on uploader tests

--- uploader/tests/startTestServer.ts
+++ uploader/tests/startTestServer.ts
@@ -1,8 +1,20 @@
-import {fork} from "child_process";
+import {spawn} from "child_process";
 
 export default function(env: {}) {
-    return fork("tests/testServer.ts", {
-        execArgv: ["ts-node"],
+    const process = spawn("yarn", ['startTestServer']);
+
+    /*process.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });*/
+    process.stderr.on('data', (data) => {
+        console.warn(data.toString());
+    });
+    process.on('error', (err) => {
+        console.error('Failed to start subprocess.', err);
+    });
+    return process;
+    /*return fork("tests/testServer.ts", {
+        execArgv: ["yarn"],
         env: env
-    })
+    })*/
 }
