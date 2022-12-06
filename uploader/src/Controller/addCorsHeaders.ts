b3de413eb3c081a750cac80d8c86dc51497787de
David Négrier <d.negrier@thecodingmachine.com>
2022-12-07 00:02:51 +0100

    Migrating Uploader to use HyperExpress

--- uploader/src/Controller/addCorsHeaders.ts
+++
@@ -1,8 +0,0 @@
-import {HttpResponse} from "uWebSockets.js";
-import {CHAT_URL} from "../Enum/EnvironmentVariable";
-
-export function addCorsHeaders(res: HttpResponse): void {
-    res.writeHeader('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.writeHeader('access-control-allow-methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.writeHeader('access-control-allow-origin', CHAT_URL || "*");
-}
---
+++ uploader/src/Middlewares/Cors.ts
@@ -0,0 +1,20 @@
+import type { MiddlewareNext, MiddlewarePromise, Request, Response } from "hyper-express";
+import { ALLOWED_CORS_ORIGIN } from "../Enum/EnvironmentVariable";
+
+export function cors(req: Request, res: Response, next: MiddlewareNext): MiddlewarePromise | void {
+    if (ALLOWED_CORS_ORIGIN) {
+        res.setHeader(
+            "access-control-allow-headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization, Pragma, Cache-Control"
+        );
+        res.setHeader("access-control-allow-methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+        if (ALLOWED_CORS_ORIGIN === "*") {
+            res.setHeader("access-control-allow-origin", req.header("Origin"));
+        } else {
+            res.setHeader("access-control-allow-origin", ALLOWED_CORS_ORIGIN);
+        }
+        res.setHeader("access-control-allow-credentials", "true");
+    }
+
+    next();
+}
