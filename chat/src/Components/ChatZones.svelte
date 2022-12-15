cd66ad402645c014ce413792efd91b91876c1cf1
César Cardinale <c.cardinale@workadventu.re>
2022-12-15 15:18:51 +0100

    Add chat documentation (#2886)
    * Add chat doc
    
    * Fix chat documentation
    
    * Fix chat documentation var
    
    * Fix chat documentation var (2)
    
    * Fix
    
    * Rename liveRoom to chatZone
    
    * Add chat zone documentation
    
    * Fix
    
    * Fix chat doc

--- chat/src/Components/ChatZones.svelte
+++ chat/src/Components/ChatZones.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
     import ChatMucRoom from "./ChatMucRoom.svelte";
     import { MucRoom } from "../Xmpp/MucRoom";
-    import { showLivesStore } from "../Stores/ChatStore";
+    import { showChatZonesStore } from "../Stores/ChatStore";
 
-    export let liveRooms: MucRoom[];
+    export let chatZones: MucRoom[];
     export let searchValue: string;
 </script>
 
-{#if liveRooms.length > 0}
-    <div id="liveRooms" class="tw-border-b tw-border-solid tw-border-0 tw-border-transparent tw-border-b-light-purple">
+{#if chatZones.length > 0}
+    <div id="chatZones" class="tw-border-b tw-border-solid tw-border-0 tw-border-transparent tw-border-b-light-purple">
         <div
             class="tw-px-4 tw-py-1 tw-flex tw-items-center tw-cursor-pointer"
-            on:click|stopPropagation={() => showLivesStore.set(!$showLivesStore)}
+            on:click|stopPropagation={() => showChatZonesStore.set(!$showChatZonesStore)}
         >
             <span
                 class="tw-bg-light-blue tw-text-dark-purple tw-w-5 tw-h-5 tw-mr-3 tw-text-sm tw-font-semibold tw-flex tw-items-center tw-justify-center tw-rounded"
             >
-                {liveRooms.length}
+                {chatZones.length}
             </span>
-            <p class="tw-text-light-blue tw-my-2 tw-text-sm tw-flex-auto">Live zones</p>
+            <p class="tw-text-light-blue tw-my-2 tw-text-sm tw-flex-auto">Chat zones</p>
             <!--<button class="tw-text-lighter-purple">
                 <ChevronUpIcon class={`tw-transform tw-transition ${$showLivesStore ? "" : "tw-rotate-180"}`} />
             </button>
             -->
         </div>
         <div>
-            {#each liveRooms as liveRoom}
-                <ChatMucRoom mucRoom={liveRoom} {searchValue} />
+            {#each chatZones as chatZone}
+                <ChatMucRoom mucRoom={chatZone} {searchValue} />
             {/each}
         </div>
     </div>
