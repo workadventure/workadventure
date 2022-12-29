0f1cf113a10f27b2ac45bea91c771ae4260e34b8
grégoire parant <gregoire@workadventu.re>
2022-12-29 14:23:15 +0100

    Improve scripting add button action bar
     - Add new type for button pushed in action bar

--- play/src/front/Api/Events/Ui/ButtonActionBarEvent.ts
+++ play/src/front/Api/Events/Ui/ButtonActionBarEvent.ts
@@ -1,10 +1,22 @@
 import { z } from "zod";
 
-export const isAddButtonActionBarEvent = z.object({
+export const isAddClassicButtonActionBarEvent = z.object({
     id: z.string(),
     label: z.string(),
+    type: z.enum(["button"]).optional().default("button"),
 });
+
+export const isAddActionButtonActionBarEvent = z.object({
+    id: z.string(),
+    type: z.enum(["action"]),
+    imageSrc: z.string(),
+    toolTip: z.string(),
+});
+
+export const isAddButtonActionBarEvent = z.union([isAddClassicButtonActionBarEvent, isAddActionButtonActionBarEvent]);
 export type AddButtonActionBarEvent = z.infer<typeof isAddButtonActionBarEvent>;
+export type AddActionButtonActionBarEvent = z.infer<typeof isAddActionButtonActionBarEvent>;
+export type AddClassicButtonActionBarEvent = z.infer<typeof isAddClassicButtonActionBarEvent>;
 export type AddActionsButtonActionBarEventCallback = (event: AddButtonActionBarEvent) => void;
 
 export const isRemoveButtonActionBarEvent = z.object({
