7cc84b9f265e791eb43827403fc6ad7cc05454a8
David Négrier <d.negrier@thecodingmachine.com>
2023-01-06 19:13:14 +0100

    Migrating tests from map-storage to jest
    This way, we could test all the settings (both S3 and Disk storage)

--- uploader/tests/testServer.ts
+++ uploader/tests/testServer.ts
@@ -1,6 +1,6 @@
 import App from "../src/App"
 
-const SERVER_PORT = parseInt(process.env.SERVER_PORT || "7373")
+const SERVER_PORT = parseInt(process.env.HTTP_PORT || "7373")
 
 App.listen(SERVER_PORT, () => {
     console.log(`started on port ${SERVER_PORT}`)
