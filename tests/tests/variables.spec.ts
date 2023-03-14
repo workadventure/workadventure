dc0c4c9197d366163c467acf1a5069542c313538
David Négrier <d.negrier@thecodingmachine.com>
2023-03-14 15:04:16 +0100

    Fixing tests in single domain setup

--- tests/tests/variables.spec.ts
+++ tests/tests/variables.spec.ts
@@ -130,6 +130,7 @@ test.describe('Variables', () => {
   test('cache doesnt prevent setting a variable in case the map changes', async ({
     page,
     browser,
+    request,
   }) => {
     // Let's start by visiting a map that DOES not have the variable.
 
@@ -170,8 +171,11 @@ test.describe('Variables', () => {
 
     // Let's check the pusher getRooms endpoint returns 2 users on the map
     await expect.poll(async () => {
-      const rooms = await getPusherRooms();
-      return rooms['http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/Variables/Cache/variables_tmp.json'];
+      const rooms = await getPusherRooms(request);
+      const json = await rooms.json();
+      // Note: the value can come from http or https (in case of single domain test)
+      const users = (json['http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/Variables/Cache/variables_tmp.json'] ?? 0) + (json['https://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/Variables/Cache/variables_tmp.json'] ?? 0);
+      return users;
     }).toBe(2);
 
     await page2.close();
