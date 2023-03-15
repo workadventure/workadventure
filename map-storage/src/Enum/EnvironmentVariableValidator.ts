da51e319388a6420eeb3a17195361547f6f11673
David Négrier <d.negrier@thecodingmachine.com>
2023-03-15 10:19:43 +0100

    Refactoring environment variables validation
    - Splitting Zod validators for env vars in separate files (in back, play and map-storage)
    - Adding a small unit test
    - Putting utility functions in shared-lib

---
+++ map-storage/src/Enum/EnvironmentVariableValidator.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+import {
+    BoolAsString,
+    PositiveIntAsString,
+    toBool,
+    toNumber,
+} from "@workadventure/shared-utils/src/EnvironmentVariables/EnvironmentVariableUtils";
+
+const BasicEnvironmentVariables = z.object({
+    AWS_ACCESS_KEY_ID: z.string().optional(),
+    AWS_SECRET_ACCESS_KEY: z.string().optional(),
+    AWS_DEFAULT_REGION: z.string().optional(),
+    AWS_BUCKET: z.string().optional(),
+    AWS_URL: z.string().url().optional(),
+    AWS_ENDPOINT: z.string().url().optional(),
+    //UPLOADER_AWS_SIGNED_URL_EXPIRATION: PositiveIntAsString.optional(),
+    S3_UPLOAD_CONCURRENCY_LIMIT: PositiveIntAsString.optional().transform((val) => toNumber(val, 100)),
+    MAX_UNCOMPRESSED_SIZE: PositiveIntAsString.optional()
+        .transform((val) => toNumber(val, 1024 * 1024 * 1024))
+        .describe(
+            "The maximum size of an uploaded file. This the total size of the uncompressed file (not the ZIP file). Defaults to 1GB"
+        ),
+    USE_DOMAIN_NAME_IN_PATH: BoolAsString.optional()
+        .transform((val) => toBool(val, false))
+        .describe("If true, the domain name will be used as a top level directory when fetching/storing files"),
+    STORAGE_DIRECTORY: z
+        .string()
+        .optional()
+        .describe("Storage directory for the maps on physical disk. Used if S3 storage is not configured."),
+    CACHE_CONTROL: z
+        .string()
+        .optional()
+        .describe(
+            'The cache-control HTTP header to be used for "normal" ressources. Note: resources containing a hash in the name will be set to "immutable", whatever this setting is.'
+        ),
+});
+
+const BearerAuthEnvVariables = z.object({
+    AUTHENTICATION_STRATEGY: z.literal("Bearer"),
+    AUTHENTICATION_TOKEN: z.string().min(1),
+});
+
+const BasicAuthEnvVariables = z.object({
+    AUTHENTICATION_STRATEGY: z.literal("Basic"),
+    AUTHENTICATION_USER: z.string().min(1),
+    AUTHENTICATION_PASSWORD: z.string().min(1),
+});
+
+const DigestAuthEnvVariables = z.object({
+    AUTHENTICATION_STRATEGY: z.literal("Digest"),
+    AUTHENTICATION_USER: z.string().min(1),
+    AUTHENTICATION_PASSWORD: z.string().min(1),
+});
+
+const AuthEnvVariable = z.union([BearerAuthEnvVariables, BasicAuthEnvVariables, DigestAuthEnvVariables]);
+
+export const EnvironmentVariables = z.intersection(BasicEnvironmentVariables, AuthEnvVariable);
+
+export type EnvironmentVariables = z.infer<typeof EnvironmentVariables>;
---
+++ play/src/pusher/enums/EnvironmentVariableValidator.ts
@@ -0,0 +1,82 @@
+import { OpidWokaNamePolicy } from "@workadventure/messages";
+import { z } from "zod";
+import {
+    AbsoluteOrRelativeUrl,
+    BoolAsString,
+    PositiveIntAsString,
+    toBool,
+    toNumber,
+} from "@workadventure/shared-utils/src/EnvironmentVariables/EnvironmentVariableUtils";
+
+export const EnvironmentVariables = z.object({
+    // Pusher related environment variables
+    SECRET_KEY: z.string().min(1),
+    API_URL: z.string().min(1),
+    ADMIN_API_URL: AbsoluteOrRelativeUrl.optional(),
+    ADMIN_URL: AbsoluteOrRelativeUrl.optional(),
+    ADMIN_API_TOKEN: z.string().optional(),
+    ADMIN_SOCKETS_TOKEN: z.string().optional(),
+    CPU_OVERHEAT_THRESHOLD: PositiveIntAsString.optional().transform((val) => toNumber(val, 80)),
+    PUSHER_HTTP_PORT: PositiveIntAsString.optional().transform((val) => toNumber(val, 3000)),
+    SOCKET_IDLE_TIMER: PositiveIntAsString.optional()
+        .transform((val) => toNumber(val, 120))
+        .describe(
+            "maximum time (in second) without activity before a socket is closed. Should be greater than 60 seconds in order to cope for Chrome intensive throttling (https://developer.chrome.com/blog/timer-throttling-in-chrome-88/#intensive-throttling)"
+        ),
+    // Used only in development
+    VITE_URL: z.string().url().optional(),
+    // Use "*" to allow any domain
+    ALLOWED_CORS_ORIGIN: z.string().url().or(z.literal("*")).optional(),
+    PUSHER_URL: AbsoluteOrRelativeUrl.optional(),
+    PUBLIC_MAP_STORAGE_URL: AbsoluteOrRelativeUrl.optional(),
+    OPID_CLIENT_ID: z.string().optional(),
+    OPID_CLIENT_SECRET: z.string().optional(),
+    OPID_CLIENT_ISSUER: z.string().optional(),
+    OPID_PROFILE_SCREEN_PROVIDER: z.string().optional(),
+    OPID_SCOPE: z.string().optional(),
+    OPID_PROMPT: z.string().optional(),
+    OPID_USERNAME_CLAIM: z.string().optional(),
+    OPID_LOCALE_CLAIM: z.string().optional(),
+    OPID_LOGOUT_REDIRECT_URL: z.string().optional(),
+    USERNAME_POLICY: z.string().optional(),
+    DISABLE_ANONYMOUS: BoolAsString.optional().transform((val) => toBool(val, false)),
+    PROMETHEUS_AUTHORIZATION_TOKEN: z.string().optional(),
+    EJABBERD_DOMAIN: z.string().optional(),
+    EJABBERD_JWT_SECRET: z.string().optional(),
+    ENABLE_CHAT: BoolAsString.optional().transform((val) => toBool(val, true)),
+    ENABLE_CHAT_UPLOAD: BoolAsString.optional().transform((val) => toBool(val, true)),
+    ENABLE_CHAT_ONLINE_LIST: BoolAsString.optional().transform((val) => toBool(val, true)),
+    ENABLE_CHAT_DISCONNECTED_LIST: BoolAsString.optional().transform((val) => toBool(val, true)),
+    DEBUG_ERROR_MESSAGES: BoolAsString.optional().transform((val) => toBool(val, false)),
+    ENABLE_OPENAPI_ENDPOINT: BoolAsString.optional().transform((val) => toBool(val, false)),
+    START_ROOM_URL: z.string().optional(),
+
+    // Front related environment variables
+    DEBUG_MODE: BoolAsString.optional().transform((val) => toBool(val, false)),
+    UPLOADER_URL: AbsoluteOrRelativeUrl,
+    ICON_URL: AbsoluteOrRelativeUrl,
+    STUN_SERVER: z.string().optional(),
+    TURN_SERVER: z.string().optional(),
+    SKIP_RENDER_OPTIMIZATIONS: BoolAsString.optional().transform((val) => toBool(val, false)),
+    DISABLE_NOTIFICATIONS: BoolAsString.optional().transform((val) => toBool(val, false)),
+    TURN_USER: z.string().optional(),
+    TURN_PASSWORD: z.string().optional(),
+    JITSI_URL: z.string().optional(),
+    JITSI_PRIVATE_MODE: BoolAsString.optional().transform((val) => toBool(val, false)),
+    ENABLE_FEATURE_MAP_EDITOR: BoolAsString.optional().transform((val) => toBool(val, false)),
+    ENABLE_MAP_EDITOR_AREAS_TOOL: BoolAsString.optional().transform((val) => toBool(val, false)),
+    MAX_USERNAME_LENGTH: PositiveIntAsString.optional().transform((val) => toNumber(val, 10)),
+    MAX_PER_GROUP: PositiveIntAsString.optional().transform((val) => toNumber(val, 4)),
+    NODE_ENV: z.string().optional(),
+    CONTACT_URL: AbsoluteOrRelativeUrl.optional(),
+    POSTHOG_API_KEY: z.string().optional(),
+    POSTHOG_URL: z.string().url().optional().or(z.literal("")),
+    FALLBACK_LOCALE: z.string().optional(),
+    CHAT_URL: AbsoluteOrRelativeUrl,
+    OPID_WOKA_NAME_POLICY: OpidWokaNamePolicy.optional(),
+    ENABLE_REPORT_ISSUES_MENU: BoolAsString.optional().transform((val) => toBool(val, false)),
+    REPORT_ISSUES_URL: z.string().url().optional().or(z.literal("")),
+    LOGROCKET_ID: z.string().optional(),
+});
+
+export type EnvironmentVariables = z.infer<typeof EnvironmentVariables>;
