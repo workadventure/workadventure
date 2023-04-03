9ef37c8511be8e7418cc994d656ad1d1f9a99195
Nolway <faizeau.alexis@gmail.com>
2023-04-03 15:11:02 +0200

    Fix Room API client port

--- libs/room-api-clients/room-api-client-js/src/index.ts
+++ libs/room-api-clients/room-api-client-js/src/index.ts
@@ -2,7 +2,7 @@ export * from "./compiled_proto/room-api";
 import {ChannelCredentials, createChannel, createClient, Metadata} from "nice-grpc";
 import { RoomApiClient, RoomApiDefinition } from "./compiled_proto/room-api";
 
-export const createRoomApiClient = (apiKey: string, host = "room-api.workadventu.re", port = 80) => {
+export const createRoomApiClient = (apiKey: string, host = "room-api.workadventu.re", port = 443) => {
     const channel = createChannel(`${host}:${port}`, ChannelCredentials.createInsecure());
     const client: RoomApiClient = createClient(
         RoomApiDefinition,
