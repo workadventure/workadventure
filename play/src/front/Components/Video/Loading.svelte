af17ab54588535a8667231e388dcf6efee2dec96
César Cardinale <c.cardinale@workadventu.re>
2023-04-05 16:00:36 +0200

    🎨 Better loading view

--- play/src/front/Components/Video/Loading.svelte
+++ play/src/front/Components/Video/Loading.svelte
@@ -4,8 +4,17 @@
 
 <div id="loading" class="media-box-camera-off-size">
     <div class="wrapper">
+        <div aria-label="Loading..." role="status">
+            <svg class="tw-h-6 tw-w-6 tw-animate-spin tw-mr-3" viewBox="3 3 18 18">
+                <path
+                    class="tw-fill-brand-blue"
+                    d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+                />
+            </svg>
+        </div>
         Announcement
         <div
+            class="svg"
             style={`-webkit-mask: url(${megaphoneIcon}) no-repeat center;mask: url(${megaphoneIcon}) no-repeat center;`}
         />
     </div>
@@ -15,13 +24,11 @@
     #loading {
         @apply tw-h-fit tw-flex tw-bg-orange tw-relative tw-rounded tw-py-2 tw-my-2;
         transition: all 0.2s ease;
-
         .wrapper {
-            @apply tw-flex tw-flex-wrap tw-justify-items-center tw-items-center tw-justify-center tw-text-brand-blue tw-text-center tw-mx-auto tw-animate-pulse;
-            div {
-                @apply tw-bg-contain tw-bg-no-repeat tw-bg-center tw-bg-brand-blue tw-w-7 tw-h-7;
+            @apply tw-flex tw-flex-wrap tw-justify-items-center tw-items-center tw-justify-center tw-text-brand-blue tw-text-center tw-mx-auto;
+            .svg {
+                @apply tw-bg-contain tw-bg-no-repeat tw-bg-center tw-bg-brand-blue tw-w-7 tw-h-7 tw-ml-3;
                 content: "";
-                margin-left: 10px;
             }
         }
     }
