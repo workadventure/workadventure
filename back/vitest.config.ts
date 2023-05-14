11cfcb42cca397b30954d78dc64640238a3b8e60
César Cardinale <c.cardinale@workadventu.re>
2023-05-15 00:40:04 +0200

    ✨ New CI steps to combine json coverage

--- back/vitest.config.ts
+++ back/vitest.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vitest/config"; // eslint-disable-line import/no-u
 
 export default defineConfig({
     test: {
+        globals: true,
         coverage: {
             all: true,
             include: ["src/*.ts", "src/**/*.ts"],
+            exclude: ["src/Messages"],
+            reporter: ["json-summary"],
         },
+        reporters: ["json"],
     },
 });
