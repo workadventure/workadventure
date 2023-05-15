abff58739a4af069d20f430f7a5cf6246f418dd8
mlima95 <milan.lima@hotmail.fr>
2023-05-15 18:06:08 +0200

    Remove Object.fromEntries

--- play/src/pusher/services/QueryValidator.ts
+++ play/src/pusher/services/QueryValidator.ts
@@ -35,7 +35,12 @@ export function validateWebsocketQuery<T extends ZodObject<ZodRawShape>>(
     validator: T
 ): z.infer<T> | undefined {
     const urlSearchParams = new URLSearchParams(req.getQuery());
-    const params = Object.fromEntries(urlSearchParams.entries());
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    const params: any = {};
+    for (const key of [...new Set(urlSearchParams.keys())]) {
+        const values = urlSearchParams.getAll(key);
+        params[key] = values.length > 1 ? values : values[0];
+    }
     const result = validator.safeParse(params);
 
     if (result.success) {
