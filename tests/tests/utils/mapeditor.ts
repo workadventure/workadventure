fe21d7e9a15c5df4cd79288fc5980fbf42df573f
César Cardinale <c.cardinale@workadventu.re>
2023-05-19 10:20:02 +0200

    ✅ E2E tests

--- tests/tests/utils/mapeditor.ts
+++ tests/tests/utils/mapeditor.ts
@@ -3,7 +3,7 @@ import {expectInViewport} from "./viewport";
 
 class MapEditor {
     async openConfigureMyRoom(page: Page) {
-        await page.getByRole('button', {name: 'open tool ConfigureMyRoom'}).first().click();
+        await page.locator('section.side-bar-container .side-bar .tool-button').last().locator('button').click();
         await expectInViewport('.map-editor .configure-my-room', page);
     }
 }
