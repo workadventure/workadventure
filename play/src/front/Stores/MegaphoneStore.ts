30880d25dd4ac0474140035dbe01b021886b1778
César Cardinale <c.cardinale@workadventu.re>
2023-05-29 10:21:06 +0200

    ✅ Adding a bulk store

--- play/src/front/Stores/MegaphoneStore.ts
+++ play/src/front/Stores/MegaphoneStore.ts
@@ -1,4 +1,12 @@
-import { writable } from "svelte/store";
-
-export const megaphoneEnabledStore = writable<boolean>(false);
+import { derived, writable } from "svelte/store";
+import { requestedCameraState, requestedMicrophoneState } from "./MediaStore";
 export const megaphoneCanBeUsedStore = writable<boolean>(false);
+
+export const requestedMegaphoneStore = writable<boolean>(false);
+
+export const megaphoneEnabledStore = derived(
+    [requestedMegaphoneStore, requestedCameraState, requestedMicrophoneState],
+    ([$requestedMegaphoneStore, $requestedCameraState, $requestedMicrophoneState], set) => {
+        set($requestedMegaphoneStore && $requestedCameraState && $requestedMicrophoneState);
+    }
+);
