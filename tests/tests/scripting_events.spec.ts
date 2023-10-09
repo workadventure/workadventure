06fc3a594a4b8dc17c503ee165d1814d725beb78
David Négrier <d.negrier@thecodingmachine.com>
2023-10-09 10:15:09 +0200

    Adding test for event with several users

--- tests/tests/scripting_events.spec.ts
+++ tests/tests/scripting_events.spec.ts
@@ -3,7 +3,7 @@ import { login } from './utils/roles';
 import {evaluateScript} from "./utils/scripting";
 
 test.describe('Scripting API Events', () => {
-    test('test global events', async ({ page }) => {
+    test('test global events', async ({ page, browser }) => {
         // Go to 
         await page.goto(
             'http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/E2E/empty.json'
@@ -34,7 +34,48 @@ test.describe('Scripting API Events', () => {
         expect(eventTriggered).toBeTruthy();
 
         // 2. Connect 2 users and check that the events are triggered on the other user
+        const newBrowser = await browser.browserType().launch();
+        const page2 = await newBrowser.newPage();
 
+        await page2.goto(
+            'http://play.workadventure.localhost/_/global/maps.workadventure.localhost/tests/E2E/empty.json'
+        );
+
+        await login(page2, 'Bob');
+
+
+        let gotExpectedNotification = false;
+        page.on('console', async (msg) => {
+            const text = await msg.text();
+            //console.log(text);
+            if (text === 'Event triggered') {
+                gotExpectedNotification = true;
+            }
+        });
+
+        await evaluateScript(page, async () => {
+            await WA.onInit();
+            WA.event.onEventTriggered("key2").subscribe((event) => {
+                if (event.key !== "key2") {
+                    return;
+                }
+                if (event.value !== "value") {
+                    return;
+                }
+                if (event.senderId !== WA.player.playerId) {
+                    return;
+                }
+
+                console.log("Event triggered");
+            });
+        });
+
+        await evaluateScript(page2, async () => {
+            await WA.onInit();
+            WA.event.dispatchEvent("key2", "value");
+        });
+
+        await expect.poll(() => gotExpectedNotification).toBe(true);
 
     });
 });
