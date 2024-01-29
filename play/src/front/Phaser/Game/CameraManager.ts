3babce5f448ff30686a85dbe5f8cd69c26fd4e02
Gregoire Parant <gregoire@workadventu.re>
2024-01-29 17:49:01 +0100

    First test for exploration mode

--- play/src/front/Phaser/Game/CameraManager.ts
+++ play/src/front/Phaser/Game/CameraManager.ts
@@ -371,94 +371,118 @@ export class CameraManager extends Phaser.Events.EventEmitter {
     // Create function to define the camera on exploration mode. The camera can be moved anywhere on the map. The camera is not locked on the player. The camera can be zoomed in and out. The camera can be moved with the mouse. The camera can be moved with the keyboard. The camera can be moved with the touchpad.
     public setExplorationMode(): void {
         setTimeout(() => {
-            console.info('CameraManager => setExplorationMode => start setExplorationMode');
+            console.info("CameraManager => setExplorationMode => start setExplorationMode");
             this.stopFollow();
             this.cameraLocked = false;
             this.setCameraMode(CameraMode.Exploration);
 
-            this.scene.cameras.main.setBounds(-(this.cameraBounds.x), -(this.cameraBounds.y), this.cameraBounds.x * 3, this.cameraBounds.y * 3, true);
+            this.scene.cameras.main.setBounds(
+                -this.cameraBounds.x,
+                -this.cameraBounds.y,
+                this.cameraBounds.x * 3,
+                this.cameraBounds.y * 3,
+                true
+            );
             //this.camera.centerOn(this.cameraBounds.x, this.cameraBounds.y);
-            this.scene.input.keyboard?.on("keydown", (event: KeyboardEvent) => {
-                console.info('CameraManager => setExplorationMode => start setExplorationMode', this.scene.cameras.main.scrollX, this.scene.cameras.main.scrollY);
-                switch (event.key) {
-                    case "ArrowUp":
-                        this.scene.add.tween({
-                            targets: this.scene.cameras.main,
-                            scrollY: this.scene.cameras.main.scrollY - 32, 
-                            duration: 100,
-                            ease: 'Linear',
-                        });
-                        break;
-                    case "ArrowDown":
-                        this.scene.add.tween({
-                            targets: this.scene.cameras.main,
-                            scrollY: this.scene.cameras.main.scrollY + 32,
-                            duration: 100,
-                            ease: 'Linear',
-                        });
-                        break;
-                    case "ArrowLeft":
-                        this.scene.add.tween({
-                            targets: this.scene.cameras.main,
-                            scrollX: this.scene.cameras.main.scrollX - 32,
-                            duration: 100,
-                            ease: 'Linear',
-                        });
-                        break;
-                    case "ArrowRight":
-                        this.scene.add.tween({
-                            targets: this.scene.cameras.main,
-                            scrollX: this.scene.cameras.main.scrollX + 32,
-                            duration: 100,
-                            ease: 'Linear',
-                        });
-                        break;
-                }
-            }, this.scene);
+            this.scene.input.keyboard?.on(
+                "keydown",
+                (event: KeyboardEvent) => {
+                    console.info(
+                        "CameraManager => setExplorationMode => start setExplorationMode",
+                        this.scene.cameras.main.scrollX,
+                        this.scene.cameras.main.scrollY
+                    );
+                    switch (event.key) {
+                        case "ArrowUp":
+                            this.scene.add.tween({
+                                targets: this.scene.cameras.main,
+                                scrollY: this.scene.cameras.main.scrollY - 32,
+                                duration: 100,
+                                ease: "Linear",
+                            });
+                            break;
+                        case "ArrowDown":
+                            this.scene.add.tween({
+                                targets: this.scene.cameras.main,
+                                scrollY: this.scene.cameras.main.scrollY + 32,
+                                duration: 100,
+                                ease: "Linear",
+                            });
+                            break;
+                        case "ArrowLeft":
+                            this.scene.add.tween({
+                                targets: this.scene.cameras.main,
+                                scrollX: this.scene.cameras.main.scrollX - 32,
+                                duration: 100,
+                                ease: "Linear",
+                            });
+                            break;
+                        case "ArrowRight":
+                            this.scene.add.tween({
+                                targets: this.scene.cameras.main,
+                                scrollX: this.scene.cameras.main.scrollX + 32,
+                                duration: 100,
+                                ease: "Linear",
+                            });
+                            break;
+                    }
+                },
+                this.scene
+            );
 
-            this.scene.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-                console.log('setExplorationMode => pointerdown');
-                this.explorationMouseIsActive = true;
-                const xCenter = this.scene.cameras.main.scrollX + pointer.x
-                const yCenter = this.scene.cameras.main.scrollY + pointer.y
-                const game = HtmlUtils.querySelectorOrFail<HTMLCanvasElement>("#game canvas");
-                const followOffsetX = (xCenter - this.cameraBounds.x / 2) / this.scene.scale.zoom;
-                const followOffsetY = (yCenter - this.cameraBounds.y / 2) / this.scene.scale.zoom;
-                this.scene.add.tween({
-                    targets: this.scene.cameras.main,
-                    scrollX: followOffsetX,
-                    scrollY: followOffsetY,
-                    flipX: true,
-                    flipY: true,
-                    duration: 100,
-                    ease: 'Linear',
-                });
-            }, this.scene);
+            this.scene.input.on(
+                "pointerdown",
+                (pointer: Phaser.Input.Pointer) => {
+                    console.log("setExplorationMode => pointerdown");
+                    this.explorationMouseIsActive = true;
+                    const xCenter = this.scene.cameras.main.scrollX + pointer.x;
+                    const yCenter = this.scene.cameras.main.scrollY + pointer.y;
+                    const followOffsetX = (xCenter - this.cameraBounds.x / 2) / this.scene.scale.zoom;
+                    const followOffsetY = (yCenter - this.cameraBounds.y / 2) / this.scene.scale.zoom;
+                    this.scene.add.tween({
+                        targets: this.scene.cameras.main,
+                        scrollX: followOffsetX,
+                        scrollY: followOffsetY,
+                        flipX: true,
+                        flipY: true,
+                        duration: 100,
+                        ease: "Linear",
+                    });
+                },
+                this.scene
+            );
 
-            this.scene.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
-                console.log('setExplorationMode => pointermove => pointe', (pointer.distance / 100));
-                if (!this.explorationMouseIsActive) return;
-                
-                const xCenter = this.scene.cameras.main.scrollX + pointer.x
-                const yCenter = this.scene.cameras.main.scrollY + pointer.y
-                const game = HtmlUtils.querySelectorOrFail<HTMLCanvasElement>("#game canvas");
-                const followOffsetX = (xCenter - this.cameraBounds.x / 2) / this.scene.scale.zoom;
-                const followOffsetY = (yCenter - this.cameraBounds.y / 2) / this.scene.scale.zoom;
-                this.scene.add.tween({
-                    targets: this.scene.cameras.main,
-                    scrollX: followOffsetX,
-                    scrollY: followOffsetY,
-                    flipX: true,
-                    flipY: true,
-                    duration: 100,
-                    ease: 'Linear',
-                });
-            }, this.scene);
+            this.scene.input.on(
+                "pointermove",
+                (pointer: Phaser.Input.Pointer) => {
+                    console.log("setExplorationMode => pointermove => pointe", pointer.distance / 100);
+                    if (!this.explorationMouseIsActive) return;
 
-            this.scene.input.on('pointerup', () => {
-                console.log('setExplorationMode => pointerup');
-                this.explorationMouseIsActive = false;
-            }, this.scene);
+                    const xCenter = this.scene.cameras.main.scrollX + pointer.x;
+                    const yCenter = this.scene.cameras.main.scrollY + pointer.y;
+                    const followOffsetX = (xCenter - this.cameraBounds.x / 2) / this.scene.scale.zoom;
+                    const followOffsetY = (yCenter - this.cameraBounds.y / 2) / this.scene.scale.zoom;
+                    this.scene.add.tween({
+                        targets: this.scene.cameras.main,
+                        scrollX: followOffsetX,
+                        scrollY: followOffsetY,
+                        flipX: true,
+                        flipY: true,
+                        duration: 100,
+                        ease: "Linear",
+                    });
+                },
+                this.scene
+            );
+
+            this.scene.input.on(
+                "pointerup",
+                () => {
+                    console.log("setExplorationMode => pointerup");
+                    this.explorationMouseIsActive = false;
+                },
+                this.scene
+            );
 
             this.scene.markDirty();
         }, 3000);
