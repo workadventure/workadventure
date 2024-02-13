099f29dd6c0e01d66bcd06aacbe05f1073706797
Matthieu Rocher <rocher.matthieu@gmail.com>
2024-02-13 14:05:51 +0100

    features(Upload asset): Update ItemUpload to upload drop zone/upload button.

--- play/src/front/Components/MapEditor/EntityEditor/ItemUpload.svelte
+++ play/src/front/Components/MapEditor/EntityEditor/ItemUpload.svelte
@@ -2,10 +2,12 @@
     import {onDestroy} from "svelte";
     import {UploadEntityMessage, UploadEntityMessage_Direction} from "@workadventure/messages";
     import {mapEditorEntityUploadEventStore} from "../../../Stores/MapEditorStore";
+    import iconUploadWhite from "../../images/icon-upload-white.svg";
 
     let files: FileList | undefined = undefined;
     let fileToUpload: File | null = null;
     let fileInput: HTMLInputElement;
+    let dropZoneRef: HTMLDivElement;
 
     $: {
         if (files) {
@@ -54,6 +56,7 @@
                 fileToUpload = filesFromDropEvent.item(0);
             }
         }
+        dropZoneRef.classList.remove("tw-border-cyan-400");
     }
 
     onDestroy(() => {
@@ -63,19 +66,24 @@
 
 <div
     on:drop|preventDefault|stopPropagation={dropHandler}
-    on:dragover|preventDefault
-    class="tw-flex tw-flex-col tw-border-dotted tw-p-2 tw-rounded-md tw-items-center tw-justify-center tw-border-cyan-300"
+    on:dragover|preventDefault={() => dropZoneRef.classList.add("tw-border-cyan-400")}
+    on:dragleave|preventDefault={() => dropZoneRef.classList.remove("tw-border-cyan-400")}
+    bind:this={dropZoneRef}
+    class="hover:tw-cursor-pointer tw-flex tw-flex-col tw-border tw-border-dotted tw-rounded-md tw-items-center tw-justify-center"
 >
     <input id="upload" class="tw-hidden" type="file" accept="image/png" bind:files bind:this={fileInput} />
     {#if fileToUpload}
-        <img src={URL.createObjectURL(fileToUpload)} alt={fileToUpload.name} />
-        <button on:click={processFileToUpload}>Upload</button>
-        <button class="tw-text-red-500" on:click={initFileUpload}>Cancel</button>
+        <div class="tw-flex tw-flex-col tw-min-w-full tw-p-2 tw-m-0 tw-items-center tw-justify-center">
+            <img src={URL.createObjectURL(fileToUpload)} width="48" height="auto" alt={fileToUpload.name} />
+            <button on:click={processFileToUpload}>Upload</button>
+            <button class="tw-text-red-500" on:click={initFileUpload}>Cancel</button>
+        </div>
     {:else}
-        <label
-            class="tw-min-w-full tw-m-0 tw-text-center hover:tw-cursor-pointer tw-text-cyan-300"
-            for="upload"
-            draggable="true">Upload asset</label
+        <label class="tw-flex tw-flex-col tw-min-w-full tw-p-2 tw-m-0 tw-items-center tw-justify-center" for="upload">
+            <img class="hover:tw-cursor-pointer" src={iconUploadWhite} width="48" height="48" alt="iconUploadWhite" />
+            <p class="hover:tw-cursor-pointer tw-text-gray-400 tw-m-0">
+                Drop your image or <span class="hover:tw-cursor-pointer tw-text-white">click to add</span>
+            </p></label
         >
     {/if}
 </div>
