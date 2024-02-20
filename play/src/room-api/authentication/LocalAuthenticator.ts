dc9f7737bc56bb4402c953765825686839fecc4f
David Négrier <d.negrier@thecodingmachine.com>
2024-02-20 14:55:38 +0100

    Fixing path to assets in test WAM files

--- play/src/room-api/authentication/LocalAuthenticator.ts
+++ play/src/room-api/authentication/LocalAuthenticator.ts
@@ -1,5 +1,5 @@
 import { Status } from "@grpc/grpc-js/build/src/constants";
-import {FRONT_URL, ROOM_API_SECRET_KEY} from "../../pusher/enums/EnvironmentVariable";
+import { FRONT_URL, ROOM_API_SECRET_KEY } from "../../pusher/enums/EnvironmentVariable";
 import { GuardError } from "../types/GuardError";
 import { AuthenticatorInterface } from "./AuthenticatorInterface";
 
