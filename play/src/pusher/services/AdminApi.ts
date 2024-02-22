75b67b1843df2fb69ed1d69d59104f773dc4f13f
PizZaKatZe <pizzakatze@chaos-siegen.de>
2024-02-22 23:47:53 +0100

    fix(doc): Update swagger doc for /api/room/sameworld response

--- play/src/pusher/services/AdminApi.ts
+++ play/src/pusher/services/AdminApi.ts
@@ -699,7 +699,13 @@ class AdminApi implements AdminInterface {
          *                     type: string
          *                     description: Name of a room
          *                     example: "My office"
-         *                   url:
+         *                     required: true
+         *                   roomUrl:
+         *                     type: string
+         *                     description: URL of a room
+         *                     example: "http://example.com/@/teamSlug/worldSlug/room2Slug"
+         *                     required: true
+         *                   wamUrl:
          *                     type: string
          *                     description: URL of a room
          *                     example: "http://example.com/@/teamSlug/worldSlug/room2Slug"
