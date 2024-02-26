c62da1c0dda3c2b166420a9b5647f11cdacce51b
Gregoire Parant <gregoire@workadventu.re>
2024-02-26 22:10:28 +0100

    Fix test e2e

--- play/src/front/Phaser/Game/GameScene.ts
+++ play/src/front/Phaser/Game/GameScene.ts
@@ -137,7 +137,6 @@ import { requestedScreenSharingState } from "../../Stores/ScreenSharingStore";
 import { JitsiBroadcastSpace } from "../../Streaming/Jitsi/JitsiBroadcastSpace";
 import { notificationPlayingStore } from "../../Stores/NotificationStore";
 import { askDialogStore } from "../../Stores/MeetingStore";
-import { errorStore } from "../../Stores/ErrorStore";
 import { GameMapFrontWrapper } from "./GameMap/GameMapFrontWrapper";
 import { gameManager } from "./GameManager";
 import { EmoteManager } from "./EmoteManager";
@@ -1313,7 +1312,8 @@ export class GameScene extends DirtyScene {
                 // The errorMessageStream is completed in the RoomConnection. No need to unsubscribe.
                 //eslint-disable-next-line rxjs/no-ignored-subscription, svelte/no-ignored-unsubscribe
                 this.connection.errorMessageStream.subscribe((errorMessage) => {
-                    errorStore.addErrorMessage(errorMessage.message, { closable: true });
+                    // TODO: Create component to display error messages and test it
+                    //errorStore.addErrorMessage(errorMessage.message, { closable: true });
                 });
 
                 this.connectionAnswerPromiseDeferred.resolve(onConnect.room);
