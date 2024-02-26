cb4b4da5f8f65727847fdea85957130a36363d70
Matthieu Rocher <rocher.matthieu@gmail.com>
2024-02-26 13:41:26 +0100

    fix(Upload asset): Fix lint issue

--- play/src/front/Components/MapEditor/EntityEditor/EntityItem/EntityVariantColorPicker.svelte
+++ play/src/front/Components/MapEditor/EntityEditor/EntityItem/EntityVariantColorPicker.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-    export let colors:string[]
-    export let selectedColor:string;
-    export let onColorChange:(color:string)=>void
-
+    export let colors: string[];
+    export let selectedColor: string;
+    export let onColorChange: (color: string) => void;
 </script>
-
-
 
 {#if colors.length > 1}
     <p class="tw-m-0 tw-text-xxs">Colors</p>
@@ -13,7 +10,9 @@
         {#each colors as color (color)}
             <div class={`hover:tw-cursor-pointer`}>
                 <button
-                    class={`tw-w-4 tw-h-4 tw-rounded-full tw-p-0 ${selectedColor === color ? "tw-border-solid tw-border-yellow-400 tw-rounded-full" : ""}`}
+                    class={`tw-w-4 tw-h-4 tw-rounded-full tw-p-0 ${
+                        selectedColor === color ? "tw-border-solid tw-border-yellow-400 tw-rounded-full" : ""
+                    }`}
                     style="background-color: {color};"
                     on:click={() => onColorChange(color)}
                 />
