843da37395aad16c4667e73b8854afe2a9883025
ydiarra <yd97@hotmail.fr>
2024-02-29 17:15:59 +0100

    change npm lint-fix command

--- libs/map-editor/src/exportJsonSchema.ts
+++ libs/map-editor/src/exportJsonSchema.ts
@@ -6,5 +6,4 @@ const jsonSchema = zodToJsonSchema(WAMFileFormat, {
     name: "WAMFileFormat",
 });
 
-
 fs.writeFileSync(__dirname + "/../../../docs/schema/1.0/wam.json", JSON.stringify(jsonSchema, null, 2));
