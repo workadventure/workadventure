9a308f385bd4c53e88e4395be95a00ed7b094c8a
alexandre artus <artusalexandre@yahoo.fr>
2024-03-15 17:51:06 +0100

    for changing branch

--- play/src/front/Components/Cowebsites/JistiCowebsite.svelte
+++ play/src/front/Components/Cowebsites/JistiCowebsite.svelte
@@ -4,13 +4,13 @@
     import { JitsiCoWebsite } from "../../WebRtc/CoWebsite/JitsiCoWebsite";
     import { GameMapPropertiesListener } from "../../Phaser/Game/GameMapPropertiesListener";
 
-    onMount(() => {
-        // jitsiCowebsite = new JitsiCoWebsite(url, allowApi, allowPolicy, widthPercent, closable);
-    });
+    // onMount(() => {
+    // jitsiCowebsite = new JitsiCoWebsite(url, allowApi, allowPolicy, widthPercent, closable);
+    // });
 
     // Je ne sais pas s'il faut faire cela car dans le game map property listener on a déja un new jitsi qui se créé
 </script>
 
 <div class="container">
-    <!-- <iframe title= "{coWebsite.getId()}" src={coWebsite.getUrl().toString()} frameborder="0" height="100%" width="100%"></iframe> -->
+    <!-- <iframe title={coWebsite.getId()} src={coWebsite.getUrl().toString()} frameborder="0" height="100%" width="100%" /> -->
 </div>
