d7977cb6b3de199eee11e7dfa011e6a227fa1734
Matthintosh <rocher.matthieu@gmail.com>
2024-03-27 10:23:28 +0100

    feature: A user can claim a personal area.

--- play/src/front/Rules/PersonalAreaPropertyEditorRules/PersonalAreaPropertyEditorRules.ts
+++ play/src/front/Rules/PersonalAreaPropertyEditorRules/PersonalAreaPropertyEditorRules.ts
@@ -1,6 +1,9 @@
 import { PersonalAreaAccessClaimMode, PersonalAreaPropertyData } from "@workadventure/map-editor";
 import { derived, get, Readable } from "svelte/store";
-import { mapEditorSelectedAreaPreviewStore } from "../../Stores/MapEditorStore";
+import _ from "lodash";
+import { mapEditorAskToClaimPersonalAreaStore, mapEditorSelectedAreaPreviewStore } from "../../Stores/MapEditorStore";
+import { localUserStore } from "../../Connection/LocalUserStore";
+import { gameManager } from "../../Phaser/Game/GameManager";
 
 export type Option = {
     value: string;
@@ -107,5 +110,39 @@ export class PersonalAreaPropertyEditorRules {
         if (areaPreview !== undefined && personalAreaPropertyData !== undefined) {
             areaPreview.updateProperty({ id: personalAreaPropertyData.id, owner: "" });
         }
+    }
+
+    public claimArea() {
+        const areaToClaim = get(mapEditorAskToClaimPersonalAreaStore);
+        const userUUID = localUserStore.getLocalUser()?.uuid;
+        if (areaToClaim === undefined) {
+            console.error("No area to claim");
+            return;
+        }
+        if (userUUID === undefined) {
+            console.error("Unable to claim the area, your UUID is undefined");
+            return;
+        }
+        const areaPersonalPropertyData = areaToClaim.properties.find(
+            (property) => property.type === "personalAreaPropertyData"
+        );
+        if (!areaPersonalPropertyData) {
+            console.error("No area property data");
+            return;
+        }
+        const scene = gameManager.getCurrentGameScene();
+
+        if (scene === undefined) {
+            console.error("GameScene is undefined");
+        }
+
+        const oldAreaData = structuredClone(areaToClaim);
+        const property = areaToClaim.properties.find((property) => property.type === "personalAreaPropertyData");
+        if (property) {
+            _.merge(property, { ownerId: userUUID });
+        }
+
+        const mapEditorModeManager = scene.getMapEditorModeManager();
+        mapEditorModeManager.claimPersonalArea(areaToClaim, oldAreaData);
     }
 }
