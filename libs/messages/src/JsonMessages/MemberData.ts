e081b87a72feddb5c6d69e99e1a664c1c6f9e9f8
Matthintosh <rocher.matthieu@gmail.com>
2024-03-27 18:04:07 +0100

    fix: Email is nullable in MemberData

--- libs/messages/src/JsonMessages/MemberData.ts
+++ libs/messages/src/JsonMessages/MemberData.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const MemberData = z.object({
   id: z.string(),
   name: z.string().nullable(),
-  email: z.string(),
+  email: z.string().nullable(),
 });
 
 export type MemberData = z.infer<typeof MemberData>;
