bf298767307439080bd4c9371f36d1aebfa9b201
Gregoire Parant <gregoire@workadventu.re>
2024-04-10 16:56:33 +0200

    Fix lint

--- libs/shared-utils/src/Application/ExcalidrawService.ts
+++ libs/shared-utils/src/Application/ExcalidrawService.ts
@@ -1,10 +1,10 @@
 import { ExcalidrawException } from "./Exception/ExcalidrawException";
 
-export const validateLink = (url: URL, excalidrawDomains = ['excalidraw.com']) => {
+export const validateLink = (url: URL, excalidrawDomains = ["excalidraw.com"]) => {
     if (isExcalidrawLink(url, excalidrawDomains)) return true;
     throw new ExcalidrawException();
 };
 
-export const isExcalidrawLink = (url: URL, excalidrawDomains = ['excalidraw.com']) => {
+export const isExcalidrawLink = (url: URL, excalidrawDomains = ["excalidraw.com"]) => {
     return excalidrawDomains?.includes(url.hostname);
 };
