5778d269c0ee338c163d1512a03d57243c39ea85
Antoine Rubeo-lisa <arotakerpro@gmail.com>
2024-04-23 17:11:29 +0200

    My first development

---
+++ play/src/front/Components/ActionBar/AppModal/AppModal.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+
+
+</script>
+
+<div class="tw-backdrop-blur tw-flex tw-justify-center tw-m-auto tw-absolute tw-left-0 tw-right-0 tw-bottom-0 tw-bg-dark-purple/75" style="margin-bottom: 4.5rem; height: 100px; width: 100px;">
+
+</div>
+
+
+<style lang="scss">
+
+.modal{
+    height: 100px;
+    width: 100px;
+    background-color: red !important;
+}
+
+</style>
