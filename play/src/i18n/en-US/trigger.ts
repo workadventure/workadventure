0dd5a75a6d97186df402475ccef9b48d804e4508
Gregoire Parant <gregoire@workadventu.re>
2024-04-29 13:19:47 +0200

    Update text

--- play/src/i18n/en-US/trigger.ts
+++ play/src/i18n/en-US/trigger.ts
@@ -1,16 +1,16 @@
 import type { BaseTranslation } from "../i18n-types";
 
 const trigger: BaseTranslation = {
-    cowebsite: `[SPACE] or touch here to open web site 👀`,
-    jitsiRoom: "[SPACE] or touch here to enter Jitsi Meet room 👀",
-    newTab: "[SPACE] or touch here to open web site in new tab 👀",
+    cowebsite: `[SPACE] to open web site 👀`,
+    jitsiRoom: "[SPACE] to enter Jitsi 👀",
+    newTab: "[SPACE] to open new tab 👀",
     object: "[SPACE] to interact with it 👀",
     spaceKeyboard: "[SPACE]",
     mobile: {
-        cowebsite: "Touch here to open web site 👀",
-        jitsiRoom: "Touch here to enter Jitsi Meet room 👀",
-        newTab: "Touch here to open web site in new tab 👀",
-        object: "Touch here to interact with it 👀",
+        cowebsite: "👆 to open web site 👀",
+        jitsiRoom: "👆 to enter Jitsi 👀",
+        newTab: "👆 to open web new tab 👀",
+        object: "👆 to interact with it 👀",
     },
 };
 
--- play/src/i18n/fr-FR/trigger.ts
+++ play/src/i18n/fr-FR/trigger.ts
@@ -2,16 +2,16 @@ import type { DeepPartial } from "../DeepPartial";
 import type { Translation } from "../i18n-types";
 
 const trigger: DeepPartial<Translation["trigger"]> = {
-    cowebsite: "[ESPACE] ou ici pour ouvrir le site Web 👀",
-    jitsiRoom: "[ESPACE] ou ici pour entrer dans la salle conférence Jitsi 👀",
-    newTab: "[ESPACE] ou ici pour ouvrir le site Web dans un nouvel onglet 👀",
+    cowebsite: "[ESPACE] pour ouvrir le site Web 👀",
+    jitsiRoom: "[ESPACE] pour entrer dans Jitsi 👀",
+    newTab: "[ESPACE] pour ouvrir un nouvel onglet 👀",
     object: "[ESPACE] pour interagir avec 👀",
     spaceKeyboard: "[ESPACE]",
     mobile: {
-        cowebsite: "Clique ici pour ouvrir le site Web 👀",
-        jitsiRoom: "Clique ici pour entrer dans la salle conférence Jitsi 👀",
-        newTab: "Clique ici pour ouvrir le site Web dans un nouvel onglet 👀",
-        object: "Clique ici pour interagir avec 👀",
+        cowebsite: "👆 pour ouvrir le site Web 👀",
+        jitsiRoom: "👆 pour entrer dans Jitsi 👀",
+        newTab: "👆 pour ouvrir un nouvel onglet 👀",
+        object: "👆 pour interagir avec 👀",
     },
 };
 
