2d8848547b098de6f8d8d6f1a1d0838c299927ea
Matthintosh <rocher.matthieu@gmail.com>
2024-05-31 16:56:47 +0200

    fix: Remove console.log

--- play/src/front/Chat/Components/Avatar.svelte
+++ play/src/front/Chat/Components/Avatar.svelte
@@ -2,17 +2,14 @@
     export let avatarUrl: string | null = null;
     export let fallbackFirstLetter = "A";
 
-    console.debug(fallbackFirstLetter)
 
-    const colors = ["CB4288","C5458A","BF488C","B94C8D","B34F8F","AD5291","A75593","A15995","9B5C96","955F98","8F629A","89669C","83699E","7D6C9F","776FA1","7173A3","6B76A5","6579A7","5F7CA8","5980AA","5383AC","4D86AE","4789B0","418DB1","3B90B3","3593B5"]
-    function getColorByFirstLetter(str:string) {
-        if (!str) {
-            return 'black'; // Default color for invalid input
+    const colors = ["CB4288", "C5458A", "BF488C", "B94C8D", "B34F8F", "AD5291", "A75593", "A15995", "9B5C96", "955F98", "8F629A", "89669C", "83699E", "7D6C9F", "776FA1", "7173A3", "6B76A5", "6579A7", "5F7CA8", "5980AA", "5383AC", "4D86AE", "4789B0", "418DB1", "3B90B3", "3593B5"];
+
+    function getColorByFirstLetter(firstLetter: string) {
+        if (!firstLetter) {
+            return "black";
         }
-
-        const indexColor = str.toLowerCase().charCodeAt(0) - 97;
-        console.debug(colors[indexColor])
-
+        const indexColor = firstLetter.toLowerCase().charCodeAt(0) - 97;
         return colors[indexColor];
     }
 
