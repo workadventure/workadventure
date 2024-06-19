6df88ab735b25e2df7605077e68672b3d8f8be74
Matthintosh <rocher.matthieu@gmail.com>
2024-06-19 12:30:34 +0200

    feature: Add e2e tests for message edition

--- play/src/front/Chat/Components/Room/MessageEdition.svelte
+++ play/src/front/Chat/Components/Room/MessageEdition.svelte
@@ -24,15 +24,23 @@
 </script>
 
 <div>
-    <input class="tw-rounded-md tw-p-1" bind:value={inputValue} />
+    <input class="tw-rounded-md tw-p-1" bind:value={inputValue} data-testid="editMessageInput" />
     {#if editError}
         <p class="tw-text-red-500 tw-text-xxs tw-p-0 tw-m-0">Unable to edit message. Try again.</p>
     {/if}
     <div class="tw-flex tw-flew-row tw-gap-2 tw-justify-end">
-        <button class="tw-text-xs tw-text-red-500 tw-p-0 tw-m-0" on:click={() => selectedChatMessageToEdit.set(null)}>
+        <button
+            class="tw-text-xs tw-text-red-500 tw-p-0 tw-m-0"
+            data-testid="cancelMessageEditionButton"
+            on:click={() => selectedChatMessageToEdit.set(null)}
+        >
             <IconX font-size={16} />
         </button>
-        <button class="tw-text-xs tw-text-green-500 tw-p-0 tw-m-0" on:click={() => editMessage(inputValue)}>
+        <button
+            class="tw-text-xs tw-text-green-500 tw-p-0 tw-m-0"
+            data-testid="saveMessageEditionButton"
+            on:click={() => editMessage(inputValue)}
+        >
             <IconCheck font-size={16} />
         </button>
     </div>
