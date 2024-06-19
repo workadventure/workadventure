8e9dd4dc04f3c931b920e08e4a160da69ff5e3a4
Gregoire Parant <gregoire@workadventu.re>
2024-06-19 15:44:30 +0200

    Fix pretty

--- libs/shared-utils/src/Application/CardsService.ts
+++ libs/shared-utils/src/Application/CardsService.ts
@@ -6,30 +6,31 @@ export const validateLink = (url: URL) => {
 };
 
 export const isCardsLink = (url: URL) => {
-    return (
-        isCardsPublicLink(url) ||
-        isCardsConnectedLink(url)
-    );
+    return isCardsPublicLink(url) || isCardsConnectedLink(url);
 };
 
 export const getCardsLink = (url: URL, userToken?: string | null): string => {
-    if (isCardsPublicLink(url)){
+    if (isCardsPublicLink(url)) {
         return url.toString();
-    }else if(isCardsConnectedLink(url)){
+    } else if (isCardsConnectedLink(url)) {
         if (userToken != undefined) url.searchParams.set("token", userToken);
         return url.toString();
-    }else{
+    } else {
         throw new CardsLinkException();
     }
 };
 
 export const isCardsPublicLink = (url: URL) => {
-    return ["app.cards-microlearning.com"].includes(url.hostname);  
+    return ["app.cards-microlearning.com"].includes(url.hostname);
 };
 
 export const isCardsConnectedLink = (url: URL) => {
-    return ["member.workadventu.re", "member.staging.workadventu.re", "member.workadventure.localhost"].includes(url.hostname) &&
-    url.pathname.indexOf("cards") !== -1 &&
-    url.searchParams.has("tenant") &&
-    url.searchParams.has("learning");
+    return (
+        ["member.workadventu.re", "member.staging.workadventu.re", "member.workadventure.localhost"].includes(
+            url.hostname
+        ) &&
+        url.pathname.indexOf("cards") !== -1 &&
+        url.searchParams.has("tenant") &&
+        url.searchParams.has("learning")
+    );
 };
