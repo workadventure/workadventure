127e92911d501c0b672a7e6cde15da57a2176cc3
Matthintosh <rocher.matthieu@gmail.com>
2024-06-25 17:20:43 +0200

    fix: Trying to fix docker compose command instead of docker-compose

--- tests/tests/utils/containers.ts
+++ tests/tests/utils/containers.ts
@@ -1,131 +1,163 @@
-import { execSync } from 'child_process';
-import { Page } from '@playwright/test';
-import Dockerode from 'dockerode';
-import {play_url} from "./urls";
+import { execSync } from "child_process";
+import { Page } from "@playwright/test";
+import Dockerode from "dockerode";
+import { play_url } from "./urls";
 
 /**
  * Execute Docker compose, passing the correct host directory
  */
 export function dockerCompose(command: string) {
-    let param = '';
-    const overrideDockerCompose = process.env.OVERRIDE_DOCKER_COMPOSE;
+  let param = "";
+  const overrideDockerCompose = process.env.OVERRIDE_DOCKER_COMPOSE;
 
-    if (overrideDockerCompose) {
-        param += ' -f '+overrideDockerCompose;
+  if (overrideDockerCompose) {
+    param += " -f " + overrideDockerCompose;
+  }
+
+  return execSync(
+    "docker compose -f docker-compose.yaml -f docker-compose-oidc.yaml " +
+      param +
+      " " +
+      command,
+    {
+      cwd: __dirname + "/../../../",
     }
-
-    return execSync('docker-compose -f docker-compose.yaml -f docker-compose-oidc.yaml '+param+' '+command, {
-        cwd: __dirname + '/../../../'
-    });
+  );
 }
 
 /**
  * Returns a container ID based on the container name.
  */
-export async function findContainer(name: string): Promise<Dockerode.ContainerInfo> {
-    const docker = new Dockerode();
+export async function findContainer(
+  name: string
+): Promise<Dockerode.ContainerInfo> {
+  const docker = new Dockerode();
 
-    const containers = await docker.listContainers();
+  const containers = await docker.listContainers();
 
-    const foundContainer = containers.find((container) => container.State === 'running' && container.Names.find((containerName) => containerName.includes(name)));
+  const foundContainer = containers.find(
+    (container) =>
+      container.State === "running" &&
+      container.Names.find((containerName) => containerName.includes(name))
+  );
 
-    if (foundContainer === undefined) {
-        throw new Error('Could not find a running container whose name contains "'+name+'"');
-    }
+  if (foundContainer === undefined) {
+    throw new Error(
+      'Could not find a running container whose name contains "' + name + '"'
+    );
+  }
 
-    return foundContainer;
+  return foundContainer;
 }
 
-export async function stopContainer(container: Dockerode.ContainerInfo): Promise<void> {
-    const docker = new Dockerode();
+export async function stopContainer(
+  container: Dockerode.ContainerInfo
+): Promise<void> {
+  const docker = new Dockerode();
 
-    await docker.getContainer(container.Id).stop();
+  await docker.getContainer(container.Id).stop();
 }
 
-export async function startContainer(container: Dockerode.ContainerInfo): Promise<void> {
-    const docker = new Dockerode();
+export async function startContainer(
+  container: Dockerode.ContainerInfo
+): Promise<void> {
+  const docker = new Dockerode();
 
-    await docker.getContainer(container.Id).start();
+  await docker.getContainer(container.Id).start();
 }
 
 export async function rebootBack(): Promise<void> {
-    dockerCompose('up --force-recreate -d back');
+  dockerCompose("up --force-recreate -d back");
 }
 
 export function rebootTraefik(): void {
-    dockerCompose('up --force-recreate -d reverse-proxy');
+  dockerCompose("up --force-recreate -d reverse-proxy");
 }
 
 export async function rebootPlay(): Promise<void> {
-    dockerCompose('up --force-recreate -d play');
+  dockerCompose("up --force-recreate -d play");
 }
 
 export async function upPlay(): Promise<void> {
-    dockerCompose('up -d play');
+  dockerCompose("up -d play");
 }
 
 export async function upMapStorage(): Promise<void> {
-    dockerCompose('up -d map-storage');
+  dockerCompose("up -d map-storage");
 }
 
 export async function gotoWait200(page: Page, url: string): Promise<void> {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const targetUrl = new URL(url, play_url).toString();
-        const response = await page.goto(targetUrl);
-        // Because we just rebooted play, we might get HTTP 502 errors from Traefik.
-        // Let's wait for a correct answer.
-        if (response.ok()) {
-            break;
-        }
-        // eslint-disable-next-line playwright/no-wait-for-timeout
-        await page.waitForTimeout(1_000);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const targetUrl = new URL(url, play_url).toString();
+    const response = await page.goto(targetUrl);
+    // Because we just rebooted play, we might get HTTP 502 errors from Traefik.
+    // Let's wait for a correct answer.
+    if (response.ok()) {
+      break;
     }
+    // eslint-disable-next-line playwright/no-wait-for-timeout
+    await page.waitForTimeout(1_000);
+  }
 }
 
 export async function stopPlay(): Promise<void> {
-    dockerCompose('stop play');
+  dockerCompose("stop play");
 }
 
 export async function stopEjabberd(): Promise<void> {
-    dockerCompose('stop ejabberd');
+  dockerCompose("stop ejabberd");
 }
 
 export async function rebootEjabberd(): Promise<void> {
-    dockerCompose('up --force-recreate -d ejabberd');
+  dockerCompose("up --force-recreate -d ejabberd");
 }
 
 export async function resetRedis(): Promise<void> {
-    dockerCompose('stop redis');
-    dockerCompose('rm -f redis');
-    dockerCompose('up --force-recreate -d redis');
+  dockerCompose("stop redis");
+  dockerCompose("rm -f redis");
+  dockerCompose("up --force-recreate -d redis");
 }
 
 export function stopRedis(): void {
-    dockerCompose('stop redis');
+  dockerCompose("stop redis");
 }
 
 export function startRedis(): void {
-    dockerCompose('start redis');
+  dockerCompose("start redis");
 }
 
 // create function to check that "play", "back", "chat", "map-storage" services are up and running
 export function checkMapStorageService(): boolean {
-    console.log('Checking map storage services',
-        dockerCompose('logs --tail=1000 map-storage').toString(),
-    );
-    const isUp = dockerCompose('logs --tail=1000 map-storage').indexOf('Application is running') !== -1;
-    if(!isUp) upMapStorage();
-    return dockerCompose('logs --tail=1000 map-storage').indexOf('Application is running') !== -1;
+  console.log(
+    "Checking map storage services",
+    dockerCompose("logs --tail=1000 map-storage").toString()
+  );
+  const isUp =
+    dockerCompose("logs --tail=1000 map-storage").indexOf(
+      "Application is running"
+    ) !== -1;
+  if (!isUp) upMapStorage();
+  return (
+    dockerCompose("logs --tail=1000 map-storage").indexOf(
+      "Application is running"
+    ) !== -1
+  );
 }
 
 // create function to check that "play", "back", "chat", "map-storage" services are up and running
 export function checkMapPlayService(): boolean {
-    console.log('Checking play services',
-        dockerCompose('logs --tail=1000 play').toString(),
-    );
-    const isUp = dockerCompose('logs --tail=1000 play').indexOf('WorkAdventure Pusher started on port') !== -1 && 
-            dockerCompose('logs --tail=1000 play').indexOf('RoomAPI starting on port') !== -1;
-    if(!isUp) upPlay();
-    return isUp;
+  console.log(
+    "Checking play services",
+    dockerCompose("logs --tail=1000 play").toString()
+  );
+  const isUp =
+    dockerCompose("logs --tail=1000 play").indexOf(
+      "WorkAdventure Pusher started on port"
+    ) !== -1 &&
+    dockerCompose("logs --tail=1000 play").indexOf(
+      "RoomAPI starting on port"
+    ) !== -1;
+  if (!isUp) upPlay();
+  return isUp;
 }
