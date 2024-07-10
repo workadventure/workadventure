a7d85a1406ba929763d77c96145eebcc1adf0d0a
Antoine <a.rubeo-lisa@workadventu.re>
2024-07-10 18:27:29 +0200

    remove console log

--- play/src/front/Components/Input/InputWorldTags.svelte
+++ play/src/front/Components/Input/InputWorldTags.svelte
@@ -3,7 +3,6 @@
     import InputTags from "./InputTags.svelte";
 
     async function searchWorldTags(filterText: string) {
-        console.log("on searchWorldTags");
         if (filterText.length < 1) return [];
         const connection = gameManager.getCurrentGameScene().connection;
         if (connection) {
