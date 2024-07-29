986e7222851e4700ca1e79b4103b325e432c8cec
Matthieu Rocher <rocher.matthieu@gmail.com>
2024-07-29 17:20:11 +0200

    fix: Prevent a user to create multiple direct room by adding a loading indicator on direct room access. (#4094)

--- play/src/front/Chat/Components/UserList/User.svelte
+++ play/src/front/Chat/Components/UserList/User.svelte
@@ -9,7 +9,7 @@
     import { chatSearchBarValue, navChat, selectedRoom } from "../../Stores/ChatStore";
     import { gameManager } from "../../../Phaser/Game/GameManager";
     import UserActionButton from "./UserActionButton.svelte";
-    import { IconShield, IconUsers } from "@wa-icons";
+    import { IconLoader, IconShield, IconUsers } from "@wa-icons";
 
     export let user: ChatUser;
 
@@ -44,11 +44,20 @@
         }
     }
 
+    let loadingDirectRoomAccess = false;
     const openChat = async () => {
         if (isMe) return;
 
         let room: ChatRoom | undefined = chatConnection.getDirectRoomFor(id);
-        if (!room) room = await chatConnection.createDirectRoom(id);
+        if (!room)
+            try {
+                loadingDirectRoomAccess = true;
+                room = await chatConnection.createDirectRoom(id);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                loadingDirectRoomAccess = false;
+            }
 
         if (!room) return;
 
@@ -59,76 +68,82 @@
     };
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-
-<div
-    on:click|stopPropagation={openChat}
-    class="tw-text-md tw-flex tw-gap-2 tw-flex-row tw-items-center tw-justify-between hover:tw-bg-white hover:tw-bg-opacity-10 hover:tw-rounded-md hover:!tw-cursor-pointer tw-p-1"
->
-    <div class={`wa-chat-item ${isAdmin ? "admin" : "user"}  tw-cursor-default`}>
-        <div
-            class={`tw-relative wa-avatar ${!$userStatus && "tw-opacity-50"}  tw-cursor-default`}
-            style={`background-color: ${color}`}
-        >
-            <div class="wa-container tw-cursor-default">
-                <img
-                    class="tw-w-full tw-cursor-default"
-                    style="image-rendering: pixelated;"
-                    src={avatarUrl}
-                    alt="Avatar"
-                />
-            </div>
-            {#if $userStatus && user.roomName}
-                <span
-                    title={getNameOfAvailabilityStatus($userStatus)}
-                    class={`status tw-w-4 tw-h-4 tw-cursor-default tw-block tw-rounded-full tw-absolute tw-right-0 tw-top-0 tw-transform tw-translate-x-2 -tw-translate-y-1 tw-border-solid tw-border-2 tw-border-light-purple`}
-                    style="--color:{getColorHexOfStatus($userStatus)}"
-                />
-            {/if}
-        </div>
-        <div class={`tw-flex-auto tw-ml-3 ${!$userStatus && "tw-opacity-50"}  tw-cursor-default`}>
-            <h1 class={`tw-text-sm tw-font-bold tw-mb-0  tw-cursor-default`}>
-                {#each chunks as chunk (chunk.key)}
-                    <span class={`${chunk.match ? "tw-text-light-blue" : ""}  tw-cursor-default`}>{chunk.text}</span>
-                {/each}
-                {#if username && username.match(/\[\d*]/)}
-                    <span class="tw-font-light tw-text-xs tw-text-gray tw-cursor-default">
-                        #{username
-                            .match(/\[\d*]/)
-                            ?.join()
-                            ?.replace("[", "")
-                            ?.replace("]", "")}
-                    </span>
-                {/if}
-                {#if isAdmin}
-                    <span class="tw-text-warning" title={$LL.chat.role.admin()}>
-                        <IconShield font-size="13" />
-                    </span>
-                {/if}
-                {#if isMember}
-                    <span title={$LL.chat.role.member()}>
-                        <IconUsers font-size="13" />
-                    </span>
-                {/if}
-            </h1>
-            <p class="tw-text-xs tw-mb-0 tw-font-condensed tw-opacity-75 tw-cursor-default tw-self-end">
-                {#if isMe}
-                    {$LL.chat.you()}
-                {:else if $userStatus}
-                    {getNameOfAvailabilityStatus($userStatus ?? 0)}
-                {:else}
-                    {$LL.chat.userList.disconnected()}
-                {/if}
-            </p>
-        </div>
+{#if loadingDirectRoomAccess}
+    <div class="tw-min-h-[60px] tw-text-md tw-flex tw-gap-2 tw-justify-center tw-flex-row tw-items-center tw-p-1">
+        <IconLoader class="tw-animate-spin" />
     </div>
-    {#if $userStatus && !isMe}
-        <UserActionButton {user} />
-    {/if}
-</div>
+{:else}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+        on:click|stopPropagation={openChat}
+        class="tw-text-md tw-flex tw-gap-2 tw-flex-row tw-items-center tw-justify-between hover:tw-bg-white hover:tw-bg-opacity-10 hover:tw-rounded-md hover:!tw-cursor-pointer tw-p-1"
+    >
+        <div class={`wa-chat-item ${isAdmin ? "admin" : "user"}  tw-cursor-default`}>
+            <div
+                class={`tw-relative wa-avatar ${!$userStatus && "tw-opacity-50"}  tw-cursor-default`}
+                style={`background-color: ${color}`}
+            >
+                <div class="wa-container tw-cursor-default">
+                    <img
+                        class="tw-w-full tw-cursor-default"
+                        style="image-rendering: pixelated;"
+                        src={avatarUrl}
+                        alt="Avatar"
+                    />
+                </div>
+                {#if $userStatus && user.roomName}
+                    <span
+                        title={getNameOfAvailabilityStatus($userStatus)}
+                        class={`status tw-w-4 tw-h-4 tw-cursor-default tw-block tw-rounded-full tw-absolute tw-right-0 tw-top-0 tw-transform tw-translate-x-2 -tw-translate-y-1 tw-border-solid tw-border-2 tw-border-light-purple`}
+                        style="--color:{getColorHexOfStatus($userStatus)}"
+                    />
+                {/if}
+            </div>
+            <div class={`tw-flex-auto tw-ml-3 ${!$userStatus && "tw-opacity-50"}  tw-cursor-default`}>
+                <h1 class={`tw-text-sm tw-font-bold tw-mb-0  tw-cursor-default`}>
+                    {#each chunks as chunk (chunk.key)}
+                        <span class={`${chunk.match ? "tw-text-light-blue" : ""}  tw-cursor-default`}>{chunk.text}</span
+                        >
+                    {/each}
+                    {#if username && username.match(/\[\d*]/)}
+                        <span class="tw-font-light tw-text-xs tw-text-gray tw-cursor-default">
+                            #{username
+                                .match(/\[\d*]/)
+                                ?.join()
+                                ?.replace("[", "")
+                                ?.replace("]", "")}
+                        </span>
+                    {/if}
+                    {#if isAdmin}
+                        <span class="tw-text-warning" title={$LL.chat.role.admin()}>
+                            <IconShield font-size="13" />
+                        </span>
+                    {/if}
+                    {#if isMember}
+                        <span title={$LL.chat.role.member()}>
+                            <IconUsers font-size="13" />
+                        </span>
+                    {/if}
+                </h1>
+                <p class="tw-text-xs tw-mb-0 tw-font-condensed tw-opacity-75 tw-cursor-default tw-self-end">
+                    {#if isMe}
+                        {$LL.chat.you()}
+                    {:else if $userStatus}
+                        {getNameOfAvailabilityStatus($userStatus ?? 0)}
+                    {:else}
+                        {$LL.chat.userList.disconnected()}
+                    {/if}
+                </p>
+            </div>
+        </div>
+        {#if $userStatus && !isMe}
+            <UserActionButton {user} />
+        {/if}
+    </div>
 
-<style lang="scss">
-    .status {
-        background-color: var(--color);
-    }
-</style>
+    <style lang="scss">
+        .status {
+            background-color: var(--color);
+        }
+    </style>
+{/if}
