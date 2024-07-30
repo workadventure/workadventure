76dec528511c2c811742001501fe1f8a552d6734
alexandre artus <artusalexandre74@gmail.com>
2024-07-30 14:31:30 +0200

    try with the test in CI

--- tests/tests/meeting.spec.ts
+++ tests/tests/meeting.spec.ts
@@ -91,71 +91,71 @@ test.describe('Meeting actions test', () => {
       return;
     }
 
-    // await resetWamMaps(request);
+    await resetWamMaps(request);
 
-    // await page.goto(Map.url("empty"));
+    await page.goto(Map.url("empty"));
 
-    // // await page.goto(publicTestMapUrl("tests/E2E/empty.json", "meeting"));
+    // await page.goto(publicTestMapUrl("tests/E2E/empty.json", "meeting"));
 
-    // //await page.evaluate(() => { localStorage.setItem('debug', '*'); });
-    // //await page.reload();
-    // await login(page, "Alice", 3);
+    //await page.evaluate(() => { localStorage.setItem('debug', '*'); });
+    //await page.reload();
+    await login(page, "Alice", 3);
 
-    // // For the moment skip the map editor part because it's broken so we can't create the speaker zone
+    // For the moment skip the map editor part because it's broken so we can't create the speaker zone
 
-    // // Open the map editor
-    // await Menu.openMapEditor(page);
-    // // Create a new area
-    // await MapEditor.openAreaEditor(page);
-    // // Draw the area
-    // await AreaEditor.drawArea(page, {x: 0*32*1.5, y: 5}, {x: 9*32*1.5, y: 4*32*1.5});
-    // // Add a property Speaker zone to create new Jitsi meeting zone
-    // await AreaEditor.addProperty(page, 'Speaker zone');
-    // // Set the speaker zone property
-    // await AreaEditor.setSpeakerMegaphoneProperty(page, `${browser.browserType().name()}SpeakerZone`);
-    // // Close the map editor
-    // await Menu.closeMapEditor(page);
+    // Open the map editor
+    await Menu.openMapEditor(page);
+    // Create a new area
+    await MapEditor.openAreaEditor(page);
+    // Draw the area
+    await AreaEditor.drawArea(page, {x: 0*32*1.5, y: 5}, {x: 9*32*1.5, y: 4*32*1.5});
+    // Add a property Speaker zone to create new Jitsi meeting zone
+    await AreaEditor.addProperty(page, 'Speaker zone');
+    // Set the speaker zone property
+    await AreaEditor.setSpeakerMegaphoneProperty(page, `${browser.browserType().name()}SpeakerZone`);
+    // Close the map editor
+    await Menu.closeMapEditor(page);
 
-    // // Move user "Alice" to the new area
-    // await Map.walkTo(page, 'ArrowRight', 2000);
+    // Move user "Alice" to the new area
+    await Map.walkTo(page, 'ArrowRight', 2000);
 
-    // // Add a second user "Bob"
-    // const newBrowser = await browser.browserType().launch();
-    // const userBob = await newBrowser.newPage();
-    // await userBob.goto(Map.url("empty"));
-    // // Login user "Bob"
-    // await login(userBob, "Bob", 3);
-    // // Move user "Bob" to the new area
-    // // FIME: the teleportToPosition does not work ??
-    // await Map.walkTo(userBob, 'ArrowUp', 2000);
+    // Add a second user "Bob"
+    const newBrowser = await browser.browserType().launch();
+    const userBob = await newBrowser.newPage();
+    await userBob.goto(Map.url("empty"));
+    // Login user "Bob"
+    await login(userBob, "Bob", 3);
+    // Move user "Bob" to the new area
+    // FIME: the teleportToPosition does not work ??
+    await Map.walkTo(userBob, 'ArrowUp', 2000);
 
-    // // The user in the bubble meeting should be visible
-    // await expect(page.locator('#cameras-container #camera-box .jitsi-video')).toBeVisible({timeout: 10_000});
-    // // The user in the bubble meeting should have action button
-    // await expect(page.locator('#cameras-container #camera-box .jitsi-video .action-button')).toBeVisible({timeout: 10_000});
+    // The user in the bubble meeting should be visible
+    await expect(page.locator('#cameras-container #camera-box .jitsi-video')).toBeVisible({timeout: 10_000});
+    // The user in the bubble meeting should have action button
+    await expect(page.locator('#cameras-container #camera-box .jitsi-video .action-button')).toBeVisible({timeout: 10_000});
 
-    // // Click on the action button of "Alice"
-    // await page.click('#cameras-container #camera-box .jitsi-video .action-button#more-action');
-    // // Click on the mute button
-    // await page.click('#cameras-container #camera-box .jitsi-video .action-button#mute-audio-user');
+    // Click on the action button of "Alice"
+    await page.click('#cameras-container #camera-box .jitsi-video .action-button#more-action');
+    // Click on the mute button
+    await page.click('#cameras-container #camera-box .jitsi-video .action-button#mute-audio-user');
 
-    // // Check if "Bob" user receive the request to be metued
-    // await expect(userBob.locator('.interact-menu')).toBeVisible({timeout: 10_000});
-    // // Click on the accept button
-    // await userBob.click('.interact-menu .accept-request');
+    // Check if "Bob" user receive the request to be metued
+    await expect(userBob.locator('.interact-menu')).toBeVisible({timeout: 10_000});
+    // Click on the accept button
+    await userBob.click('.interact-menu .accept-request');
 
-    // // Check if the user has been muted
-    // await expect(page.locator('#cameras-container #camera-box .jitsi-video .voice-meter-cam-off')).toBeVisible({timeout: 10_000});
-    // // Click on the mute video button
-    // await page.click('#cameras-container #camera-box .jitsi-video .action-button#mute-video-user');
+    // Check if the user has been muted
+    await expect(page.locator('#cameras-container #camera-box .jitsi-video .voice-meter-cam-off')).toBeVisible({timeout: 10_000});
+    // Click on the mute video button
+    await page.click('#cameras-container #camera-box .jitsi-video .action-button#mute-video-user');
 
-    // // Check if "Bob" user receive the request to be metued
-    // await expect(userBob.locator('.interact-menu')).toBeVisible({timeout: 10_000});
-    // // Click on the accept button
-    // await userBob.click('.interact-menu .accept-request');
+    // Check if "Bob" user receive the request to be metued
+    await expect(userBob.locator('.interact-menu')).toBeVisible({timeout: 10_000});
+    // Click on the accept button
+    await userBob.click('.interact-menu .accept-request');
 
-    // // Check if the user has been muted
-    // await expect(page.locator('#cameras-container #camera-box .jitsi-video video')).toBeHidden({timeout: 10_000});
+    // Check if the user has been muted
+    await expect(page.locator('#cameras-container #camera-box .jitsi-video video')).toBeHidden({timeout: 10_000});
 
   });
 });
