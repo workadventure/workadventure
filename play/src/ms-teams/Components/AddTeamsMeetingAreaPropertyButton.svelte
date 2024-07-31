1d915e0256dd32690c0d28210e819cd12da7df28
Gregoire Parant <gregoire@workadventu.re>
2024-07-31 18:58:20 +0200

    add new folder Component

---
+++ play/src/ms-teams/Components/AddTeamsMeetingAreaPropertyButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import AddPropertyButton from "../../front/Components/MapEditor/PropertyEditor/AddPropertyButton.svelte";
+    import teamsLogo from "./images/TeamsLogo.png";
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<AddPropertyButton
+    headerText="Open meeting"
+    descriptionText="Open a new meeting in new tab"
+    img={teamsLogo}
+    style={`z-index: 200`}
+    on:click={(event) => {
+        dispatch("click", event);
+    }}
+/>
