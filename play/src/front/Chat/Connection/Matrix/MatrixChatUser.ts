cd54938f07892f8a8fae8d7a782cdfedca1c7be0
ydiarra <yd97@hotmail.fr>
2024-08-02 18:00:48 +0200

    Revert "WIP : woka name in chat"
    This reverts commit 9495ac49fb7d6a9c83079514d667a8e0da88b45d.

--- play/src/front/Chat/Connection/Matrix/MatrixChatUser.ts
+++ play/src/front/Chat/Connection/Matrix/MatrixChatUser.ts
@@ -1,8 +1,7 @@
 import { MatrixClient, SetPresence, User } from "matrix-js-sdk";
-import { get, writable } from "svelte/store";
+import { writable } from "svelte/store";
 import { AvailabilityStatus } from "@workadventure/messages";
 import { ChatUser } from "../ChatConnection";
-import { gameManager } from "../../../Phaser/Game/GameManager";
 
 export const chatUserFactory: (matrixChatUser: User, matrixClient: MatrixClient) => ChatUser = (
     matrixChatUser,
@@ -32,28 +31,4 @@ function mapMatrixPresenceToAvailabilityStatus(presence: string = SetPresence.Of
             //TODO : Create Error
             throw new Error(`Do not handle the status ${presence}`);
     }
-}
-
-function getWokaNameFromMatrixID(matrixID : string) : string | undefined {
-    const connectedUsers = get(gameManager.getCurrentGameScene().chatConnection.connectedUsers);
-    const userDisconnected= get(gameManager.getCurrentGameScene().chatConnection.userDisconnected);
-
-    console.log('getWokaNameFromMatrixID',userDisconnected.get(matrixID),Array.from(connectedUsers.values()).find((user)=> user.id === matrixID));
-
-    if(userDisconnected.has(matrixID)){
-        return userDisconnected.get(matrixID)?.username
-    }
-
-    return Array.from(connectedUsers.values()).find((user)=> user.id === matrixID)?.username; 
-   
-}
-
-
-function getWokaAvatarURLFromMatrixID(matrixID : string) : string | undefined | null {
-    const connectedUsers = get(gameManager.getCurrentGameScene().chatConnection.connectedUsers);
-    const userDisconnected= get(gameManager.getCurrentGameScene().chatConnection.userDisconnected);
-
-
-    return Array.from(connectedUsers.values()).find((user)=> user.id === matrixID)?.avatarUrl ?? userDisconnected.get(matrixID)?.avatarUrl ; 
-   
 }
