603cbf3194598291eebdb8b918ef4346f8bbc9bb
ydiarra <yd97@hotmail.fr>
2024-08-16 11:26:10 +0200

    on room area creation add room in space states

--- play/src/pusher/services/MatrixProvider.ts
+++ play/src/pusher/services/MatrixProvider.ts
@@ -1,26 +1,27 @@
 import axios from "axios";
-import { EventType, Visibility } from "matrix-js-sdk";
+import { EventType } from "matrix-js-sdk";
 import { MATRIX_API_URI, MATRIX_DOMAIN } from "../enums/EnvironmentVariable";
 
+//env var
+const ADMIN_CHAT_ID = "@admin:matrix.workadventure.localhost";
 class MatrixProvider {
     private accessToken: string | undefined;
     private lastAccessTokenDate: number = Date.now();
     //TODO : env var ?
     private roomAreaSpaceName = "space_for_area_room";
-    private roomAreaSpaceID : string;
+    private roomAreaSpaceID: string;
 
-    constructor(){
+    constructor() {
         //TODO: DELETE and move in synapse config or ...
-        this
-            .overrideRateLimitForAdminAccount()
-            .then(()=>console.log('overrideRateLimitForAdminAccount'))
-            .catch((error)=>console.error(error));
-        
-            this.createChatSpaceAreaAndSetID()
-            .then((roomID)=>{
-                this.roomAreaSpaceID = roomID
+        this.overrideRateLimitForAdminAccount()
+            .then(() => console.log("overrideRateLimitForAdminAccount"))
+            .catch((error) => console.error(error));
+
+        this.createChatSpaceAreaAndSetID()
+            .then((roomID) => {
+                this.roomAreaSpaceID = roomID;
             })
-            .catch((error)=>console.error(error));
+            .catch((error) => console.error(error));
     }
 
     getMatrixIdFromEmail(email: string): string {
@@ -81,170 +82,212 @@ class MatrixProvider {
             });
     }
 
-    async createRoomForArea():Promise<string>{
-        return await axios.post(`${MATRIX_API_URI}_matrix/client/r0/createRoom`,{
-        
-                visibility : "private",
-                initial_state : [
-                    {
-                        type: EventType.RoomHistoryVisibility,
-                        content: { history_visibility: "joined" },
-                    },
-                    {
-                        type: EventType.SpaceParent,
-                        state_key: this.roomAreaSpaceID,
-                        content: {
-                            via: [MATRIX_DOMAIN],
+    async createRoomForArea(): Promise<string> {
+        return axios
+            .post(
+                `${MATRIX_API_URI}_matrix/client/r0/createRoom`,
+                {
+                    visibility: "private",
+                    initial_state: [
+                        {
+                            type: EventType.RoomHistoryVisibility,
+                            content: { history_visibility: "joined" },
                         },
-                    }
-                ] 
-            }
-            ,     {
-                headers: {
-                    Authorization: "Bearer " + (await this.getAccessToken()),
+                        {
+                            type: EventType.SpaceParent,
+                            state_key: this.roomAreaSpaceID,
+                            content: {
+                                via: [MATRIX_DOMAIN],
+                            },
+                        },
+                    ],
                 },
-            }
-        ).then((response)=>{
-            if (response.status === 200) {
-                return Promise.resolve(response.data.room_id);
-            } else {
-                return Promise.reject(new Error("Failed with status " + response.status));
-            }
-        })
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return this.AddRoomToSpace(response.data.room_id);
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-    async kickUserFromRoom(userID:string , roomID : string):Promise<void>{
-
-        return await axios.post(`${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/kick`,{
-            //TODO : change reason
-            reason: "deconnection",
-            user_id: userID
-            }
-            ,     {
-                headers: {
-                    Authorization: "Bearer " + (await this.getAccessToken()),
+    async kickUserFromRoom(userID: string, roomID: string): Promise<void> {
+        return await axios
+            .post(
+                `${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/kick`,
+                {
+                    //TODO : change reason label
+                    reason: "deconnection",
+                    user_id: userID,
                 },
-            }
-        ).then((response)=>{
-            if (response.status === 200) {
-                return Promise.resolve();
-            } else {
-                return Promise.reject(new Error("Failed with status " + response.status));
-            }
-        })
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve();
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-    async inviteUserToRoom(userID:string , roomID : string):Promise<void>{
-        return await axios.post(`${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/invite`,{
-            user_id: userID
-            }
-            ,     {
-                headers: {
-                    Authorization: "Bearer " + (await this.getAccessToken()),
+    async inviteUserToRoom(userID: string, roomID: string): Promise<void> {
+        return await axios
+            .post(
+                `${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/invite`,
+                {
+                    user_id: userID,
                 },
-            }
-        ).then((response)=>{
-            if (response.status === 200) {
-                return Promise.resolve();
-            } else {
-                return Promise.reject(new Error("Failed with status " + response.status));
-            }
-        })
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve();
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-    async changeRoomName(roomID : string , name : string ):Promise<void>{
-        return await axios.put(`${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/state/m.room.name`,{
-            name
-            }
-            ,     {
-                headers: {
-                    Authorization: "Bearer " + (await this.getAccessToken()),
+    async changeRoomName(roomID: string, name: string): Promise<void> {
+        return await axios
+            .put(
+                `${MATRIX_API_URI}_matrix/client/r0/rooms/${roomID}/state/m.room.name`,
+                {
+                    name,
                 },
-            }
-        ).then((response)=>{
-            if (response.status === 200) {
-                return Promise.resolve();
-            } else {
-                return Promise.reject(new Error("Failed with status " + response.status));
-            }
-        })
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve();
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-    private async overrideRateLimitForAdminAccount(){
-        //env var
-        const adminChatID = "@admin:matrix.workadventure.localhost";
-        return await axios.post(`${MATRIX_API_URI}_synapse/admin/v1/users/${adminChatID}/override_ratelimit`,{
-            message_per_second: 0,
-            burst_count:0
-            },{
-                headers: {
-                    Authorization: "Bearer " + (await this.getAccessToken()),
+    private async overrideRateLimitForAdminAccount() {
+        return await axios
+            .post(
+                `${MATRIX_API_URI}_synapse/admin/v1/users/${ADMIN_CHAT_ID}/override_ratelimit`,
+                {
+                    message_per_second: 0,
+                    burst_count: 0,
                 },
-            }
-        ).then((response)=>{
-            if (response.status === 200) {
-                return Promise.resolve();
-            } else {
-                return Promise.reject(new Error("Failed with status " + response.status));
-            }
-        })
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve();
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-    //TODO : try to find a solution to call this function 1 time 
+    //TODO : try to find a solution to call this function 1 time for all pushers
     //TODO:Rename and split in 2 functions
-    private async createChatSpaceAreaAndSetID() : Promise<string>{
+    private async createChatSpaceAreaAndSetID(): Promise<string> {
         const isChatSpaceAreaExist = await this.isChatSpaceAreaExist();
-        if(isChatSpaceAreaExist){
-            console.log('matrixSpaceAreaID :>>>>>>>>>>>>>>>>>>>>',isChatSpaceAreaExist)
-            return Promise.resolve(isChatSpaceAreaExist)
-        }; 
-        //cas particulier de createRoom 
-        return await axios.post(`${MATRIX_API_URI}_matrix/client/r0/createRoom`,{
-            //preset: "public_chat",
-            visibility : "public",
-            room_alias_name: this.roomAreaSpaceName,
-            name: "Room Area Space",
-            creation_content: {
-            type: "m.space"
+        if (isChatSpaceAreaExist) {
+            return Promise.resolve(isChatSpaceAreaExist);
         }
-    }
-        ,     {
-            headers: {
-                Authorization: "Bearer " + (await this.getAccessToken()),
-            },
-        }
-    ).then((response)=>{
-        if (response.status === 200) {
-            return Promise.resolve(response.data.room_id);
-        } else {
-            return Promise.reject(new Error("Failed with status " + response.status));
-        }
-    })
+        return await axios
+            .post(
+                `${MATRIX_API_URI}_matrix/client/r0/createRoom`,
+                {
+                    //preset: "public_chat",
+                    visibility: "public",
+                    room_alias_name: this.roomAreaSpaceName,
+                    name: "Room Area Space",
+                    creation_content: {
+                        type: "m.space",
+                    },
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + (await this.getAccessToken()),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve(response.data.room_id);
+                } else {
+                    return Promise.reject(new Error("Failed with status " + response.status));
+                }
+            });
     }
 
-
-    private async isChatSpaceAreaExist() : Promise<string|undefined> {
-            
-        return axios.get(`${MATRIX_API_URI}_matrix/client/r0/directory/room/%23${this.roomAreaSpaceName}:${MATRIX_DOMAIN}`, {
-            headers: {
-                Authorization: "Bearer " + (await this.getAccessToken()),
-            },
-        }).then((response) => {
-            if (response.status === 200) {
-                // Space exists if we get a 200 OK response
-                return Promise.resolve(response.data.room_id);
-            } else {
+    private async isChatSpaceAreaExist(): Promise<string | undefined> {
+        return axios
+            .get(`${MATRIX_API_URI}_matrix/client/r0/directory/room/%23${this.roomAreaSpaceName}:${MATRIX_DOMAIN}`, {
+                headers: {
+                    Authorization: "Bearer " + (await this.getAccessToken()),
+                },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    return Promise.resolve(response.data.room_id);
+                } else {
+                    return Promise.resolve(undefined);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
                 return Promise.resolve(undefined);
-            }
-        }).catch((error) => {
-            console.error(error);
-            return Promise.resolve(undefined);
-        });
-            
+            });
+    }
 
-        
+    async AddRoomToSpace(roomID: string): Promise<string> {
+        const roomLinkContent = {
+            via: [MATRIX_DOMAIN],
+        };
+
+        try {
+            const response = await axios.put(
+                `${MATRIX_API_URI}/_matrix/client/r0/rooms/${this.roomAreaSpaceID}/state/m.space.child/${roomID}`,
+                roomLinkContent,
+                {
+                    headers: {
+                        Authorization: `Bearer ${await this.getAccessToken()}`,
+                    },
+                }
+            );
+
+            if (response.status === 200) {
+                return roomID;
+            } else {
+                throw new Error("Failed with status " + response.status);
+            }
+        } catch (error) {
+            console.error("An error occurred: " + (error as Error).message);
+            throw new Error("Failed with error: " + (error as Error).message);
+        }
     }
 }
-
 
 export const matrixProvider = new MatrixProvider();
