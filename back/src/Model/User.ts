25d6fbc4dc23b94832da56ec11dade452dc65232
ydiarra <yd97@hotmail.fr>
2024-08-26 13:46:14 +0200

    Update back/src/Model/User.ts
    Co-authored-by: David Négrier <d.negrier@thecodingmachine.com>

--- back/src/Model/User.ts
+++ back/src/Model/User.ts
@@ -335,6 +335,12 @@ export class User implements Movable, CustomJsonReplacerInterface {
         return undefined;
     }
 
+    /**
+     * Due to race conditions, it is possible that the first call to "write" is not a "roomJoinedMessage".
+     * If this is the case, the message is lost on the front side. This method is putting back the messages
+     * in the correct order. If the first message is not a "roomJoinedMessage", it is buffered until the 
+     * "roomJoinedMessage" message is received.
+     */
     public write(chunk: NonNullable<ServerToClientMessage["message"]>, cb?: (...args: unknown[]) => unknown): boolean {
         //TODO : handle socket.write return false
         if (this.isRoomJoinedMessage) {
