959b0941eee3e1acb099ae82003c4705663cf868
grégoire parant <gregoire@workadventu.re>
2024-08-29 10:36:33 +0200

    Improve function to set status in Teams application (#4149)
    * Improve function to set status in Teams application
    * Improve perf for presence

--- play/src/external-modules/ms-teams/index.ts
+++ play/src/external-modules/ms-teams/index.ts
@@ -180,7 +180,7 @@ class MSTeams implements MSTeamsExtensionModule {
                 this.listenToWorkadventureStatus = subscribe(
                     this.moduleOptions.workadventureStatusStore,
                     (workadventureStatus: AvailabilityStatus) => {
-                        this.setStatus(workadventureStatus);
+                        this.setStatusToTeams(workadventureStatus);
                     }
                 );
             }
@@ -199,11 +199,14 @@ class MSTeams implements MSTeamsExtensionModule {
                 const type = externalModuleMessage.message["@odata.type"];
                 switch (type.toLowerCase()) {
                     case MSGraphMessageEventSource.MicrosoftGraphPresence.toLocaleLowerCase():
-                        // get the presence status
-                        if (this.moduleOptions.onExtensionModuleStatusChange)
-                            this.moduleOptions.onExtensionModuleStatusChange(
-                                this.mapTeamsStatusToWorkAdventureStatus(externalModuleMessage.message.availability)
-                            );
+                        if (!this.moduleOptions.onExtensionModuleStatusChange) break;
+                        // Update the teams status of this module and do not call API to set presence
+                        this.teamsAvailability = externalModuleMessage.message.availability;
+
+                        // Update the workadventure status
+                        this.moduleOptions.onExtensionModuleStatusChange(
+                            this.mapTeamsStatusToWorkAdventureStatus(externalModuleMessage.message.availability)
+                        );
                         break;
                     case MSGraphMessageEventSource.MicrosoftGraphEvent.toLocaleLowerCase():
                         this.updateCalendarEvents().catch((e) =>
@@ -315,7 +318,7 @@ class MSTeams implements MSTeamsExtensionModule {
             .catch((e) => console.error("Error while getting MSTeams status", e));
     }
 
-    setStatus(workadventureNewStatus: AvailabilityStatus) {
+    setStatusToTeams(workadventureNewStatus: AvailabilityStatus) {
         const newTeamsAvailability = this.mapWorkAdventureStatusToTeamsStatus(workadventureNewStatus);
         if (newTeamsAvailability === this.teamsAvailability) {
             return;
