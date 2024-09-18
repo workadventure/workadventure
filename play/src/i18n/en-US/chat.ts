9b20d877b92978a33aa372ccdca03cbd11c71a60
ydiarra <yd97@hotmail.fr>
2024-09-18 18:34:24 +0200

    add button in AccessSecretStorageDialog to restore backup key

--- play/src/i18n/en-US/chat.ts
+++ play/src/i18n/en-US/chat.ts
@@ -296,7 +296,7 @@ const chat: BaseTranslation = {
         accessSecretStorage: {
             title: "Chat session verification",
             description:
-                "In order to verify your session and retrieve historical encrypted message your need to enter your recovery key or passphrase.",
+                "In order to verify your session and retrieve historical encrypted messages you need to enter your recovery key or passphrase.",
             passphrase: "Passphrase",
             recoveryKey: "Recovery key",
             placeholder: "Enter your",
