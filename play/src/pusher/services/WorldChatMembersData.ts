3658600208cbe529313f5a1848bed11e742d50d4
ydiarra <yd97@hotmail.fr>
2024-09-19 12:18:07 +0200

    change email type

--- play/src/pusher/services/WorldChatMembersData.ts
+++ play/src/pusher/services/WorldChatMembersData.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 const ChatMemberData = z.object({
     uuid: z.string(),
     wokaName: z.string().optional(),
-    email: z.string().optional(),
+    email: z
+        .string()
+        .nullable()
+        .transform((value) => (value === null ? undefined : value)),
     chatId: z.string().optional(),
     tags: z.string().array(),
 });
