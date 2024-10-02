5bbbbfd2916b4b51ae25d69040407791fd7edca6
David Négrier <d.negrier@thecodingmachine.com>
2024-10-02 15:29:00 +0200

    Linting NL language files

--- play/src/i18n/nl-NL/error.ts
+++ play/src/i18n/nl-NL/error.ts
@@ -4,19 +4,22 @@ const error: BaseTranslation = {
     accessLink: {
         title: "Toegangskoppeling onjuist",
         subTitle: "Kaart niet gevonden. Controleer je toegangskoppeling.",
-        details: "Als je meer informatie wilt, kun je contact opnemen met de beheerder of ons bereiken via: hello@workadventu.re",
+        details:
+            "Als je meer informatie wilt, kun je contact opnemen met de beheerder of ons bereiken via: hello@workadventu.re",
     },
     connectionRejected: {
         title: "Verbinding afgewezen",
         subTitle: "Je kunt de Wereld niet betreden. Probeer het later opnieuw {error}.",
-        details: "Als je meer informatie wilt, kun je contact opnemen met de beheerder of ons bereiken via: hello@workadventu.re",
+        details:
+            "Als je meer informatie wilt, kun je contact opnemen met de beheerder of ons bereiken via: hello@workadventu.re",
     },
     connectionRetry: {
         unableConnect: "Kan geen verbinding maken met WorkAdventure. Ben je verbonden met internet?",
     },
     errorDialog: {
         title: "Fout 😱",
-        hasReportIssuesUrl: "Als je meer informatie wilt, kun je contact opnemen met de beheerder of een probleem melden via:",
+        hasReportIssuesUrl:
+            "Als je meer informatie wilt, kun je contact opnemen met de beheerder of een probleem melden via:",
         noReportIssuesUrl: "Als je meer informatie wilt, kun je contact opnemen met de beheerder van de wereld.",
         messageFAQ: "Je kunt ook onze volgende informatie raadplegen:",
         reload: "Opnieuw laden",
