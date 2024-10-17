ef95b6f5cabf78befc76cf570c5e51940d550842
David Négrier <d.negrier@thecodingmachine.com>
2024-10-17 09:52:49 +0200

    Adding E2E tests for scripting audio streams

---
+++ tests/tests/scripting_audio_stream.spec.ts
@@ -0,0 +1,58 @@
+import { test } from "@playwright/test";
+import { evaluateScript } from "./utils/scripting";
+import { login } from "./utils/roles";
+import Map from "./utils/map";
+
+test.describe("Scripting audio streams", () => {
+  test("can play and listen to sounds", async ({
+    page,
+    browser,
+  }) => {
+    await page.goto(Map.url("empty"));
+    await login(page, "bob", 3, "us-US", false);
+
+    await Map.teleportToPosition(page, 32, 32);
+
+    // Open new page for alice
+    const newBrowser = await browser.browserType().launch();
+    const alice = await newBrowser.newPage();
+    await alice.goto(Map.url("empty"));
+    await login(alice, "alice", 4, "us-US", false);
+
+    // Move alice to the same position as bob
+    await Map.teleportToPosition(alice, 32, 32);
+
+    // Test play sound scripting
+    await evaluateScript(page, async () => {
+      const sampleRate = 24000;
+
+      const audioStream = await WA.player.proximityMeeting.startAudioStream(sampleRate);
+
+      // Generate a sine wave
+      const frequency = 440;
+      const amplitude = 1;
+      const duration = 20;
+      const numSamples = duration * sampleRate;
+      const samples = new Float32Array(numSamples);
+      for (let i = 0; i < numSamples; i++) {
+        samples[i] = amplitude * Math.sin(2 * Math.PI * frequency * i / sampleRate);
+      }
+
+      audioStream.appendAudioData(samples);
+    });
+
+    // Test listen to sound scripting
+    await evaluateScript(alice, async () => {
+      const sampleRate = 24000;
+
+      return new Promise((resolve) => {
+        WA.player.proximityMeeting.listenToAudioStream(sampleRate).subscribe((data: Float32Array) => {
+          // At some point, the volume of the sound should be high enough to be noticed in the sample
+          if (data.some((sample) => Math.abs(sample) > 0.7)) {
+              resolve();
+          }
+        });
+      });
+    });
+  });
+});
