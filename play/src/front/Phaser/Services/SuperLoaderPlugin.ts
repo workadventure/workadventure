32d0598de6861c3c3fa20e50235ed88257be4bee
David Négrier <d.negrier@thecodingmachine.com>
2024-10-22 16:53:48 +0200

    Putting rexAwait loader inside an easier to use function: superLoad.loadPromise.

--- play/src/front/Phaser/Services/SuperLoaderPlugin.ts
+++ play/src/front/Phaser/Services/SuperLoaderPlugin.ts
@@ -15,6 +15,18 @@ const debug = Debug("SuperLoad");
 export class SuperLoaderPlugin {
     constructor(private scene: Scene) {}
 
+    /**
+     * Add any promise to the loader.
+     * The loader will consider the promise as a resource that is being loaded. Loading is done when the promise is resolved or rejected.
+     * @param promise
+     */
+    public loadPromise(promise: PromiseLike<unknown>) {
+        //eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (this.scene.load as any).rexAwait((successCallback: () => void, failureCallback: (e: unknown) => void) => {
+            promise.then(successCallback).catch(failureCallback);
+        });
+    }
+
     public spritesheet(
         key: string,
         url: string,
