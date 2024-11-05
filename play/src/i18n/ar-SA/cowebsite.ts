c173efb232851f4b4e50bd671cc313df4e49eb63
ydiarra <yd97@hotmail.fr>
2024-11-05 10:34:38 +0100

    fix typecheck

--- play/src/i18n/ar-SA/cowebsite.ts
+++ play/src/i18n/ar-SA/cowebsite.ts
@@ -1,10 +1,9 @@
 import type { DeepPartial } from "../DeepPartial";
 import type { Translation } from "../i18n-types";
 
-const arabicTranslations: DeepPartial<Translation["cowebsite"]> = {
+const cowebsite: DeepPartial<Translation["cowebsite"]> = {
     open: "افتح", // open
     close: "أغلق", // close
 };
 
-Object.assign(companion, arabicTranslations);
-export default companion;
+export default cowebsite;
