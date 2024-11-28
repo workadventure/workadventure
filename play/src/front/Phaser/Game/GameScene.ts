7be347c4632a89950dc8741cb6581f0a832a4678
Gregoire Parant <gregoire@workadventu.re>
2024-11-28 19:53:46 +0100

    Fix when the GameScene is reload for external module
    We need to call destroy of the external module and clean the store.

--- play/src/front/Phaser/Game/GameScene.ts
+++ play/src/front/Phaser/Game/GameScene.ts
@@ -1074,6 +1074,7 @@ export class GameScene extends DirtyScene {
         get(extensionModuleStore).forEach((extensionModule) => {
             extensionModule.destroy();
         });
+        extensionModuleStore.set([]);
 
         //When we leave game, the camera is stop to be reopen after.
         // I think that we could keep camera status and the scene can manage camera setup
