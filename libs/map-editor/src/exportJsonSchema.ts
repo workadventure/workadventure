9f9a9ae643c2dd34be61fde2777a53deedb51c20
ydiarra <yd97@hotmail.fr>
2024-12-02 18:04:42 +0100

    recreate docs schema folder

--- libs/map-editor/src/exportJsonSchema.ts
+++ libs/map-editor/src/exportJsonSchema.ts
@@ -1,13 +1,13 @@
 import * as fs from "fs";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import { WAMFileFormat } from "./types";
-import { entitiesFileMigration } from "./Migrations/EntitiesFileMigration";
+import { wamFileMigration } from "./Migrations/WamFileMigration";
 
 const jsonSchema = zodToJsonSchema(WAMFileFormat, {
     name: "WAMFileFormat",
 });
 
-const latestVersion = entitiesFileMigration.getLatestVersion();
+const latestVersion = wamFileMigration.getLatestVersion();
 
 const schemaDir = __dirname + `/../../../docs/schema/${latestVersion}`;
 
