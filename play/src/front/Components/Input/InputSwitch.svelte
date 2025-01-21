fe490d2d058e0614f03a684b096d2f314d8acf70
Jade-Keina POGNON <jade.keina@gmail.com>
2025-01-21 17:07:34 +0100

    I fixed the button's bug and i put the design system and fixed the  children main container  to  the container id app

---
+++ play/src/front/Components/Input/InputSwitch.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let id;
+    export let label;
+    export let value = false;
+    export let onChange = () => {};
+    export let disabled = false;
+</script>
+
+<div class="value-switch">
+    <label for={id}>{label}</label>
+    <label class="inline-flex cursor-pointer">
+        <input {id} type="checkbox" class="sr-only peer" bind:checked={value} on:change={onChange} {disabled} />
+        <div class="input-switch input-switch-white" />
+    </label>
+</div>
