c30b0a85aa00b76d2b5207f689af7382707ce0a2
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-01-21 16:42:26 +0100

    faster userlist test

--- tests/tests/userlist.spec.ts
+++ tests/tests/userlist.spec.ts
@@ -1,34 +1,25 @@
 import { expect, test } from "@playwright/test";
-import { login } from "./utils/roles";
 import Map from "./utils/map";
 import { publicTestMapUrl } from "./utils/urls";
 import chatUtils from "./utils/chat";
+import { getPage } from "./utils/auth";
 
 test.describe("Walk to", () => {
-  test("walk to a user ", async ({ page, browser }, { project }) => {
+  test("walk to a user ", async ({ browser }, { project }) => {
     if (project.name === "mobilechromium") {
       //eslint-disable-next-line playwright/no-skipped-test
       test.skip();
       return;
     }
-
-    await page.goto(publicTestMapUrl("tests/E2E/empty.json", "userlist"));
-
-    await login(page, "Alice", 2, "en-US");
-
+    const page = await getPage(browser, "Alice", publicTestMapUrl("tests/E2E/empty.json", "userlist"));
     const alicePosition = {
       x: 3 * 32,
       y: 4 * 32,
     };
-
     await Map.teleportToPosition(page, alicePosition.x, alicePosition.y);
 
-    const newBrowser = await browser.newContext();
-    const userBob = await newBrowser.newPage();
+    const userBob = await getPage(browser, "Bob", publicTestMapUrl("tests/E2E/empty.json", "userlist"));
 
-    await userBob.goto(publicTestMapUrl("tests/E2E/empty.json", "userlist"));
-    // Login user "Bob"
-    await login(userBob, "Bob", 3, "en-US");
     //await Map.teleportToPosition(userBob, positionToDiscuss.x, positionToDiscuss.y);
     await chatUtils.openUserList(userBob, false);
     await chatUtils.UL_walkTo(userBob, "Alice");
@@ -44,6 +35,8 @@ test.describe("Walk to", () => {
     );
 
     await userBob.close();
-    await newBrowser.close();
+    await userBob.context().close();
+    await page.close();
+    await page.context().close();
   });
 });
