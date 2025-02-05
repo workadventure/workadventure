d30546fd38fb3228992a8144eb961b13a8f089ae
Jade-Keina POGNON <jade.keina@gmail.com>
2025-02-05 16:33:10 +0100

    i made a new component of different checkbox

---
+++ play/src/front/Components/Input/InputRadio.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let id: string | undefined = undefined;
+    export let label: string;
+    export let onChange = () => {};
+    export let disabled = false;
+    export let value = false;
+    export let variant: "contrast" | "" = "";
+    //Pour changer la forme du bouton en checkbox (carré) au lieu de Radio (rond)
+    export let checkbox = false;
+
+    let uniqueId = id || `input-${Math.random().toString(36).substr(2, 9)} `;
+</script>
+
+<div class="flex items-center gap-2 p-2">
+    <label class="inline-flex cursor-pointer relative ">
+        <input
+            id={uniqueId}
+            class="sr-only peer"
+            type="checkbox"
+            bind:checked={value}
+            on:change={onChange}
+            {disabled}
+        />
+
+        {#if checkbox === true}
+            <div class="input-checkbox" />
+        {:else}
+            <div class="input-radio input-radio-light" class:input-radio-contrast={variant === "contrast"} />
+        {/if}
+    </label>
+
+    <label for={uniqueId} class="  input-label input-label-inline input-label-light text-white "
+        >{label} <slot />
+    </label>
+</div>
