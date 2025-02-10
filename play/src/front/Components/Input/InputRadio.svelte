78b73349595b88718f0a2bdc37ef86f3b8bae528
Jade-Keina POGNON <jade.keina@gmail.com>
2025-02-10 10:57:55 +0100

    I add some details in the component InputRadio so that the type Radio works well

--- play/src/front/Components/Input/InputRadio.svelte
+++ play/src/front/Components/Input/InputRadio.svelte
@@ -3,16 +3,16 @@
     export let label: string;
     export let onChange = () => {};
     export let disabled = false;
-    export let value = false;
+    export let value: unknown;
     export let variant: "contrast" | "" = "";
-    //Pour changer la forme du bouton en checkbox (carré) au lieu de Radio (rond)
+    export let group: unknown;
 
     let uniqueId = id || `input-${Math.random().toString(36).substr(2, 9)} `;
 </script>
 
 <div class="flex items-center gap-2 p-2">
     <label class="inline-flex cursor-pointer relative ">
-        <input id={uniqueId} class="sr-only peer" type="radio" bind:group={value} on:change={onChange} {disabled} />
+        <input id={uniqueId} class="sr-only peer" type="radio" bind:group {value} on:change={onChange} {disabled} />
 
         <div class="input-radio input-radio-light" class:input-radio-contrast={variant === "contrast"} />
     </label>
