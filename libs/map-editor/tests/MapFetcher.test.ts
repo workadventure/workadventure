4ab5530e5e884d34f3c3d63dfc35099aea089d29
ozraru <ozraru@raru.work>
2025-02-16 13:49:23 +0900

    Add IPv6 testcases of isLocalUrl

--- libs/map-editor/tests/MapFetcher.test.ts
+++ libs/map-editor/tests/MapFetcher.test.ts
@@ -9,6 +9,7 @@ describe("MapFetcher", () => {
 
     it("should return true on DNS resolving to a local domain", async () => {
         expect(await mapFetcher.isLocalUrl("https://127.0.0.1.nip.io")).toBe(true);
+        expect(await mapFetcher.isLocalUrl("https://fe80--1.sslip.io")).toBe(true);
     });
 
     it("should return true on an IP resolving to a local domain", async () => {
@@ -18,10 +19,12 @@ describe("MapFetcher", () => {
 
     it("should return false on an IP resolving to a global domain", async () => {
         expect(await mapFetcher.isLocalUrl("https://51.12.42.42")).toBe(false);
+        expect(await mapFetcher.isLocalUrl("https://[2606:4700:4700::1111]")).toBe(false);
     });
 
     it("should return false on an DNS resolving to a global domain", async () => {
         expect(await mapFetcher.isLocalUrl("https://maps.workadventu.re")).toBe(false);
+        expect(await mapFetcher.isLocalUrl("https://2606-4700-4700--1111.sslip.io")).toBe(false);
     });
 
     it("should throw error on invalid domain", async () => {
