1e0c557d6d7e6e791f075ea287252abee0033905
Antoine <arotakerpro@gmail.com>
2025-02-20 10:07:34 +0100

    finish room list modal

--- play/src/front/Components/Exploration/MapList.svelte
+++ play/src/front/Components/Exploration/MapList.svelte
@@ -80,47 +80,43 @@
 </script>
 
 <div class="absolute top-0 bottom-0 w-full h-full flex items-center justify-center">
-    <div class="w-2/3 relative h-fit">
+    <div class="w-2/3  relative h-fit">
         <PopUpContainer extraClasses="w-full relative">
-            <div
-                class="flex flex-col items-center pointer-events-auto"
-            >
+            <div class="flex flex-col items-center pointer-events-auto">
                 {#if $isMoving}
                     <h1>{$LL.mapEditor.listRoom.movingToRoom({ roomNameSelected: $roomNameSelected })}</h1>
                 {:else}
                     <!-- room card -->
-                    <div class="h-fit w-full flex flex-col items-center gap-4">
-                        <div class="w-full">
-                            <label for="search" class="w-1/2 text-white pb-2 pl-4 block"
-                                >{$LL.mapEditor.listRoom.searchLabel()}</label
-                            >
-                            <div class="relative flex grow w-1/2 m-auto">
+                    <div class="h-fit w-full flex flex-col items-center gap-8">
+                        <div class="w-full flex flex-col items-start gap-2 px-6">
+                            <label for="search" class="text-white pl-2">{$LL.mapEditor.listRoom.searchLabel()}</label>
+                            <div class="relative flex grow w-full ">
                                 <input
                                     id="search"
                                     type="text"
                                     placeholder={$LL.mapEditor.listRoom.searchPlaceholder()}
                                     bind:value={search}
                                     on:input={onUpdateSearch}
-                                    class="grow input-search input-search-lg peer"
+                                    class="grow input-search input-search-lg peer w-full"
                                 />
+                                <svg
+                                    class="icon icon-tabler icon-tabler-search stroke-contrast-400 absolute top-0 bottom-0 right-5 m-auto peer-focus:stroke-secondary peer-hover:stroke-secondary-500 transition-all peer-focus:-translate-x-1"
+                                    fill="none"
+                                    height="20"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+                                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                                    <path d="M21 21l-6 -6" />
+                                </svg>
+                            </div>
                         </div>
-                            <svg
-                                class="icon icon-tabler icon-tabler-search stroke-contrast-400 absolute top-0 bottom-0 right-5 m-auto peer-focus:stroke-secondary peer-hover:stroke-secondary-500 transition-all peer-focus:-translate-x-1"
-                                fill="none"
-                                height="20"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="1.5"
-                                viewBox="0 0 24 24"
-                                width="20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-                                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                <path d="M21 21l-6 -6" />
-                            </svg>
-                        </div>
-                        <div class="flex flex-row flex-nowrap space-x-4 justify-center items-start w-full">
+                        <div class="flex flex-row flex-nowrap space-x-4 justify-center items-center w-full min-h-52">
                             {#if $isFetching}
                                 <h3>{$LL.mapEditor.listRoom.isFetching()}</h3>
                             {:else if !$isFetching && $roomListFiltered.size === 0}
@@ -128,7 +124,7 @@
                             {:else}
                                 <div class="relative w-full flex space-x-6 snap-mandatory snap-x overflow-x-auto">
                                     <div class="snap-center shrink-0">
-                                        <div class="shrink-0 "></div>
+                                        <div class="shrink-0 " />
                                     </div>
                                     {#each Array.from($roomListFiltered) as [roomUrl, roomData] (roomUrl)}
                                         <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -137,8 +133,15 @@
                                             class="flex flex-col items-center snap-center shrink-0 first:pl-8 last:pr-8  rounded-lg overflow-hidden"
                                             on:click={() => clickRoom(roomData.roomUrl, roomData.name)}
                                         >
-                                            <div class="w-full rounded-lg relative overflow-hidden group cursor-pointer">
-                                                <div class="absolute z-10  left-0 bottom-0-0 w-full h-full bg-gradient-to-t to-90% to-transparent {currentRoomUrl === new URL(roomData.roomUrl, window.location.href).toString()?'from-secondary-900' :'from-contrast'}"></div>
+                                            <div
+                                                class="w-full rounded-lg relative overflow-hidden group cursor-pointer"
+                                            >
+                                                <div
+                                                    class="absolute z-10  left-0 bottom-0-0 w-full h-full bg-gradient-to-t to-90% to-transparent {currentRoomUrl ===
+                                                    new URL(roomData.roomUrl, window.location.href).toString()
+                                                        ? 'from-secondary-900'
+                                                        : 'from-contrast'}"
+                                                />
                                                 <img
                                                     src={roomData.thumbnail ?? defaultMapImg}
                                                     alt={roomData.name}
@@ -147,14 +150,16 @@
                                                         this.src = defaultMapImg;
                                                     }}
                                                 />
-                                                <span class="absolute z-20 bottom-0 max-w-full w-full flex items-center justify-center text-ellipsis overflow-hidden text-white text-xs font-bold px-2 py-3 bg-se">
+                                                <span
+                                                    class="absolute z-20 bottom-0 max-w-full w-full flex items-center justify-center text-ellipsis overflow-hidden text-white text-xs font-bold px-2 py-3 bg-se"
+                                                >
                                                     {roomData.name}
                                                 </span>
                                                 {#if currentRoomUrl === new URL(roomData.roomUrl, window.location.href).toString()}
-                                                    <span class="chip z-20 chip-sm chip-secondary bg-secondary text-white rounded-[8px] absolute top-3 right-3">
-                                                        <div class="px-2">
-                                                            Active
-                                                        </div>
+                                                    <span
+                                                        class="chip z-20 chip-sm chip-secondary bg-secondary text-white rounded-[8px] absolute top-3 right-3"
+                                                    >
+                                                        <div class="px-2">Active</div>
                                                     </span>
                                                 {/if}
                                             </div>
@@ -171,7 +176,7 @@
                                         </div>
                                     {/each}
                                     <div class="snap-center shrink-0">
-                                        <div class="shrink-0"></div>
+                                        <div class="shrink-0" />
                                     </div>
                                 </div>
                             {/if}
