478432d411967ad2aa43d8c3f3e513e0d1240c8b
Antoine <arotakerpro@gmail.com>
2025-02-20 15:51:01 +0100

    run pretty

--- play/src/front/Components/PopUp/PopUpContainer.svelte
+++ play/src/front/Components/PopUp/PopUpContainer.svelte
@@ -3,26 +3,25 @@
     export let extraClasses = "";
 </script>
 
-    <div
-        class="bg-contrast/80 flex flex-col backdrop-blur-md text-white min-w-60 min-h-20 rounded-lg overflow-hidden transition-all animation responsive {SLOTS.iconButton
-            ? 'px-10'
-            : ''} {extraClasses}"
-    >
-        <div class="flex items-center p-4 pointer-events-auto justify-center grow">
-            <div class="text-center leading-6 responsive-message w-full">
-                <slot />
-            </div>
+<div
+    class="bg-contrast/80 flex flex-col backdrop-blur-md text-white min-w-60 min-h-20 rounded-lg overflow-hidden transition-all animation responsive {SLOTS.iconButton
+        ? 'px-10'
+        : ''} {extraClasses}"
+>
+    <div class="flex items-center p-4 pointer-events-auto justify-center grow">
+        <div class="text-center leading-6 responsive-message w-full">
+            <slot />
         </div>
-        {#if SLOTS.buttons}
-            <div class="buttons-wrapper flex items-center justify-center p-2 space-x-2 bg-contrast/50 pointer-events-auto">
-                <slot name="buttons" />
-            </div>
-        {/if}
-        {#if SLOTS.iconButton}
-            <slot name="iconButton" />
-        {/if}
     </div>
-
+    {#if SLOTS.buttons}
+        <div class="buttons-wrapper flex items-center justify-center p-2 space-x-2 bg-contrast/50 pointer-events-auto">
+            <slot name="buttons" />
+        </div>
+    {/if}
+    {#if SLOTS.iconButton}
+        <slot name="iconButton" />
+    {/if}
+</div>
 
 <style lang="scss">
     @import "../../style/breakpoints.scss";
