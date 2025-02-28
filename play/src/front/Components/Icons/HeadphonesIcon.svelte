e6660bbeb16e5cd8439d78d791fe1cb8fe7af1b6
Antoine <arotakerpro@gmail.com>
2025-02-28 10:29:29 +0100

    :lipstick: enhance hardware select in action bar

---
+++ play/src/front/Components/Icons/HeadphonesIcon.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let height = "h-6";
+    export let width = "w-6";
+    export let strokeColor = "stroke-white";
+    export let fillColor = "fill-transparent";
+    export let hover = "group-hover/btn-headphones:fill-white";
+    export let strokeWidth = "1.5";
+    export let classList = "aspect-ratio transition-all";
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill={hover}
+    stroke="currentColor"
+    stroke-width={strokeWidth}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="icon {height} {width} {strokeColor} {fillColor} {classList}"
+>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" class="fill-transparent transition-all" />
+    <path d="M4 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z" class={hover} />
+    <path d="M15 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z" class={hover} />
+    <path d="M4 15v-3a8 8 0 0 1 16 0v3" />
+</svg>
