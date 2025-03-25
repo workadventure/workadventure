f5c703ed18d71dde1640429a4addc8706daf344b
David Négrier <d.negrier@thecodingmachine.com>
2025-03-25 11:17:07 +0100

    Saving in localStorage the chat bar width

--- play/src/front/Chat/ChatSidebarWidthStore.ts
+++ play/src/front/Chat/ChatSidebarWidthStore.ts
@@ -1,8 +1,15 @@
 import { derived, writable } from "svelte/store";
-import { chatVisibilityStore, INITIAL_SIDEBAR_WIDTH } from "../Stores/ChatStore";
+import { chatVisibilityStore } from "../Stores/ChatStore";
 import { windowSize } from "../Stores/CoWebsiteStore";
+import { localUserStore } from "../Connection/LocalUserStore";
 
-export const chatSidebarWidthStore = writable(INITIAL_SIDEBAR_WIDTH);
+export const chatSidebarWidthStore = writable(localUserStore.getChatSideBarWidth());
+
+// Not unsubscribing is ok, this is a singleton.
+//eslint-disable-next-line svelte/no-ignored-unsubscribe
+chatSidebarWidthStore.subscribe((value) => {
+    localUserStore.setChatSideBarWidth(value);
+});
 
 export const hideActionBarStoreBecauseOfChatBar = derived(
     [chatVisibilityStore, chatSidebarWidthStore, windowSize],
