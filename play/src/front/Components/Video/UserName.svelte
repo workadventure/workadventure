d2094f89de54c56b054f6c8e4636bfa8f3cb81ad
Jade-Keina POGNON <jade.keina@gmail.com>
2025-03-26 11:16:40 +0100

    Fixed the design of the camera disabled

--- play/src/front/Components/Video/UserName.svelte
+++ play/src/front/Components/Video/UserName.svelte
@@ -2,7 +2,7 @@
     import Woka from "../Woka/Woka.svelte";
     import { PictureStore } from "../../Stores/PictureStore";
 
-    export let isCameraDisabled = false; 
+    export let isCameraDisabled = false;
 
     /**
      * A component that displays the Woka, the user's name and optionally a dropdown menu
@@ -17,36 +17,33 @@
 </script>
 
 {#if isCameraDisabled}
-<div class="{position} z-30 responsive-dimension ">
-    <div class="flex justify-between  rounded bg-transparent">
-        <div class="relative  backdrop-blur px-2 py-1 text-white text-sm bold rounded text-nowrap flex flex-col items-center ">
-            <div class="" style="image-rendering:pixelated">
-                <Woka
-                src={$picture ?? ""}
-                customHeight="100px"
-                customWidth="100px"
-                />
+    <div class="{position} z-30 responsive-dimension ">
+        <div class="flex justify-between  rounded bg-transparent">
+            <div
+                class="relative  backdrop-blur px-2 py-1 text-white text-sm bold rounded text-nowrap flex flex-col items-center "
+            >
+                <div class="" style="image-rendering:pixelated">
+                    <Woka src={$picture ?? ""} customHeight="100px" customWidth="100px" />
+                </div>
+                <div class="flex items-center">
+                    <span>{name}</span>
+                    <slot />
+                </div>
             </div>
-            <div class="flex items-center"> <span>{name}</span>
-                <slot /></div>
         </div>
     </div>
-</div>
 {:else}
-<div class="{position} z-30 responsive-dimension ">
-    <div class="flex justify-between  rounded {isPlayingAudio ? 'bg-secondary/90' : 'bg-contrast/90'}">
-        <div class="relative backdrop-blur px-2 py-1 text-white text-sm pl-12 bold rounded text-nowrap  flex items-center">
-            <div class="absolute left-1 -top-1 z-30" style="image-rendering:pixelated">
-                <Woka
-                src={$picture ?? ""}
-                customHeight="42px"
-                customWidth="42px"
-                />
-            </div>
-            {name} 
+    <div class="{position} z-30 responsive-dimension ">
+        <div class="flex justify-between  rounded {isPlayingAudio ? 'bg-secondary/90' : 'bg-contrast/90'}">
+            <div
+                class="relative backdrop-blur px-2 py-1 text-white text-sm pl-12 bold rounded text-nowrap  flex items-center"
+            >
+                <div class="absolute left-1 -top-1 z-30" style="image-rendering:pixelated">
+                    <Woka src={$picture ?? ""} customHeight="42px" customWidth="42px" />
+                </div>
+                {name}
                 <slot />
+            </div>
         </div>
     </div>
-</div>
 {/if}
-
