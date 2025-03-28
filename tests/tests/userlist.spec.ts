ba9ea9ea7c733d0948d8b4d789b00cc7a4b53ba9
David Négrier <d.negrier@thecodingmachine.com>
2025-03-28 10:58:30 +0100

    Fixing E2E test post merge

--- tests/tests/userlist.spec.ts
+++ tests/tests/userlist.spec.ts
@@ -4,7 +4,6 @@ import { publicTestMapUrl } from "./utils/urls";
 import chatUtils from "./utils/chat";
 import { getPage } from "./utils/auth";
 import {isMobile} from "./utils/isMobile";
-import { oidcAdminTagLogin, oidcMemberTagLogin } from "./utils/oidc";
 
 test.describe("Walk to", () => {
   test.beforeEach(async ({ page, browserName }) => {
@@ -47,18 +46,14 @@ test.describe("Walk to", () => {
 
 
 test.describe("Send Message from User List", () => {
-  test("Send Message from User List", async ({ page, browser }, { project }) => {
+  test("Send Message from User List", async ({ browser }, { project }) => {
     if (project.name === "mobilechromium") {
       //eslint-disable-next-line playwright/no-skipped-test
       test.skip();
       return;
     }
 
-    const isMobileTest = project.name === "mobilechromium";
-    await page.goto(publicTestMapUrl("tests/E2E/empty.json", "userlist"));
-
-    await login(page, "Alice", 2, "en-US", isMobileTest);
-    await oidcAdminTagLogin(page);
+    const page = await getPage(browser, "Admin1", publicTestMapUrl("tests/E2E/empty.json", "userlist"));
 
     const alicePosition = {
       x: 3 * 32,
@@ -67,24 +62,20 @@ test.describe("Send Message from User List", () => {
 
     await Map.teleportToPosition(page, alicePosition.x, alicePosition.y);
 
-    const newBrowser = await browser.newContext();
-    const userBob = await newBrowser.newPage();
+    const userBob = await getPage(browser, "Member1", publicTestMapUrl("tests/E2E/empty.json", "userlist"));
 
-    await userBob.goto(publicTestMapUrl("tests/E2E/empty.json", "userlist"));
-    // Login user "Bob"
-    await login(userBob, "Bob", 3, "en-US", isMobileTest);
-    await oidcMemberTagLogin(userBob);
     //await Map.teleportToPosition(userBob, positionToDiscuss.x, positionToDiscuss.y);
 
     await chatUtils.open(userBob, false);
     await chatUtils.slideToUsers(userBob);
-    await chatUtils.UL_sendMessage(userBob, "Alice", "Hello Alice");
+    await chatUtils.UL_sendMessage(userBob, "Admin1", "Hello Alice");
 
     await expect(userBob.getByTestId("roomName")).toHaveText(
       "John Doe"
     );
 
     await userBob.close();
-    await newBrowser.close();
+    await page.close();
+    await browser.close();
   });
 });
