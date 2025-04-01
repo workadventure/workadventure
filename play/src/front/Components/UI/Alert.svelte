88c9cdd4250cfacbe4952ee524847497cc9699fe
Jade-Keina POGNON <jade.keina@gmail.com>
2025-04-01 09:53:04 +0200

    Fixed the design of the global communication page

--- play/src/front/Components/UI/Alert.svelte
+++ play/src/front/Components/UI/Alert.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<div class="bg-danger-1200 border border-solid border-danger text-danger-400 rounded-sm px-4 py-1 inline-block text-sm">
+<div class="bg-danger-1200 border border-solid border-danger text-danger-400 rounded-sm  py-1 inline-block text-xs">
     <slot />
 </div>
