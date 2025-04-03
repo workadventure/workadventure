2dee488227a0ffd6a91fdb511feb7e3971bbdc13
yannisdia <y.diarra@workadventu.re>
2025-04-03 11:55:10 +0200

    Add ‘Participants’ button for folder

--- play/src/front/Chat/Components/Room/CreateRoomOrFolderOption.svelte
+++ play/src/front/Chat/Components/Room/CreateRoomOrFolderOption.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
+    import { get, readable } from "svelte/store";
     import { openModal } from "svelte-modals";
+    import ManageParticipantsModal from "./ManageParticipantsModal.svelte";
     import LL from "../../../../i18n/i18n-svelte";
     import { notificationPlayingStore } from "../../../Stores/NotificationStore";
     import { RoomFolder } from "../../Connection/ChatConnection";
-
+    import { ChatRoomModeration } from "../../Connection/ChatConnection";
     import CreateFolderModal from "./CreateFolderModal.svelte";
     import CreateRoomModal from "./CreateRoomModal.svelte";
     import RoomOption from "./RoomMenu/RoomOption.svelte";
-    import { IconDots, IconFolder, IconLogout, IconMessage } from "@wa-icons";
+    import { IconDots, IconFolder, IconLogout, IconMessage , IconUserEdit} from "@wa-icons";
 
     export let parentID: string | undefined = undefined;
     export let parentName = "";
-    export let folder: RoomFolder | undefined;
+    export let folder: RoomFolder &  ChatRoomModeration | undefined;
     let optionButtonRef: HTMLButtonElement | undefined = undefined;
     let hideFolderOptions = true;
+
+    console.log(get(folder?.name));
+
+    const hasPermissionToInvite = folder?.hasPermissionTo("invite") ?? readable(false);
+    const hasPermissionToKick = folder?.hasPermissionTo("kick") ?? readable(false);
+    const hasPermissionToBan = folder?.hasPermissionTo("ban") ?? readable(false);
+
+    $: shouldDisplayManageParticipantButton = $hasPermissionToInvite || $hasPermissionToKick || $hasPermissionToBan;
+
 
     function toggleSpaceOption() {
         if (optionButtonRef === undefined) {
@@ -42,6 +53,11 @@
             })
             .catch(() => console.error("Failed to leave room"));
     }
+
+    function openManageParticipantsModal() {
+        openModal(ManageParticipantsModal, { room : folder });
+    }
+
 </script>
 
 <button
@@ -71,6 +87,15 @@
         title={$LL.chat.createFolder.title()}
         on:click={openCreateSpace}
     />
+    {#if shouldDisplayManageParticipantButton && folder}
+    <RoomOption
+        dataTestId="manageParticipantOption"
+        IconComponent={IconUserEdit}
+        title={$LL.chat.manageRoomUsers.roomOption()}
+        on:click={openManageParticipantsModal}
+    />
+{/if}
+
 
     {#if folder}
         <RoomOption
