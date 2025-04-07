b7087fc134cbcc4f5425966b266b01c7162699e4
Jade-Keina POGNON <jade.keina@gmail.com>
2025-04-07 19:30:24 +0200

    Fixed the design of the global Message

--- play/src/front/Components/Menu/Menu.svelte
+++ play/src/front/Components/Menu/Menu.svelte
@@ -252,7 +252,7 @@
             class="bg-contrast/80 h-[calc(100vh-20rem)] mt-0 @md/main-layout:mt-20 overflow-y-auto text-white pb-8 rounded-none @md/main-layout:rounded-tl overflow-y-scroll @md/main-layout:overflow-none h-full"
             id="submenu"
         >
-        <svelte:component this={activeComponent} {...props} />
+            <svelte:component this={activeComponent} {...props} />
         </div>
     </div>
 </div>
