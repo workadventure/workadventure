96e6779ae984f52ad64f28c9ff85ab0cc4d7cf36
yannisdia <y.diarra@workadventu.re>
2025-04-09 15:52:04 +0200

    run pretty

--- play/src/front/Chat/Components/RoomFolder.svelte
+++ play/src/front/Chat/Components/RoomFolder.svelte
@@ -76,68 +76,74 @@
         {#if isOpen}
             <div class="flex flex-col overflow-auto">
                 {#if $suggestedRooms.length > 0}
-                <div class={`mx-2 p-1 bg-contrast-300/10 rounded-lg mb-4`}>
-                    <div
-                        class={`group relative px-3 m-0 rounded-none text-white/75 hover:text-white h-11 hover:bg-contrast-200/10 w-full flex space-x-2 items-center`}
-                        class:mb-2={suggestedRoomsOpen}
-                    >
-                        <div class="flex items-center space-x-2 grow m-0 p-0">
+                    <div class={`mx-2 p-1 bg-contrast-300/10 rounded-lg mb-4`}>
+                        <div
+                            class={`group relative px-3 m-0 rounded-none text-white/75 hover:text-white h-11 hover:bg-contrast-200/10 w-full flex space-x-2 items-center`}
+                            class:mb-2={suggestedRoomsOpen}
+                        >
+                            <div class="flex items-center space-x-2 grow m-0 p-0">
+                                <button
+                                    class="flex items-center space-x-2 grow m-0 p-0"
+                                    data-testid="openSuggestedRooms"
+                                    on:click={toggleSuggestedRooms}
+                                >
+                                    <div class={`text-sm font-bold tracking-widest uppercase grow text-left`}>
+                                        {$LL.chat.suggestedRooms()}
+                                    </div>
+                                </button>
+                            </div>
+
                             <button
-                                class="flex items-center space-x-2 grow m-0 p-0"
-                                data-testid="openSuggestedRooms"
+                                class="transition-all group-hover:bg-white/10 p-1 rounded-lg aspect-square flex items-center justify-center text-white"
                                 on:click={toggleSuggestedRooms}
                             >
-                                <div
-                                    class={`text-sm font-bold tracking-widest uppercase grow text-left`}
-                                >
-                                    {$LL.chat.suggestedRooms()}
-                                </div>
+                                <IconChevronUp
+                                    class={`transform transition ${!suggestedRoomsOpen ? "" : "rotate-180"}`}
+                                />
                             </button>
                         </div>
-
-                        <button
-                            class="transition-all group-hover:bg-white/10 p-1 rounded-lg aspect-square flex items-center justify-center text-white"
-                            on:click={toggleSuggestedRooms}
-                        >
-                            <IconChevronUp
-                                class={`transform transition ${!suggestedRoomsOpen ? "" : "rotate-180"}`}
-                            />
-                        </button>
+                        {#if suggestedRoomsOpen}
+                            <div class="flex flex-col overflow-auto pl-3 pr-4 pb-3">
+                                <ShowMore items={$suggestedRooms} maxNumber={8} idKey="id" let:item={room}>
+                                    <RoomSuggested roomInformation={room} />
+                                </ShowMore>
+                            </div>
+                        {/if}
                     </div>
-                    {#if suggestedRoomsOpen}
-                        <div class="flex flex-col overflow-auto pl-3 pr-4 pb-3">
-                            <ShowMore items={$suggestedRooms} maxNumber={8} idKey="id" let:item={room}>
-                                <RoomSuggested roomInformation={room} />
-                            </ShowMore>
-                        </div>
-                    {/if}
-                </div>
-            {/if}
-        </div>
-        <div class="flex flex-col overflow-auto">
-            {#if $invitations.length > 0}
-                <div class="flex flex-col overflow-auto pl-3 pr-4 pb-3">
-                    <ShowMore items={$invitations} maxNumber={8} idKey="id" let:item={room}>
-                        <RoomInvitation {room} />
-                    </ShowMore>
-                </div>
-            {/if}
-            {#each Array.from($folders.values()) as folder (folder.id)}
-                <svelte:self {folder} rootFolder={false} />
-            {/each}
-            <ShowMore items={filteredRoom} maxNumber={8} idKey="id" let:item={room} showNothingToDisplayMessage={false}>
-                <Room {room} />
-            </ShowMore>
-            {#if $rooms.length === 0 && $folders.length === 0 && $suggestedRooms.length === 0}
-                <p
-                    class={`${
-                        rootFolder ? "self-center text-md text-gray-500" : "py-2 px-3 m-0 text-white/50 italic text-sm"
-                    }`}
+                {/if}
+            </div>
+            <div class="flex flex-col overflow-auto">
+                {#if $invitations.length > 0}
+                    <div class="flex flex-col overflow-auto pl-3 pr-4 pb-3">
+                        <ShowMore items={$invitations} maxNumber={8} idKey="id" let:item={room}>
+                            <RoomInvitation {room} />
+                        </ShowMore>
+                    </div>
+                {/if}
+                {#each Array.from($folders.values()) as folder (folder.id)}
+                    <svelte:self {folder} rootFolder={false} />
+                {/each}
+                <ShowMore
+                    items={filteredRoom}
+                    maxNumber={8}
+                    idKey="id"
+                    let:item={room}
+                    showNothingToDisplayMessage={false}
                 >
-                    {$LL.chat.nothingToDisplay()}
-                </p>
-            {/if}
-        </div>
+                    <Room {room} />
+                </ShowMore>
+                {#if $rooms.length === 0 && $folders.length === 0 && $suggestedRooms.length === 0}
+                    <p
+                        class={`${
+                            rootFolder
+                                ? "self-center text-md text-gray-500"
+                                : "py-2 px-3 m-0 text-white/50 italic text-sm"
+                        }`}
+                    >
+                        {$LL.chat.nothingToDisplay()}
+                    </p>
+                {/if}
+            </div>
         {/if}
     </div>
 </div>
