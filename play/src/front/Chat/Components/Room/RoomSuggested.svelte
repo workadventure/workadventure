69bbf6f68a41eaf7683e9db3ba65de4e8ae6856f
grégoire parant <gregoire@workadventu.re>
2025-04-14 16:46:02 +0200

    Remove legacy tailwind suffix in new component created from master (#4749)

--- play/src/front/Chat/Components/Room/RoomSuggested.svelte
+++ play/src/front/Chat/Components/Room/RoomSuggested.svelte
@@ -36,23 +36,23 @@
 </script>
 
 <div
-    class="tw-text-md tw-flex tw-gap-2 tw-flex-row tw-items-center hover:tw-bg-white tw-transition-all hover:tw-bg-opacity-10 hover:tw-rounded-md hover:!tw-cursor-pointer tw-p-2"
+    class="text-md flex gap-2 flex-row items-center hover:bg-white transition-all hover:bg-opacity-10 hover:rounded-md hover:!cursor-pointer p-2"
     data-testid="userInvitation"
 >
-    <div class="tw-relative">
+    <div class="relative">
         <Avatar avatarUrl={roomInformation.avatarUrl} fallbackName={roomName} />
     </div>
-    <div class="tw-m-0 tw-grow tw-text-sm tw-font-bold">
+    <div class="m-0 grow text-sm font-bold">
         {roomName}
     </div>
     {#if loadingInvitation}
-        <div class="tw-min-h-[60px] tw-text-md tw-flex tw-gap-2 tw-justify-center tw-flex-row tw-items-center tw-p-1">
-            <IconLoader class="tw-animate-spin" />
+        <div class="min-h-[60px] text-md flex gap-2 justify-center flex-row items-center p-1">
+            <IconLoader class="animate-spin" />
         </div>
     {:else}
-        <div class="tw-flex tw-gap-1">
+        <div class="flex gap-1">
             <button
-                class="tw-border tw-border-solid tw-border-success tw-text-success hover:tw-bg-success-400/10 tw-rounded tw-text-xs tw-py-1 tw-px-2 tw-m-0"
+                class="border border-solid border-success text-success hover:bg-success-400/10 rounded text-xs py-1 px-2 m-0"
                 data-testid="acceptInvitationButton"
                 on:click={() => joinRoom()}
             >
