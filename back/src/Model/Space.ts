c71277396560e88803ee534d72205a05fa081905
Grégoire Parant <gregoire@workadventu.re>
2025-04-18 14:16:05 +0200

    try to stabilize space and space watcher

--- back/src/Model/Space.ts
+++ back/src/Model/Space.ts
@@ -50,70 +50,61 @@ export class Space implements CustomJsonReplacerInterface {
             Sentry.captureException(e);
             debug("Error while adding user", e);
             // If we have an error, it means that the user list is not initialized
+            // So we need to remove user from the source watcher
+            this.removeUser(sourceWatcher, spaceUser.spaceUserId);
+        }
+    }
+    public updateUser(sourceWatcher: SpacesWatcher, spaceUser: SpaceUser, updateMask: string[]) {
+        try{
+            const usersList = this.usersList(sourceWatcher);
+            const user = usersList.get(spaceUser.spaceUserId);
+            if (!user) {
+                console.error("User not found in this space", spaceUser);
+                Sentry.captureMessage(`User not found in this space ${spaceUser.spaceUserId}`);
+                return;
+            }
+
+            const updateValues = applyFieldMask(spaceUser, updateMask);
+
+            merge(user, updateValues);
+
+            usersList.set(spaceUser.spaceUserId, user);
+        }catch (e) {
+            console.error("Error while updating user", e);
+            Sentry.captureException(e);
+            debug("Error while updating user", e);
+            // If we have an error, it means that the user list is not initialized
+            // So we need to remove user from the source watcher
+            this.removeUser(sourceWatcher, spaceUser.spaceUserId);
+        }
+    }
+    public removeUser(sourceWatcher: SpacesWatcher, spaceUserId: string) {
+        try{
+            const usersList = this.usersList(sourceWatcher);
+            usersList.delete(spaceUserId);
+            debug(`${this.name} : user => removed ${spaceUserId}`);
+
+            if (usersList.size === 0) {
+                debug(`${this.name} : users list => deleted ${sourceWatcher.id}`);
+                this.users.delete(sourceWatcher);
+            }
+        }catch (e) {
+            console.error("Error while removing user", e);
+            Sentry.captureException(e);
+            debug("Error while removing user", e);
+        }finally {
             this.notifyWatchers(
                 {
                     message: {
                         $case: "removeSpaceUserMessage",
                         removeSpaceUserMessage: RemoveSpaceUserMessage.fromPartial({
                             spaceName: this.name,
-                            spaceUserId: spaceUser.spaceUserId,
+                            spaceUserId: spaceUserId,
                         }),
                     },
                 },
                 sourceWatcher
             );
-        }
-    }
-    public updateUser(sourceWatcher: SpacesWatcher, spaceUser: SpaceUser, updateMask: string[]) {
-        const usersList = this.usersList(sourceWatcher);
-        const user = usersList.get(spaceUser.spaceUserId);
-        if (!user) {
-            console.error("User not found in this space", spaceUser);
-            Sentry.captureMessage(`User not found in this space ${spaceUser.spaceUserId}`);
-            return;
-        }
-
-        const updateValues = applyFieldMask(spaceUser, updateMask);
-
-        merge(user, updateValues);
-
-        usersList.set(spaceUser.spaceUserId, user);
-        this.notifyWatchers(
-            {
-                message: {
-                    $case: "updateSpaceUserMessage",
-                    updateSpaceUserMessage: {
-                        spaceName: this.name,
-                        user: spaceUser,
-                        updateMask,
-                    },
-                },
-            },
-            sourceWatcher
-        );
-        debug(`${this.name} : user => updated ${spaceUser.spaceUserId}`);
-    }
-    public removeUser(sourceWatcher: SpacesWatcher, spaceUserId: string) {
-        const usersList = this.usersList(sourceWatcher);
-        usersList.delete(spaceUserId);
-
-        this.notifyWatchers(
-            {
-                message: {
-                    $case: "removeSpaceUserMessage",
-                    removeSpaceUserMessage: RemoveSpaceUserMessage.fromPartial({
-                        spaceName: this.name,
-                        spaceUserId: spaceUserId,
-                    }),
-                },
-            },
-            sourceWatcher
-        );
-        debug(`${this.name} : user => removed ${spaceUserId}`);
-
-        if (usersList.size === 0) {
-            debug(`${this.name} : users list => deleted ${sourceWatcher.id}`);
-            this.users.delete(sourceWatcher);
         }
     }
 
