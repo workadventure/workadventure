08d6a8146780f92c9022200ae2295eb2149abd22
grégoire parant <gregoire@workadventu.re>
2025-04-23 16:14:41 +0200

    Fix end to end chat test (#4774)
    * Fix end to end chat test
    * Fix google workspace test end to end

--- play/src/front/Chat/Components/UserList/User.svelte
+++ play/src/front/Chat/Components/UserList/User.svelte
@@ -147,7 +147,7 @@
                     <UserActionButton {user} />
                 {/if}
             </div>
-            {#if !isMe && !showRoomCreationInProgress && user.chatId !== user.uuid}
+            {#if !isMe && !showRoomCreationInProgress}
                 <button
                     class="transition-all hover:bg-white/10 p-2 rounded-md aspect-square flex items-center justify-center text-white m-0"
                     data-testId={`send-message-${user.username}`}
