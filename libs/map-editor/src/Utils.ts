e07bfade9d8e5508b2beb184ca05eb755d3130cf
Antoine <arotakerpro@gmail.com>
2025-04-30 16:20:59 +0200

    introduce an error

--- libs/map-editor/src/Utils.ts
+++ libs/map-editor/src/Utils.ts
@@ -5,7 +5,7 @@ export function getSpeakerMegaphoneAreaName(
     areaId: string
 ): string | undefined {
     for (const area of areas.values()) {
-        const speakerMegaphonePropertyRaw = area.properties.find((property) => property.type === "speakerMegaphone");
+        const speakerMegaphonePropertyRaw = area.properties.type === "speakerMegaphone"; // Erreur ici
         if (speakerMegaphonePropertyRaw) {
             const speakerMegaphoneProperty = SpeakerMegaphonePropertyData.safeParse(speakerMegaphonePropertyRaw);
             if (speakerMegaphoneProperty.success && area.id === areaId) {
