94d389106950d171ef2849fe41d02dae367d86b8
Antoine <arotakerpro@gmail.com>
2025-05-05 17:04:02 +0200

    try to fix ci

--- play/src/front/Components/Input/ButtonClose.svelte
+++ play/src/front/Components/Input/ButtonClose.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
-    import {IconX} from "@wa-icons";
+    import { IconX } from "@wa-icons";
 
     export let dataTestId: string | undefined = undefined;
     export let bgColor = "bg-white/10";
@@ -8,13 +8,14 @@
     export let textColor = "text-white";
     export let size: "xs" | "sm" | "md" | "lg" = "lg";
 
-    $: sizeClasses = size === "xs"
-        ? "h-6 w-6 text-sm"
-        : size === "sm"
+    $: sizeClasses =
+        size === "xs"
+            ? "h-6 w-6 text-sm"
+            : size === "sm"
             ? "h-8 w-8 text-base"
             : size === "md"
-                ? "h-10 w-10 text-lg"
-                    : "h-12 w-12 text-2xl";
+            ? "h-10 w-10 text-lg"
+            : "h-12 w-12 text-2xl";
 
     const dispatch = createEventDispatcher();
 
@@ -27,9 +28,9 @@
 
 <button
     type="button"
-    class="{sizeClasses} rounded backdrop-blur close-window transition-all aspect-square text-2xl flex items-center justify-center !{textColor} {bgColor} hover:{hoverColor}"
+    class="{sizeClasses} close-btn rounded backdrop-blur close-window transition-all aspect-square text-2xl flex items-center justify-center !{textColor} {bgColor} hover:{hoverColor}"
     data-testid={dataTestId}
     on:click={handleClick}
-    >
-    <IconX/>
+>
+    <IconX />
 </button>
