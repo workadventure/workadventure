de4f4cc6e3f4611e5f35310f1e8ded9c001df3da
Antoine <arotakerpro@gmail.com>
2025-05-06 18:10:49 +0200

    overflow scroll on code block in chat

--- play/src/front/Chat/Components/Room/Message/MessageText.svelte
+++ play/src/front/Chat/Components/Room/Message/MessageText.svelte
@@ -99,6 +99,33 @@
             text-decoration: underline;
             opacity: 0.75;
         }
+        pre {
+            margin: 0;
+        }
+        code {
+            display: block;
+            width: 100%;
+            background-color: rgba(255, 255, 255, 0.1);
+            padding: 0.25em 0.5em;
+            border-radius: 8px;
+            overflow-x: auto;
+            padding: 0.2em 0.5em;
+        }
+        ::-webkit-scrollbar {
+            width: 2px;
+            height: 6px;
+        }
+
+        /* Track */
+        /* Track */
+        ::-webkit-scrollbar-track {
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: rgba(255, 255, 255, 0.5);
+            border-radius: 3px;
+        }
     </style>
 </div>
 
