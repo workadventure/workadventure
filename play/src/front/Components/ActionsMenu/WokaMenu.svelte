0dcf4905666dc828f696f008f9089d306c0e1fec
Jade-Keina P. <127255892+jadekeina@users.noreply.github.com>
2025-05-19 10:54:07 +0200

    Added the Woka in the Business Card (#4847)
    * Added the woka in the visit Card 1
    
    * Added the woka in the visit card

--- play/src/front/Components/ActionsMenu/WokaMenu.svelte
+++ play/src/front/Components/ActionsMenu/WokaMenu.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
     import type { Unsubscriber } from "svelte/store";
     import { onDestroy } from "svelte";
-    import { actionsMenuStore } from "../../Stores/ActionsMenuStore";
+    import { wokaMenuStore } from "../../Stores/WokaMenuStore";
     import ButtonClose from "../Input/ButtonClose.svelte";
     import VisitCard from "../VisitCard/VisitCard.svelte";
+    import WokaFromUserId from "../Woka/WokaFromUserId.svelte";
 
-    import type { ActionsMenuAction, ActionsMenuData } from "../../Stores/ActionsMenuStore";
+    import type { WokaMenuAction, WokaMenuData } from "../../Stores/WokaMenuStore";
     import { analyticsClient } from "../../Administration/AnalyticsClient";
     import LL from "../../../i18n/i18n-svelte";
 
-    let actionsMenuData: ActionsMenuData | undefined;
-    let sortedActions: ActionsMenuAction[] | undefined;
+    let wokaMenuData: WokaMenuData | undefined;
+    let sortedActions: WokaMenuAction[] | undefined;
 
-    let actionsMenuStoreUnsubscriber: Unsubscriber | null;
+    let wokaMenuStoreUnsubscriber: Unsubscriber | null;
 
     function onKeyDown(e: KeyboardEvent) {
         if (e.key === "Escape") {
@@ -21,15 +22,15 @@
     }
 
     function closeActionsMenu() {
-        actionsMenuStore.clear();
+        wokaMenuStore.clear();
     }
 
     let buttonsLayout: "row" | "column" = "row";
 
-    actionsMenuStoreUnsubscriber = actionsMenuStore.subscribe((value) => {
-        actionsMenuData = value;
-        if (actionsMenuData) {
-            sortedActions = [...actionsMenuData.actions.values()].sort((a, b) => {
+    wokaMenuStoreUnsubscriber = wokaMenuStore.subscribe((value) => {
+        wokaMenuData = value;
+        if (wokaMenuData) {
+            sortedActions = [...wokaMenuData.actions.values()].sort((a, b) => {
                 const ap = a.priority ?? 0;
                 const bp = b.priority ?? 0;
                 if (ap > bp) {
@@ -41,7 +42,7 @@
                     return 0;
                 }
             });
-            const nbButtons = sortedActions.length + (actionsMenuData.menuName ? 0 : 1);
+            const nbButtons = sortedActions.length + (wokaMenuData.wokaName ? 0 : 1);
             if (nbButtons > 2) {
                 buttonsLayout = "column";
             } else {
@@ -51,20 +52,20 @@
     });
 
     onDestroy(() => {
-        if (actionsMenuStoreUnsubscriber) {
-            actionsMenuStoreUnsubscriber();
+        if (wokaMenuStoreUnsubscriber) {
+            wokaMenuStoreUnsubscriber();
         }
     });
 </script>
 
 <svelte:window on:keydown={onKeyDown} />
 
-{#if actionsMenuData}
+{#if wokaMenuData}
     <div
-        class="absolute max-w-md z-50 bg-contrast/80 transition-all backdrop-blur rounded-lg pointer-events-auto overflow-hidden top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+        class="absolute left-0 right-0 m-auto max-w-md z-50 bg-contrast/80 transition-all backdrop-blur rounded-lg pointer-events-auto overflow-hidden top-1/2 -translate-y-1/2"
         data-testid="actions-menu"
     >
-        {#if actionsMenuData.menuName}
+        {#if wokaMenuData.wokaName}
             <div>
                 <div class="w-full bg-cover relative">
                     <div class="absolute top-2 right-2">
@@ -72,40 +73,45 @@
                     </div>
 
                     <div class="flex items-center justify-center p-2">
-                        <div class="text-white flex flex-col justify-center items-center font-bold text-xl pl-3">
-                            <div class="w-max ml-5 mt-[29px] ">
-                                <h3>{actionsMenuData.menuName}</h3>
+                        <div class="text-white flex flex-col justify-center items-center font-bold text-xl ">
+                            <div
+                                id="woka"
+                                class=" bt-3 overflow-hidden mt-3 border w-fit h-fit rounded-lg cursor-not-allowed bg-[rgb(103,185,133)]"
+                            >
+                                <WokaFromUserId
+                                    userId={wokaMenuData.userId}
+                                    placeholderSrc="/assets/placeholder-woka.png"
+                                    customWidth="4rem"
+                                />
+                            </div>
+                            <div class=" w-max mt-[29px] ">
+                                <h3>{wokaMenuData.wokaName}</h3>
                             </div>
                         </div>
                     </div>
 
-                    {#if actionsMenuData.visitCardUrl}
+                    {#if wokaMenuData.visitCardUrl}
                         <VisitCard
-                            visitCardUrl={actionsMenuData.visitCardUrl}
+                            visitCardUrl={wokaMenuData.visitCardUrl}
                             isEmbedded={true}
                             showSendMessageButton={false}
                         />
                     {/if}
                 </div>
-                {#if actionsMenuData.menuDescription}
-                    <p class="text-sm opacity-50 text-white px-4">
-                        {actionsMenuData.menuDescription}
-                    </p>
-                {/if}
             </div>
         {/if}
 
         {#if sortedActions}
             <div
-                class="flex items-center bg-contrast p-2"
-                class:margin-close={!actionsMenuData.menuName}
+                class="flex items-center bg-contrast px-2"
+                class:margin-close={!wokaMenuData.wokaName}
                 class:flex-col={buttonsLayout === "column"}
                 class:flex-row={buttonsLayout === "row"}
             >
                 {#each sortedActions ?? [] as action (action.uuid)}
                     <button
                         type="button"
-                        class="btn btn-light btn-ghost text-nowrap justify-center w-full {action.style ?? ''}"
+                        class="btn btn-light btn-ghost text-nowrap justify-center m-2 w-full {action.style ?? ''}"
                         class:mx-2={buttonsLayout === "column"}
                         on:click={() => analyticsClient.clickPropertyMapEditor(action.actionName, action.style)}
                         on:click|preventDefault={() => {
@@ -125,7 +131,7 @@
                         </span>
                     </button>
                 {/each}
-                {#if !actionsMenuData.menuName}
+                {#if !wokaMenuData.wokaName}
                     <button
                         type="button"
                         class="btn btn-light btn-ghost text-nowrap justify-center m-2 w-full"
