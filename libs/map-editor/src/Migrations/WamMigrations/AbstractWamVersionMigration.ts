12fb4811ed8510211463194f57eb79b4ed5073d6
yannisdia <y.diarra@workadventu.re>
2025-05-28 10:51:34 +0200

    fix lint

--- libs/map-editor/src/Migrations/WamMigrations/AbstractWamVersionMigration.ts
+++ libs/map-editor/src/Migrations/WamMigrations/AbstractWamVersionMigration.ts
@@ -4,7 +4,7 @@ import { EntityCollectionRaw, WAMFileFormat } from "../../types";
  * Eslint rules are disabled here, because we don't want to type for all possible version of the file.
  * Only the last version has his own type
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any , @typescript-eslint/no-unsafe-member-access */
 
 export abstract class VersionMigration<T extends WAMFileFormat | EntityCollectionRaw> {
     abstract version: string;
