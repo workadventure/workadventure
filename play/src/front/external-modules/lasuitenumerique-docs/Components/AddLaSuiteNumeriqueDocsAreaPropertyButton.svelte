3415d277c62edc0479652ff6c3139b6f92b8f8c2
David Négrier <d.negrier@thecodingmachine.com>
2025-06-02 14:13:31 +0200

    [WIP] Adding first work on integrating LaSuiteNumérique Docs with WorkAdventure

---
+++ play/src/front/external-modules/lasuitenumerique-docs/Components/AddLaSuiteNumeriqueDocsAreaPropertyButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import AddPropertyButton from "../../../Components/MapEditor/PropertyEditor/AddPropertyButton.svelte";
+    import docsLogo from "./images/icon-docs.svg";
+
+    const dispatch = createEventDispatcher<{
+        click: CustomEvent<undefined>;
+    }>();
+</script>
+
+<AddPropertyButton
+    headerText="Open docs"
+    descriptionText="Open a Doc (from La Suite Numérique)"
+    img={docsLogo}
+    style="z-index: 200"
+    testId="laSuiteNumeriqueDocsButton"
+    on:click={(event) => {
+        dispatch("click", event);
+    }}
+/>
