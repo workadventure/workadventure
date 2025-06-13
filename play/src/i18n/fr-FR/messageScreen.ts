15fe1560f9965a364665d2d2f0b3a038ff836ae2
Antoine <arotakerpro@gmail.com>
2025-06-13 12:31:37 +0200

    fix typo

--- play/src/i18n/fr-FR/messageScreen.ts
+++ play/src/i18n/fr-FR/messageScreen.ts
@@ -1,7 +1,7 @@
 import type { BaseTranslation } from "../i18n-types";
 
 const messageScreen: BaseTranslation = {
-    connecting: "Connection ...",
+    connecting: "Connexion ...",
     pleaseWait: "Veuillez patienter pendant que nous nous connectons à la salle.",
 };
 export default messageScreen;
