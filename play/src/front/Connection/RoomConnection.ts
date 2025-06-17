46858fd8083474463b8d54144c94498d22c52b56
yannisdia <y.diarra@workadventu.re>
2025-06-17 12:57:42 +0200

    delete emitUpdateSpaceFilter

--- play/src/front/Connection/RoomConnection.ts
+++ play/src/front/Connection/RoomConnection.ts
@@ -1377,16 +1377,6 @@ export class RoomConnection implements RoomConnection {
         });
     }
 
-    //TODO : delete in tests
-    // public emitUpdateSpaceFilter(filter: UpdateSpaceFilterMessage) {
-    //     this.send({
-    //         message: {
-    //             $case: "updateSpaceFilterMessage",
-    //             updateSpaceFilterMessage: filter,
-    //         },
-    //     });
-    // }
-
     public emitRemoveSpaceFilter(filter: RemoveSpaceFilterMessage) {
         this.send({
             message: {
