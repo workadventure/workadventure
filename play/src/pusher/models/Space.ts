bda6992e187886b154543dd1b71c80b7b81568f4
yannisdia <y.diarra@workadventu.re>
2025-06-18 16:06:20 +0200

    [PUSHER]: move back connection in space

--- play/src/pusher/models/Space.ts
+++ play/src/pusher/models/Space.ts
@@ -7,12 +7,16 @@ import {
     SpaceUser,
     SubMessage,
     FilterType,
+    BackToPusherSpaceMessage,
 } from "@workadventure/messages";
 import { applyFieldMask } from "protobuf-fieldmask";
 import merge from "lodash/merge";
 import Debug from "debug";
 import * as Sentry from "@sentry/node";
+import { z } from "zod";
+import { asError } from "catch-unknown";
 import { Socket } from "../services/SocketManager";
+import { apiClientRepository } from "../services/ApiClientRepository";
 import { BackSpaceConnection } from "./Websocket/SocketData";
 import { EventProcessor } from "./EventProcessor";
 
@@ -20,7 +24,7 @@ export type SpaceUserExtended = {
     lowercaseName: string;
     // If the user is connected to this pusher, we store the socket to be able to contact the user directly.
     // Useful to forward public and private event that are dispatched even if the space is not watched.
-    client: Socket | undefined;
+    //client: Socket | undefined;
 } & SpaceUser;
 
 type PartialSpaceUser = Partial<Omit<SpaceUser, "spaceUserId">> & Pick<SpaceUser, "spaceUserId">;
@@ -43,20 +47,166 @@ export class Space {
     // The list of users connected to THIS pusher specifically
     private readonly _localConnectedUser: Map<string, Socket>;
     private readonly _localWatchers: Set<string> = new Set<string>();
+    private spaceStreamToBackPromise: Promise<BackSpaceConnection> | undefined;
+    public readonly backId: number;
 
     constructor(
         public readonly name: string,
         // The local name is the name of the space in the browser (i.e. the name without the "world" prefix)
         private readonly localName: string,
-        private spaceStreamToBack: BackSpaceConnection,
-        public backId: number,
         private eventProcessor: EventProcessor,
-        private _filterType: FilterType
+        private _filterType: FilterType,
+        private _apiClientRepository = apiClientRepository
     ) {
         this._users = new Map<string, SpaceUserExtended>();
         this._metadata = new Map<string, unknown>();
         this._localConnectedUser = new Map<string, Socket>();
+        this.backId = this._apiClientRepository.getIndex(this.name);
         debug(`created : ${name}`);
+    }
+
+    public initSpace() {
+        this.spaceStreamToBackPromise = (async () => {
+            const apiSpaceClient = await this._apiClientRepository.getSpaceClient(this.name);
+            const spaceStreamToBack = apiSpaceClient.watchSpace() as BackSpaceConnection;
+            spaceStreamToBack
+                .on("data", (message: BackToPusherSpaceMessage) => {
+                    if (!message.message) {
+                        console.warn("spaceStreamToBack => Empty message received.", message);
+                        return;
+                    }
+                    try {
+                        switch (message.message.$case) {
+                            case "addSpaceUserMessage": {
+                                const addSpaceUserMessage = noUndefined(message.message.addSpaceUserMessage);
+                                this.addUser(addSpaceUserMessage.user);
+                                break;
+                            }
+                            case "updateSpaceUserMessage": {
+                                const updateSpaceUserMessage = noUndefined(message.message.updateSpaceUserMessage);
+                                this.updateUser(updateSpaceUserMessage.user, updateSpaceUserMessage.updateMask);
+                                break;
+                            }
+                            case "removeSpaceUserMessage": {
+                                this.removeUser(message.message.removeSpaceUserMessage.spaceUserId);
+                                break;
+                            }
+                            case "updateSpaceMetadataMessage": {
+                                const updateSpaceMetadataMessage = message.message.updateSpaceMetadataMessage;
+
+                                const isMetadata = z
+                                    .record(z.string(), z.unknown())
+                                    .safeParse(JSON.parse(updateSpaceMetadataMessage.metadata));
+                                if (!isMetadata.success) {
+                                    Sentry.captureException(
+                                        `Invalid metadata received. ${updateSpaceMetadataMessage.metadata}`
+                                    );
+                                    console.error("Invalid metadata received.", updateSpaceMetadataMessage.metadata);
+                                    return;
+                                }
+                                this.updateMetadata(isMetadata.data);
+                                break;
+                            }
+                            case "pingMessage": {
+                                if (spaceStreamToBack.pingTimeout) {
+                                    clearTimeout(spaceStreamToBack.pingTimeout);
+                                    spaceStreamToBack.pingTimeout = undefined;
+                                }
+                                const pusherToBackMessage: PusherToBackSpaceMessage = {
+                                    message: {
+                                        $case: "pongMessage",
+                                        pongMessage: {},
+                                    },
+                                } as PusherToBackSpaceMessage;
+                                spaceStreamToBack.write(pusherToBackMessage);
+
+                                spaceStreamToBack.pingTimeout = setTimeout(() => {
+                                    console.error("Error spaceStreamToBack timed out for back:", this.backId);
+                                    Sentry.captureException(
+                                        "Error spaceStreamToBack timed out for back: " + this.backId
+                                    );
+                                    spaceStreamToBack.end();
+                                    this.spaceStreamToBackPromise = undefined;
+                                    this.initSpace();
+                                }, 1000 * 60);
+                                break;
+                            }
+                            case "kickOffMessage": {
+                                debug("[space] kickOffSMessage received");
+                                this.forwardMessageToSpaceBack({
+                                    $case: "kickOffMessage",
+                                    kickOffMessage: {
+                                        userId: message.message.kickOffMessage.userId,
+                                        spaceName: message.message.kickOffMessage.spaceName,
+                                        filterName: message.message.kickOffMessage.filterName,
+                                    },
+                                });
+                                break;
+                            }
+                            case "publicEvent": {
+                                debug("[space] publicEvent received");
+                                this.sendPublicEvent(noUndefined(message.message.publicEvent));
+                                break;
+                            }
+                            case "privateEvent": {
+                                debug("[space] privateEvent received");
+                                const privateEvent = message.message.privateEvent;
+                                this.sendPrivateEvent(noUndefined(privateEvent));
+                                break;
+                            }
+                            default: {
+                                const _exhaustiveCheck: never = message.message;
+                            }
+                        }
+                    } catch (error) {
+                        this.notifyAllUsers(
+                            {
+                                message: {
+                                    $case: "errorMessage",
+                                    errorMessage: {
+                                        message:
+                                            "An error occurred in pusher connection to back: " + asError(error).message,
+                                    },
+                                },
+                            },
+                            "pusher"
+                        );
+
+                        console.error(error);
+                        Sentry.captureException(error);
+                    }
+                })
+                .on("end", () => {
+                    debug("[space] spaceStreamsToBack ended");
+                    if (spaceStreamToBack.pingTimeout) clearTimeout(spaceStreamToBack.pingTimeout);
+                    //this.cleanup();
+                    this.spaceStreamToBackPromise = undefined;
+                    this.initSpace();
+                })
+                .on("error", (err: Error) => {
+                    console.error(
+                        "Error in connection to back server '" +
+                            apiSpaceClient.getChannel().getTarget() +
+                            "' for space '" +
+                            this.name +
+                            "':",
+                        err
+                    );
+                    Sentry.captureException(err);
+                });
+
+            spaceStreamToBack.write({
+                message: {
+                    $case: "joinSpaceMessage",
+                    joinSpaceMessage: {
+                        spaceName: this.name,
+                        filterType: this.filterType,
+                    },
+                },
+            });
+
+            return spaceStreamToBack;
+        })();
     }
 
     public registerUserAndForwardMessageToBack(spaceUser: SpaceUser, client: Socket) {
@@ -97,10 +247,9 @@ export class Space {
     }
 
     // This function is called when we received a message from the back
-    public addUser(spaceUser: SpaceUser, client: Socket | undefined) {
+    public addUser(spaceUser: SpaceUser) {
         const user: Partial<SpaceUserExtended> = spaceUser;
         user.lowercaseName = spaceUser.name.toLowerCase();
-        user.client = client;
 
         if (this._users.has(spaceUser.spaceUserId)) {
             throw new Error(`User ${spaceUser.spaceUserId} already exists in space ${this.name}`);
@@ -305,20 +454,6 @@ export class Space {
         this.notifyMe(watcher, subMessage);
     }
 
-    /*private notifyMeUpdateUser(watcher: Socket, user: SpaceUserExtended, filterName: string | undefined) {
-        const subMessage: SubMessage = {
-            message: {
-                $case: "updateSpaceUserMessage",
-                updateSpaceUserMessage: {
-                    spaceName: this.removeSpaceNamePrefix(this.name, watcher.getUserData().world),
-                    user,
-                    filterName,
-                },
-            },
-        };
-        this.notifyMe(watcher, subMessage);
-    }*/
-
     public isEmpty() {
         return this._localConnectedUser.size === 0;
     }
@@ -388,7 +523,13 @@ export class Space {
             throw new Error(`Private message sender ${message.senderUserId} not found in space ${this.name}`);
         }
 
-        receiver.client?.getUserData().emitInBatch({
+        const receiverSocket = this._localConnectedUser.get(message.receiverUserId);
+
+        if (!receiverSocket) {
+            throw new Error(`Private message receiver ${message.receiverUserId} not connected to this pusher`);
+        }
+
+        receiverSocket.getUserData().emitInBatch({
             message: {
                 $case: "privateEvent",
                 privateEvent: {
@@ -409,23 +550,29 @@ export class Space {
      * It is used solely for public events.
      */
     private notifyAllUsers(subMessage: SubMessage, senderId: string) {
-        /*this.clientWatchers.forEach((watcher) => {
-            const socketData = watcher.getUserData();
-            debug(`${this.name} : kickOff sent to ${socketData.name}`);
-            socketData.emitInBatch(subMessage);
-        });*/
-
-        for (const user of this._users.values()) {
-            if (user.client && user.spaceUserId !== senderId) {
-                user.client.getUserData().emitInBatch(subMessage);
+        for (const user of this._localConnectedUser.values()) {
+            const userData = user.getUserData();
+            if (userData.spaceUser.spaceUserId !== senderId) {
+                userData.emitInBatch(subMessage);
             }
         }
     }
 
     public forwardMessageToSpaceBack(pusherToBackSpaceMessage: PusherToBackSpaceMessage["message"]) {
-        this.spaceStreamToBack.write({
-            message: pusherToBackSpaceMessage,
-        });
+        if (!this.spaceStreamToBackPromise) {
+            throw new Error("Space stream to back not found");
+        }
+
+        this.spaceStreamToBackPromise
+            .then((spaceStreamToBack) => {
+                spaceStreamToBack.write({
+                    message: pusherToBackSpaceMessage,
+                });
+            })
+            .catch((error) => {
+                console.error("Error while forwarding message to space back", error);
+                Sentry.captureException(error);
+            });
     }
 
     get metadata(): Map<string, unknown> {
@@ -460,7 +607,27 @@ export class Space {
         // Finally, let's send a message to the front to warn that the space is deleted
     }
 
+    public closeBackConnection(): void {
+        if (this.spaceStreamToBackPromise) {
+            this.spaceStreamToBackPromise
+                .then((spaceStreamToBack) => spaceStreamToBack.end())
+                .catch((error) => {
+                    console.error("Error while closing space back connection", error);
+                    Sentry.captureException(error);
+                });
+        }
+    }
+
     get filterType(): FilterType {
         return this._filterType;
     }
 }
+
+/*
+TODO : 
+    - Séparation en 2 classes forwarder/Proxy et forwarder to front 
+    - le space ne gere que la connexion au back 
+    - test unitaire sur les 2 classes 
+    - on peut faire un test unitaire sur l'event "end" en mockant la connexion le on devient juste une map ou on ajoute les callback bien on déclenche le callback + expect 
+    - revoir le coté back et voir si on peut faire un test unitaire sur le back 
+*/
