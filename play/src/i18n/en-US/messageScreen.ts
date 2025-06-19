611ee297325299eb896c9e0e39081f22df96b39b
Antoine <arotakerpro@gmail.com>
2025-06-19 09:08:03 +0200

    fix typo

--- play/src/i18n/en-US/messageScreen.ts
+++ play/src/i18n/en-US/messageScreen.ts
@@ -2,6 +2,6 @@ import type { BaseTranslation } from "../i18n-types";
 
 const messageScreen: BaseTranslation = {
     connecting: "Connecting...",
-    pleaseWait: "Please wait while we connect to the room.",
+    pleaseWait: "Please wait while we connect you to the room.",
 };
 export default messageScreen;
--- play/src/i18n/fr-FR/messageScreen.ts
+++ play/src/i18n/fr-FR/messageScreen.ts
@@ -2,6 +2,6 @@ import type { BaseTranslation } from "../i18n-types";
 
 const messageScreen: BaseTranslation = {
     connecting: "Connexion ...",
-    pleaseWait: "Veuillez patienter pendant que nous nous connectons à la salle.",
+    pleaseWait: "Veuillez patienter pendant que nous vous connectons à la salle.",
 };
 export default messageScreen;
