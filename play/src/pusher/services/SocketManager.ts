1ee88d084dd1ae362b88ee791873cc5d46b30452
yannisdia <y.diarra@workadventu.re>
2025-06-26 15:17:12 +0200

    update error message

--- play/src/pusher/services/SocketManager.ts
+++ play/src/pusher/services/SocketManager.ts
@@ -361,7 +361,7 @@ export class SocketManager implements ZoneEventListener {
         }
 
         if (space.filterType !== filterType) {
-            throw new Error("Space filter type mismatch");
+            throw new Error("Error: Space filter type mismatch");
         }
 
         const deferred = new Deferred<void>();
