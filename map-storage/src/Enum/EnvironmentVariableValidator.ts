3348c7594d06ea8f566a184613487bad116bf6fa
David Négrier <d.negrier@thecodingmachine.com>
2025-07-04 17:04:10 +0200

    Add S3 configuration options for connection and request timeouts
    In the map-storage container, you can now set the following options as environment variables:
    
    - **S3_MAX_PARALLEL_REQUESTS**: the maximum number of parallel requests one can do to S3
    - **S3_CONNECTION_TIMEOUT**: The timeout in milliseconds for the S3 connection in milliseconds. Defaults to 5000 (5 seconds).
    - **S3_REQUEST_TIMEOUT**: The timeout in milliseconds for the S3 requests in milliseconds. Defaults to 60000 (60 seconds).

--- map-storage/src/Enum/EnvironmentVariableValidator.ts
+++ map-storage/src/Enum/EnvironmentVariableValidator.ts
@@ -22,6 +22,15 @@ export const EnvironmentVariables = z.object({
         .optional()
         .transform(emptyStringToUndefined)
         .describe("URL of the S3 endpoint."),
+    S3_MAX_PARALLEL_REQUESTS: PositiveIntAsString.optional()
+        .transform((val) => toNumber(val, 50))
+        .describe("The maximum parallel number of requests done to the S3 bucket. Defaults to 50."),
+    S3_CONNECTION_TIMEOUT: PositiveIntAsString.optional()
+        .transform((val) => toNumber(val, 5000))
+        .describe("The timeout in milliseconds for the S3 connection in milliseconds. Defaults to 5000 (5 seconds)."),
+    S3_REQUEST_TIMEOUT: PositiveIntAsString.optional()
+        .transform((val) => toNumber(val, 60000))
+        .describe("The timeout in milliseconds for the S3 requests in milliseconds. Defaults to 60000 (60 seconds)."),
     //UPLOADER_AWS_SIGNED_URL_EXPIRATION: PositiveIntAsString.optional(),
     S3_UPLOAD_CONCURRENCY_LIMIT: PositiveIntAsString.optional().transform((val) => toNumber(val, 100)),
     MAX_UNCOMPRESSED_SIZE: PositiveIntAsString.optional()
