4b6c1d4424b14033fe93f0fde4e600bd2c19d71c
Felipe Cecagno <fcecagno@gmail.com>
2025-07-30 10:03:43 -0300

    fix: form being exported as login

--- play/src/i18n/en-US/form.ts
+++ play/src/i18n/en-US/form.ts
@@ -1,7 +1,7 @@
 import type { BaseTranslation } from "../i18n-types";
 
-const login: BaseTranslation = {
+const form: BaseTranslation = {
     optional: "Optional",
 };
 
-export default login;
+export default form;
