80b036db701b469117eb730d5c062d7d57400b5f
jozzzzzz <113442867+jozzzzzz@users.noreply.github.com>
2025-08-05 19:11:52 +0200

    Allow upload file for guests (#5006)
    * allow drop in right area
    
    * allow custom entity
    
    ---------
    
    Co-authored-by: jojo <j.hayot@workadventu.re>

--- play/src/front/Phaser/Game/MapEditor/Tools/EntityEditorTool.ts
+++ play/src/front/Phaser/Game/MapEditor/Tools/EntityEditorTool.ts
@@ -229,11 +229,6 @@ export class EntityEditorTool extends EntityRelatedEditorTool {
     protected subscribeToEntityUpload() {
         this.mapEditorEntityUploadStoreUnsubscriber = mapEditorEntityUploadEventStore.subscribe(
             (uploadEntityMessage) => {
-                const isAdmin = this.scene.connection?.isAdmin();
-                const isEditor = this.scene.connection?.hasTag("editor");
-                if (!isAdmin && !isEditor) {
-                    return;
-                }
                 if (uploadEntityMessage) {
                     (async () => {
                         await this.mapEditorModeManager.executeCommand(
