543ccafbb4c020418a7c7f26bdd56ddf021c4dde
Felipe Cecagno <fcecagno@gmail.com>
2025-08-08 09:21:16 -0300

    fix: do not use BaseTranslation

--- play/src/i18n/pt-BR/companion.ts
+++ play/src/i18n/pt-BR/companion.ts
@@ -1,6 +1,7 @@
-import type { BaseTranslation } from "../i18n-types";
+import type { DeepPartial } from "../DeepPartial";
+import type { Translation } from "../i18n-types";
 
-const companion: BaseTranslation = {
+const companion: DeepPartial<Translation["companion"]> = {
     select: {
         title: "Selecione seu companheiro",
         any: "Sem companheiro",
