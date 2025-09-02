fba80f9cabc8a311c9a23c38cce57b11f9765230
Grégoire Parant <gregoire@workadventu.re>
2025-09-02 16:23:36 +0200

    Mute and Unmute audio

--- play/src/front/Components/UI/AudioPlaying.svelte
+++ play/src/front/Components/UI/AudioPlaying.svelte
@@ -1,28 +1,68 @@
 <script lang="ts">
     import { fly } from "svelte/transition";
-    import { afterUpdate } from "svelte";
+    import { onDestroy, afterUpdate } from "svelte";
     import { soundPlayingStore } from "../../Stores/SoundPlayingStore";
     import { LL } from "../../../i18n/i18n-svelte";
     import megaphoneImg from "./images/megaphone.svg";
+    import { IconPause, IconPauseFilled, IconPlay, IconPlayFilled } from "@wa-icons";
 
     export let url: string;
     let audio: HTMLAudioElement;
+    let muted = false;
+    let timeOutToEnd: ReturnType<typeof setTimeout>;;
 
     function soundEnded() {
         soundPlayingStore.soundEnded();
     }
 
+    function toggleMuteUnmuteAudio() {
+        if (muted) {
+            unmute();
+        } else {
+            mute();
+        }
+    }
+
+    function unmute(){
+        if(timeOutToEnd) clearTimeout(timeOutToEnd);
+        audio.muted = false;
+        muted = false;
+    }
+    function mute(){
+        audio.muted = true;
+        muted = true;
+        timeOutToEnd = setTimeout(() => {
+           soundEnded();
+        }, 5000);
+    }
+
     afterUpdate(() => {
         audio.play().catch((e) => console.error(e));
     });
+
+    onDestroy(() => {
+        if (timeOutToEnd) clearTimeout(timeOutToEnd);
+    });
 </script>
 
-<div class="audio-playing bg-dark-blue/95" transition:fly={{ x: 210, duration: 500 }}>
-    <img src={megaphoneImg} alt="Audio playing" class="bg-medium-purple rounded-full" />
+<div class="audio-playing bg-contrast/80 backdrop-blur pointer-events-auto" transition:fly={{ x: 210, duration: 500 }}>
+    <img src={megaphoneImg} alt="Audio playing" class="rounded-full" />
     <p>{$LL.audio.message()}</p>
     <audio bind:this={audio} src={url} on:ended={soundEnded}>
         <track kind="captions" />
     </audio>
+    <!-- Button to stop the audio -->
+    <button class="group/audio-playing absolute bg-contrast/80 hover:bg-contrast backdrop-blur hover:backdrop-blur-0 p-3 w-14 -left-16 rounded-lg top-0 bottom-0 flex justify-center items-center cursor-pointer"
+        on:click={toggleMuteUnmuteAudio}
+    >
+        {#if muted == false}
+            <IconPause class="block group-hover/audio-playing:hidden h-full w-full" />
+            <IconPauseFilled class="hidden group-hover/audio-playing:block h-full w-full" />
+        {:else}
+            <IconPlay class="block group-hover/audio-playing:hidden h-full w-full" />
+            <IconPlayFilled class="hidden group-hover/audio-playing:block h-full w-full" />
+        {/if}
+    </button>
 </div>
 
 <style lang="scss">
