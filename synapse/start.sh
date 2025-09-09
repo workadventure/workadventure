3e814625025b0e3d017a7c46afd6845e09e59a19
Isabelle Silva <isabelle.silva@dnxbrasil.com>
2025-09-09 17:46:31 -0300

    [DEV] subindo arquivos para a ec2

--- synapse/start.sh
+++ synapse/start.sh
@@ -20,21 +20,6 @@ done
 
 envsubst < /data/homeserver.template.yaml > /data/homeserver.yaml
 
-# Wait for the OIDC mock server to be up, exit with an error after 2 minutes
-timeout=120
-elapsed=0
-echo "Waiting for OIDC mock server to be up..."
-while ! wget -q http://oidc.workadventure.localhost/.well-known/openid-configuration; do
-    if [ $elapsed -ge $timeout ]; then
-        echo "Error: OIDC mock server did not start within 2 minutes."
-        exit 1
-    fi
-    echo "Waiting for OIDC mock server to be up..."
-    sleep 1
-    elapsed=$((elapsed + 1))
-done
-echo "OIDC mock server is up."
-
 python -m synapse.app.homeserver \
     --config-path /data/homeserver.yaml \
     --generate-config \
