c6725203610721079ee76ce6f9c62019095199f2
David Négrier <d.negrier@thecodingmachine.com>
2025-10-14 16:15:59 +0200

    Fixing handling of MAX_DISPLAYED_VIDEOS
    The MAX_DISPLAYED_VIDEOS was not taken into account anymore.
    Also, refactoring env vars in the dev docker-compose to avoid warnings on startup.

--- play/src/front/Components/EmbedScreens/CamerasContainer.svelte
+++ play/src/front/Components/EmbedScreens/CamerasContainer.svelte
@@ -84,6 +84,13 @@
     $: {
         if (!isOnOneLine) {
             containerHeight = maxContainerHeight * localUserStore.getCameraContainerHeight();
+            if (camerasContainer) {
+                camerasContainer.style.height = `${containerHeight}px`;
+            }
+        } else {
+            if (camerasContainer) {
+                camerasContainer.style.height = "";
+            }
         }
     }
 
@@ -144,7 +151,6 @@
             // This is not 100% accurate, as if we are in "solution 2", the maximum number of videos
             // will be maximumVideosPerPage + nbVideos % vpr
             const maxNbVideos = Math.min($streamableCollectionStore.size, maximumVideosPerPage);
-
             // If we need scrolling, calculate the maximum height that would fit
             if (maxVisibleVideos < maxNbVideos) {
                 // Calculate total number of rows needed
@@ -187,7 +193,6 @@
                     }
                     // if solution 1 is better
                     adjustedWidth = adjustedWidthWithReducedHeight;
-
                     // We put the maximum number of visible videos in a store. This store will be used to show active participants first.
                     maxVisibleVideosStore.set(vpr * (rowsPerPage + 1));
                 } else {
@@ -266,6 +271,14 @@
     function onResizeHandler(height: number) {
         containerHeight = height;
         localUserStore.setCameraContainerHeight(containerHeight / maxContainerHeight);
+        if (camerasContainer) {
+            const oldHeight = camerasContainer.scrollHeight;
+            // Move the scroll position to keep the same percentage of position
+            const oldScrollPercent = camerasContainer.scrollTop / oldHeight;
+
+            camerasContainer.style.height = `${containerHeight}px`;
+            camerasContainer.scrollTop = camerasContainer.scrollHeight * oldScrollPercent;
+        }
     }
 </script>
 
@@ -332,7 +345,6 @@
         <ResizeHandle
             minHeight={maxContainerHeight * 0.1}
             maxHeight={maxContainerHeight * 0.9}
-            currentHeight={containerHeight}
             onResize={onResizeHandler}
             onResizeEnd={() => analyticsClient.resizeCameraLayout()}
             dataTestid="resize-handle"
