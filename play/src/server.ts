e07a0af826af18ed3ef8df4dee31c772d1228657
David Négrier <d.negrier@thecodingmachine.com>
2025-11-06 16:18:24 +0100

    Adding gRPC reflection to RoomAPI

--- play/src/server.ts
+++ play/src/server.ts
@@ -1,9 +1,12 @@
 import fs from "fs";
 import process from "process";
+import path from "path";
 import * as Sentry from "@sentry/node";
 import * as grpc from "@grpc/grpc-js";
 import { RoomApiService } from "@workadventure/messages/src/ts-proto-generated/room-api";
 import { setErrorHandler } from "@workadventure/shared-utils";
+import * as protoLoader from "@grpc/proto-loader";
+import * as reflection from "@grpc/reflection";
 import app from "./pusher/app";
 import {
     PUSHER_HTTP_PORT,
@@ -68,6 +71,12 @@ if (!ADMIN_API_URL && !ROOM_API_SECRET_KEY) {
     console.info("RoomAPI is disabled! ROOM_API_SECRET_KEY is not defined on environment variables.");
 } else {
     const RoomAPI = new grpc.Server();
+
+    const PROTO_PATH = path.join(__dirname, "../../messages/protos/room-api.proto");
+    const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+    const reflectionInstance = new reflection.ReflectionService(packageDefinition);
+
+    reflectionInstance.addToServer(RoomAPI);
 
     RoomAPI.addService(RoomApiService, RoomApiServer);
 
