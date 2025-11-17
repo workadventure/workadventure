d6ded39e617f229d34a50eda2f6e22fa6384de66
Grégoire Parant <gregoire@workadventu.re>
2025-11-17 11:11:33 +0100

    Remove use less type properties from the svelte component

--- play/src/front/Components/MapEditor/AreaEditor/AreaPropertiesEditor.svelte
+++ play/src/front/Components/MapEditor/AreaEditor/AreaPropertiesEditor.svelte
@@ -825,7 +825,6 @@
             {#each properties as property (property.id)}
                 {#if property.type !== "areaDescriptionProperties"}
                     <div class="property-box border border-solid border-white/20 bg-white/5 rounded p-2 my-8">
-                        {property.type}
                         {#if property.type === "focusable"}
                             <FocusablePropertyEditor
                                 {property}
