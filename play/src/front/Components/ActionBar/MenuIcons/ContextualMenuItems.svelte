68f6f40fca760964c35983867099256df7539ce3
yannisdia <y.diarra@workadventu.re>
2025-11-20 17:35:13 +0100

    fix: update megaphone state when opening GlobalCommunication modal and add action button to stop megaphone in actionbar

--- play/src/front/Components/ActionBar/MenuIcons/ContextualMenuItems.svelte
+++ play/src/front/Components/ActionBar/MenuIcons/ContextualMenuItems.svelte
@@ -4,6 +4,7 @@
     import { bottomActionBarVisibilityStore } from "../../../Stores/BottomActionBarStore";
     import { inLivekitStore } from "../../../Stores/MediaStore";
     import { followStateStore } from "../../../Stores/FollowStore";
+    import { requestedMegaphoneStore } from "../../../Stores/MegaphoneStore";
     import LL from "../../../../i18n/i18n-svelte";
     import AppsMenuItem from "./AppsMenuItem.svelte";
     import FollowMenuItem from "./FollowMenuItem.svelte";
@@ -11,6 +12,7 @@
     import LockDiscussionMenuItem from "./LockDiscussionMenuItem.svelte";
     import MusicMenuItem from "./MusicMenuItem.svelte";
     import HeaderMenuItem from "./HeaderMenuItem.svelte";
+    import MegaphoneMenuItem from "./MegaphoneMenuItem.svelte";
 
     const inProfileMenu = getContext("profileMenu");
 
@@ -41,6 +43,10 @@
     <!-- <ChangeLayoutMenuItem /> -->
 
     <LockDiscussionMenuItem />
+{/if}
+
+{#if $requestedMegaphoneStore}
+    <MegaphoneMenuItem />
 {/if}
 
 {#if inProfileMenu}
