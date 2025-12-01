dcfaae4ae9d624ddd2cc2de5019d990723be6883
yannisdia <y.diarra@workadventu.re>
2025-12-01 17:15:23 +0100

    fix test and run pretty

--- play/src/front/Stores/StreamableCollectionStore.ts
+++ play/src/front/Stores/StreamableCollectionStore.ts
@@ -100,7 +100,7 @@ const localstreamStoreValue = derived(stableLocalStreamStore, (myLocalStream) =>
 const mutedLocalStream = muteMediaStreamStore(localstreamStoreValue);
 
 export const myCameraPeerStore: Readable<VideoBox> = derived([LL], ([$LL]) => {
-    const streamable : Streamable = {
+    const streamable: Streamable = {
         uniqueId: "-1",
         media: {
             type: "webrtc" as const,
