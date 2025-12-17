e58f478fb0f001e2b7170a211d044df31afec7c1
yannisdia <y.diarra@workadventu.re>
2025-12-17 11:12:37 +0100

    WIP

--- play/src/front/Components/ActionBar/MenuIcons/MapSubMenu.svelte
+++ play/src/front/Components/ActionBar/MenuIcons/MapSubMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { clickOutside } from "svelte-outside";
     import { getContext, setContext } from "svelte";
-    import { IconChevronDown,IconTools } from "@wa-icons";
+    import { IconChevronDown,IconMapEditor } from "@wa-icons";
     import { mapMenuVisibleStore, openedMenuStore } from "../../../Stores/MenuStore";
     import { LL } from "../../../../i18n/i18n-svelte";
     import { createFloatingUiActions } from "../../../Utils/svelte-floatingui";
@@ -45,19 +45,20 @@
                 openedMenuStore.toggle("mapMenu");
             }}
         >
-            <div class="group bg-contrast/80 backdrop-blur rounded-lg h-16 @sm/actions:h-14 @xl/actions:h-16 p-2mr">
-                <div class="flex items-center h-full group-hover:bg-white/10mr group-hover:rounded gap-2 pl-4 pr-3">
-                    <IconTools font-size="18" class="text-white" />
-                    <div class="pr-2">
+            <div class="group bg-contrast/80 backdrop-blur rounded-lg h-16 @sm/actions:h-14 @xl/actions:h-16 p-2 ">
+                <div class="flex items-center h-full group-hover:bg-white/10mr group-hover:rounded pl-4 pr-4 gap-2 hover:bg-white/10">
+                    <IconMapEditor font-size="20" class="text-white" />
+                    <div class="pr">
                         <div
                             class="font-bold text-white leading-3 whitespace-nowrap select-none text-base @sm/actions:text-sm @xl/actions:text-base"
                         >
                             {$LL.actionbar.map()}
                         </div>
                     </div>
+
                     <IconChevronDown
                         stroke={2}
-                        Class="h-4 w-4 aspect-square transition-all opacity-50 {$openedMenuStore === 'mapMenu'
+                        class="h-4 w-4 aspect-square transition-all opacity-50 {$openedMenuStore === 'mapMenu'
                             ? 'rotate-180'
                             : ''}"
                         height="16px"
