592032a94749bccb5b9ede73a62c9c96a8fb60d0
grégoire parant <gregoire@workadventu.re>
2026-01-13 08:35:23 +0100

    Guest Access Mode (#5516)
    * Integrate new feature add guest access
    
    * feat: add guest access (#5425)
    
    * feat: add guest access
    
    * fix: generate env docs
    
    * Revert "fix: generate env docs"
    
    This reverts commit dd123969b9b4960167a643a9450185771f60f2cc.
    
    ---------
    
    Co-authored-by: grégoire parant <gregoire@workadventu.re>
    
    * Create random for woka collection and woka texture
    
    * Create random funny name
    
    * change to have enableFastPass verified by "MapDetailsData" type
    
    ---------
    
    Co-authored-by: Thomas PEREZ <92369255+ThomasPerez91@users.noreply.github.com>

--- play/src/pusher/enums/EnvironmentVariable.ts
+++ play/src/pusher/enums/EnvironmentVariable.ts
@@ -88,6 +88,9 @@ export const ENABLE_CHAT: boolean = env.ENABLE_CHAT;
 export const ENABLE_CHAT_UPLOAD: boolean = env.ENABLE_CHAT_UPLOAD;
 export const ENABLE_CHAT_ONLINE_LIST: boolean = env.ENABLE_CHAT_ONLINE_LIST;
 export const ENABLE_CHAT_DISCONNECTED_LIST: boolean = env.ENABLE_CHAT_DISCONNECTED_LIST;
+export const DEFAULT_GUEST_NAME: string = env.DEFAULT_GUEST_NAME || "";
+export const DEFAULT_GUEST_TEXTURE: string = env.DEFAULT_GUEST_TEXTURE || "";
+export const GUEST_NAME_APPEND_RANDOM_NUMBERS: boolean = env.GUEST_NAME_APPEND_RANDOM_NUMBERS || false;
 //export const DEBUG_ERROR_MESSAGES = env.DEBUG_ERROR_MESSAGES;
 
 // If set to the string "true", the /openapi route will return the OpenAPI definition and the swagger-ui/ route will display the documentation
@@ -217,5 +220,8 @@ export const FRONT_ENVIRONMENT_VARIABLES: FrontConfigurationInterface = {
     GRPC_MAX_MESSAGE_SIZE: env.GRPC_MAX_MESSAGE_SIZE,
     TURN_CREDENTIALS_RENEWAL_TIME: env.TURN_CREDENTIALS_RENEWAL_TIME,
     BACKGROUND_TRANSFORMER_ENGINE: env.BACKGROUND_TRANSFORMER_ENGINE || "selfie-segmentation",
+    DEFAULT_GUEST_NAME: DEFAULT_GUEST_NAME,
+    DEFAULT_GUEST_TEXTURE: DEFAULT_GUEST_TEXTURE,
+    GUEST_NAME_APPEND_RANDOM_NUMBERS: GUEST_NAME_APPEND_RANDOM_NUMBERS,
 };
 export const GRPC_MAX_MESSAGE_SIZE = env.GRPC_MAX_MESSAGE_SIZE;
