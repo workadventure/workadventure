5fe4dca98784481c6f024a673ef98b20013ac697
yannisdia <y.diarra@workadventu.re>
2026-01-27 17:25:23 +0100

    add translation

--- play/src/i18n/ca-ES/area.ts
+++ play/src/i18n/ca-ES/area.ts
@@ -3,6 +3,11 @@ import type { Translation } from "../i18n-types";
 
 const area: DeepPartial<Translation["area"]> = {
     noAccess: "Ho sentim, no tens accés a aquesta àrea.",
+    blocked: {
+        locked: "Aquesta àrea està bloquejada. No pots entrar.",
+        maxUsers: "Aquesta àrea està plena. No pots entrar.",
+        noAccess: "Ho sentim, no tens accés a aquesta àrea.",
+    },
     personalArea: {
         claimDescription: "Aquesta és una àrea personal. Vols fer-la teva?",
         buttons: {
