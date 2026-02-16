66e29b13bd0a51398402f21486bad4f95502f174
Grégoire Parant <gregoire@workadventu.re>
2026-02-16 10:58:58 +0100

    Fix to use "blur" and "focus" event

--- play/src/front/Stores/VisibilityStore.ts
+++ play/src/front/Stores/VisibilityStore.ts
@@ -8,9 +8,23 @@ export const visibilityStore = readable(document.visibilityState === "visible", 
         set(document.visibilityState === "visible");
     };
 
+    // Create a blur event listener
+    const onBlur = () => {
+        set(false);
+    };
+
+    // Create a focus event listener
+    const onFocus = () => {
+        set(true);
+    };
+
     document.addEventListener("visibilitychange", onVisibilityChange);
+    window.addEventListener("blur", onBlur);
+    window.addEventListener("focus", onFocus);
 
     return function stop() {
         document.removeEventListener("visibilitychange", onVisibilityChange);
+        window.removeEventListener("blur", onBlur);
+        window.removeEventListener("focus", onFocus);
     };
 });
