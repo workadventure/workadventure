00d6130529548776168adc8c930a7d0947905520
Grégoire Parant <gregoire@workadventu.re>
2026-02-16 11:11:18 +0100

    Update to remove useless visibilityState API

--- play/src/front/Stores/VisibilityStore.ts
+++ play/src/front/Stores/VisibilityStore.ts
@@ -3,10 +3,13 @@ import { readable } from "svelte/store";
 /**
  * A store containing whether the current page is visible or not.
  */
-export const visibilityStore = readable(document.visibilityState === "visible", function start(set) {
-    const onVisibilityChange = () => {
-        set(document.visibilityState === "visible");
-    };
+export const visibilityStore = readable(true, function start(set) {
+    // visibilitychange and document.hidden is not working as expected, so we use blur and focus events instead
+    // For example if the user open a new browser behind the current one, the visibilitychange event will not be triggered.
+    // So in this case, only the blur and focus events will be triggered.
+    /*const onVisibilityChange = () => {
+        set(document.visibilityState === "visible" && !document.hidden);
+    };*/
 
     // Create a blur event listener
     const onBlur = () => {
@@ -18,12 +21,12 @@ export const visibilityStore = readable(document.visibilityState === "visible", 
         set(true);
     };
 
-    document.addEventListener("visibilitychange", onVisibilityChange);
+    //document.addEventListener("visibilitychange", onVisibilityChange);
     window.addEventListener("blur", onBlur);
     window.addEventListener("focus", onFocus);
 
     return function stop() {
-        document.removeEventListener("visibilitychange", onVisibilityChange);
+        //document.removeEventListener("visibilitychange", onVisibilityChange);
         window.removeEventListener("blur", onBlur);
         window.removeEventListener("focus", onFocus);
     };
