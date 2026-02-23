e270bcccb0958c81f343a220bdecf84aca5a9895
Grégoire Parant <gregoire@workadventu.re>
2026-02-23 12:07:15 +0100

    Fix onboarding

--- tests/tests/utils/onboarding.ts
+++ tests/tests/utils/onboarding.ts
@@ -7,14 +7,30 @@ import type { Page } from "playwright/test";
  * @param page - The Playwright page instance
  */
 export async function closeOnboarding(page: Page): Promise<void> {
-    await page.addLocatorHandler(page.getByTestId("onboarding-step"), async () => {
-        try {
-            // Click on the button to close onboarding
-            await page.getByTestId("onboarding-button-welcome-skip").click();
-            // Check if onboarding is hidden
-            await expect(page.getByTestId("onboarding-step")).toBeHidden();
-        } catch (error) {
-            console.error("Error closing onboarding", error);
+    try {
+        // Check if the local storage has the key "workadventure-onboarding-completed"
+        const localStorage = await page.evaluate(() => localStorage.getItem("workadventure-onboarding-completed"));
+        if (localStorage == "true") {
+            return;
         }
-    });
+        // Wait for onboarding to be visible
+        await expect(page.getByTestId("onboarding-step")).toBeVisible({ timeout: 10_000 });
+        // Click on the button to close onboarding
+        await page.getByTestId("onboarding-button-welcome-skip").click({ timeout: 1000 });
+        // Check if onboarding is hidden
+        await expect(page.getByTestId("onboarding-step")).toBeHidden({ timeout: 1000 });
+    } catch (error) {
+        //console.error("Error closing onboarding", error);
+        // Add a localtor handler. When onboarding is showing, close it.
+        await page.addLocatorHandler(page.getByTestId("onboarding-step"), async () => {
+            try {
+                // Click on the button to close onboarding
+                await page.getByTestId("onboarding-button-welcome-skip").click({ timeout: 1000 });
+                // Check if onboarding is hidden
+                await expect(page.getByTestId("onboarding-step")).toBeHidden({ timeout: 1000 });
+            } catch (error) {
+                console.error("Error closing onboarding", error);
+            }
+        });
+    }
 }
