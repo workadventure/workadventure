5a4186454e5eec9558d340ba65761752e33126f1
David Négrier <d.negrier@thecodingmachine.com>
2026-03-10 17:35:29 +0100

    Removing dead code, fixing tests

--- libs/map-editor/tests/GameMap/MapValidator.test.ts
+++ libs/map-editor/tests/GameMap/MapValidator.test.ts
@@ -1,7 +1,8 @@
 import path from "node:path";
 import * as fs from "fs";
 import { describe, expect, it } from "vitest";
-import { ErrorType, isFailure, MapValidation, MapValidator } from "../../src/GameMap/MapValidator";
+import type { ErrorType, MapValidation } from "../../src/GameMap/MapValidator";
+import { isFailure, MapValidator } from "../../src/GameMap/MapValidator";
 import { ZipFileFetcher } from "../../src/GameMap/Validator/ZipFileFetcher";
 
 async function loadMap(mapPath: string, logLevel: ErrorType = "info"): Promise<MapValidation> {
@@ -375,7 +376,7 @@ describe("Map validator", () => {
             // eslint-disable-next-line @typescript-eslint/require-await
             const validator = new MapValidator("error", { fileExists: async () => true });
             const validWAM = {
-                version: "2.0.0",
+                version: "2.1.0",
                 mapUrl: "https://example.com/map.tmj",
                 entities: {},
                 areas: [],
@@ -395,7 +396,7 @@ describe("Map validator", () => {
             // eslint-disable-next-line @typescript-eslint/require-await
             const validator = new MapValidator("error", { fileExists: async () => true });
             const invalidWAM = {
-                version: "2.0.0",
+                version: "2.1.0",
                 mapUrl: "https://example.com/map.tmj",
                 entities: {},
                 entityCollections: [],
