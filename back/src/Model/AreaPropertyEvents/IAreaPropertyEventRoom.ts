d83f71b0ffd0363efa1c1d7cd1438821037376c8
ydiarra <yd97@hotmail.fr>
2026-03-12 12:47:13 +0100

    Revert "Adding a WamManager class in the back that tracks the true state of the WAM file. The WAM file is now updated based on messages received from the map-storage."
    This reverts commit 9c1cb383e6533492a9dc06d422e9315ada501efa.

--- back/src/Model/AreaPropertyEvents/IAreaPropertyEventRoom.ts
+++ back/src/Model/AreaPropertyEvents/IAreaPropertyEventRoom.ts
@@ -6,7 +6,7 @@ import type { PointInterface } from "../Websocket/PointInterface";
  * to apply area-empty and area-geometry-change behaviors without depending on GameRoom directly.
  */
 export interface IAreaPropertyEventRoom {
-    getWam(): WAMFileFormat | undefined;
+    getWam(): Promise<WAMFileFormat | undefined>;
     getAreasWithPropertyTypesContainingPosition(
         position: PointInterface,
         propertyTypes: string[]
