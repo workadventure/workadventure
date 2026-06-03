3dfe850056776032c47541080121de2adeb90922
David Négrier <d.negrier@thecodingmachine.com>
2026-06-03 15:41:34 +0200

    Removing wobbling effect because it is too slow performance-wise. Adding a pulse effect in place, removing old talk icon and adding a special color when we are a speaker

--- play/src/front/Phaser/Components/UsernameDisplay.ts
+++ play/src/front/Phaser/Components/UsernameDisplay.ts
@@ -234,11 +234,12 @@ export class UsernameDisplay {
             color === undefined ? PLAYER_NAME_BACKGROUND_COLOR : `#${color.toString(16).padStart(6, "0")}`;
     }
 
-    public setTalking(talking: boolean): void {
+    public setTalking(talking: boolean, isSpeaker: boolean): void {
         if (talking) {
-            this.element.classList.add("talking");
+            this.element.classList.add(isSpeaker ? "speaker" : "talking");
         } else {
             this.element.classList.remove("talking");
+            this.element.classList.remove("speaker");
         }
     }
 }
