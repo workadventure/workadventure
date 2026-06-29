0e8cd96cb5ea5b79c46960635d4234a8c18c6283
David Négrier <d.negrier@thecodingmachine.com>
2026-06-29 15:39:38 +0200

    Fixing map-storage UI following Svelte 5 migration
    Svelte 5 migration broke the map-storage UI. We are fixing it here.

--- map-storage/src-ui/main.ts
+++ map-storage/src-ui/main.ts
@@ -1,8 +1,9 @@
 //import "./app.css";
+import { mount } from "svelte";
 import App from "./App.svelte";
 
-const app = new App({
-    target: document.getElementById("app"),
+const app = mount(App, {
+    target: document.getElementById("app") as HTMLElement,
 });
 
 export default app;
