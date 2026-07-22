1d1974ffaf661903a68f4d84ba796cef0c578f6d
yannisdia <y.diarra@workadventu.re>
2026-07-22 09:29:32 +0200

    test(play/storybook): misc prop-driven batch (Tooltip, HelpTooltip, BrowserNotSupported, FollowButton, Search)
    All render-direct / prop-driven, no store setup:
    - Util/Tooltip, Tooltip/HelpTooltip (+ shortcuts variant)
    - BrowserNotSupported, FollowMenu/FollowButton (visible/hidden), Util/Search

---
+++ play/src/front/Components/BrowserNotSupported/BrowserNotSupported.stories.svelte
@@ -0,0 +1,19 @@
+<script module lang="ts">
+    import { expect } from "storybook/test";
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import BrowserNotSupported from "./BrowserNotSupported.svelte";
+
+    const { Story } = defineMeta({
+        title: "Feedback/Browser Not Supported",
+        component: BrowserNotSupported,
+        tags: ["autodocs"],
+    });
+</script>
+
+<Story
+    name="Default"
+    play={async ({ canvasElement }) => {
+        await expect(canvasElement.querySelector("[data-testid='update-browser-button']")).toBeInTheDocument();
+        await expect(canvasElement.querySelector("[data-testid='leave-button']")).toBeInTheDocument();
+    }}
+/>
---
+++ play/src/front/Components/FollowMenu/FollowButton.stories.svelte
@@ -0,0 +1,30 @@
+<script module lang="ts">
+    import { expect } from "storybook/test";
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import FollowButton from "./FollowButton.svelte";
+
+    const { Story } = defineMeta({
+        title: "Feature/Follow Button",
+        component: FollowButton,
+        tags: ["autodocs"],
+        parameters: { layout: "centered" },
+        args: { hidden: false },
+    });
+</script>
+
+<Story
+    name="Visible"
+    play={async ({ canvasElement }) => {
+        const button = canvasElement.querySelector(".btn-follow");
+        await expect(button).toBeInTheDocument();
+        await expect(button).not.toHaveClass("hide");
+    }}
+/>
+
+<Story
+    name="Hidden"
+    args={{ hidden: true }}
+    play={async ({ canvasElement }) => {
+        await expect(canvasElement.querySelector(".btn-follow")).toHaveClass("hide");
+    }}
+/>
