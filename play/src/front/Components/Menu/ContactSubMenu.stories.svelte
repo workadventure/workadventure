87da7a0bee3af94d565e1f2d12160a8b270f3451
yannisdia <y.diarra@workadventu.re>
2026-07-22 09:54:14 +0200

    test(play/storybook): Onboarding + Menu batch
    - Onboarding/OnboardingStep (children snippet)
    - Menu/CustomSubMenu (iframe), Menu/ContactSubMenu (renders its default branch)

---
+++ play/src/front/Components/Menu/ContactSubMenu.stories.svelte
@@ -0,0 +1,18 @@
+<script module lang="ts">
+    import { expect } from "storybook/test";
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import ContactSubMenu from "./ContactSubMenu.svelte";
+
+    const { Story } = defineMeta({
+        title: "Feature/Menu/Contact Sub Menu",
+        component: ContactSubMenu,
+        tags: ["autodocs"],
+    });
+</script>
+
+<Story
+    name="Default"
+    play={async ({ canvasElement }) => {
+        await expect(canvasElement.querySelector(".blue-title")).toBeInTheDocument();
+    }}
+/>
---
+++ play/src/front/Components/Menu/CustomSubMenu.stories.svelte
@@ -0,0 +1,25 @@
+<script module lang="ts">
+    import { expect } from "storybook/test";
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import CustomSubMenu from "./CustomSubMenu.svelte";
+
+    const { Story } = defineMeta({
+        title: "Feature/Menu/Custom Sub Menu",
+        component: CustomSubMenu,
+        tags: ["autodocs"],
+        args: {
+            url: "https://workadventu.re",
+            allowApi: false,
+            allow: undefined,
+        },
+    });
+</script>
+
+<Story
+    name="Default"
+    play={async ({ canvasElement }) => {
+        const iframe = canvasElement.querySelector("iframe[title='customSubMenu']");
+        await expect(iframe).toBeInTheDocument();
+        await expect(iframe).toHaveAttribute("src", "https://workadventu.re");
+    }}
+/>
